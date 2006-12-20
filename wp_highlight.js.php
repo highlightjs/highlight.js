9c6e360ac60fb934e307ade9904432ae30064668
maniac <>
2006-12-20 08:19:41 +0000

    Версия 2.0 в плагин WP

--- wp_highlight.js.php
+++ wp_highlight.js.php
@@ -3,7 +3,7 @@
 Plugin Name: highlight.js
 Plugin URI: http://softwaremaniacs.org/soft/highlight/
 Description: Syntax highlighting with language autodetection
-Version: 1.0
+Version: 2.0
 Author: Ivan Sagalaev
 Author URI: http://softwaremaniacs.org/about/
 */
