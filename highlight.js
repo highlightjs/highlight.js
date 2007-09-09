31297a3150ae5fcdc41bf57e74a4a0e693d05a3b
maniac <>
2007-09-09 07:50:01 +0000

    Удалил Forth, слишком жадные правила, а с автором связаться не получилось

--- highlight.js
+++ highlight.js
@@ -4,7 +4,7 @@ http://softwaremaniacs.org/soft/highlight/
 */
 
 var DEFAULT_LANGUAGES = ['python', 'ruby', 'perl', 'php', 'css', 'html', 'django', 'javascript', 'java', 'cpp', 'sql', 'smalltalk'];
-var ALL_LANGUAGES = (DEFAULT_LANGUAGES.join(',') + ',' + ['1c', 'axapta', 'delphi', 'rib', 'rsl', 'vbscript', 'forth'].join(',')).split(',');
+var ALL_LANGUAGES = (DEFAULT_LANGUAGES.join(',') + ',' + ['1c', 'axapta', 'delphi', 'rib', 'rsl', 'vbscript'].join(',')).split(',');
 var LANGUAGE_GROUPS = {
   'html': 'html',
   'css': 'html',
