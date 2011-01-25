8d705ce3ee6207d33bb3f9dfe72ba330c5b9a48f
Ivan Sagalaev <Maniac@SoftwareManiacs.Org>
2011-01-25 17:39:51 +0800

    Zeroed out relevance for label, removed single-letter operators form built-ins.

--- src/languages/haskell.js
+++ src/languages/haskell.js
@@ -9,7 +9,7 @@ hljs.LANGUAGES.haskell = {
     contains: ['comment', 'string', 'import', 'module', 'class', 'number', 'shebang', 'label', 'title'],
     keywords: {
       'keyword': {'let': 1,'in': 1,'if': 1,'then': 1,'else': 1,'case': 1,'of': 1,'where': 1,'do': 1,'module': 1,'import': 1, 'hiding': 1,'qualified': 1,'type': 1,'data': 1,'deriving': 1,'class': 1,'instance': 1,'null': 1,'not': 1,'as': 1},
-      'built_in': {'Bool': 1,'True': 1,'False': 1,'Int': 1,'Char': 1,'Maybe': 1,'Nothing': 1,'String': 1,'::': 1,'->': 1,'<-': 1,'=>': 1, '<=': 1,'=': 1,'\\\\': 1,'$': 1,'+': 1,'-': 1,'*': 1,'/': 1,'==': 1,'/=': 1,'<': 1,'>': 1,'|': 1,'++': 1}
+      'built_in': {'Bool': 1,'True': 1,'False': 1,'Int': 1,'Char': 1,'Maybe': 1,'Nothing': 1,'String': 1}
     }
   },
   modes: [
@@ -52,7 +52,8 @@ hljs.LANGUAGES.haskell = {
     },
     {
       className: 'label',
-      begin: '\\b[A-Z][\\w\\\']*', end: hljs.IMMEDIATE_RE
+      begin: '\\b[A-Z][\\w\\\']*', end: hljs.IMMEDIATE_RE,
+      relevance: 0
     },
     {
       className: 'import',
