af788218b4df3033777f2bba2e0d87645dc89773
Ivan Sagalaev <Maniac@SoftwareManiacs.Org>
2011-03-09 17:03:56 -0800

    Converted Erlang to mode-less syntax

--- src/languages/erlang.js
+++ src/languages/erlang.js
@@ -6,9 +6,7 @@ Author: Nikolay Zakharov <nikolay.desh@gmail.com>, Dmitry Kovega <arhibot@gmail.
 
 hljs.LANGUAGES.erlang = function(){
   var BASIC_ATOM_RE = '[a-z\'][a-zA-Z0-9_\']*';
-  var BASIC_MODES = ['comment', 'named_fun', 'block_statements', 'function_call', 'atom', 'string', 'number', 'tuple', 'variable', 'record_access'];
   var FUNCTION_NAME_RE = '(' + BASIC_ATOM_RE + ':' + BASIC_ATOM_RE + '|' + BASIC_ATOM_RE + ')';
-
   var ERLANG_RESERVED = {
     'keyword': {
         'after': 1,
@@ -40,145 +38,164 @@ hljs.LANGUAGES.erlang = function(){
     },
     'literal': {'false': 1, 'true': 1}
   };
-  return {
-    defaultMode: {
-      lexems: hljs.UNDERSCORE_IDENT_RE,
-      illegal: '(</|\\*=|\\+=|-=|/=|/\\*|\\*/|\\(\\*|\\*\\))',
-      contains: ['function', 'operators', 'dot', 'comment', 'pp', 'number', 'string', 'record_access', 'variable', 'tuple'],
-      keywords: ERLANG_RESERVED
-    },
-    modes: [
-      hljs.C_NUMBER_MODE,
-      hljs.QUOTE_STRING_MODE,
+
+  var COMMENT = {
+    className: 'comment',
+    begin: '%', end: '$',
+    relevance: 0
+  };
+  var NAMED_FUN = {
+    begin: 'fun\\s+' + BASIC_ATOM_RE + '/\\d+'
+  };
+  var FUNCTION_CALL = {
+    begin: FUNCTION_NAME_RE + '\\(', end: '\\)',
+    returnBegin: true,
+    relevance: 0,
+    contains: [
       {
-        className: 'comment',
-        begin: '%', end: '$',
-        relevance: 0
+        className: 'function_name', begin: FUNCTION_NAME_RE
       },
       {
-        className: 'function',
-        begin: '^' + BASIC_ATOM_RE + '\\(', end: ';|\\.',
-        returnBegin: true,
-        contains: ['params', 'title','function_body']
-      },
-      {
-        className: 'function_body',
-        lexems: BASIC_ATOM_RE,
-        keywords: ERLANG_RESERVED,
-        begin: '->', endsWithParent: true,
-        contains: BASIC_MODES
-      },
-      {
-        className: 'function_name',
-        begin: FUNCTION_NAME_RE
-      },
-      {
-        className: 'title',
-        begin: BASIC_ATOM_RE
-      },
-      {
-        className: 'variable',
-        begin: '\\b_([A-Z][A-Za-z0-9_]*)?',
-        relevance: 0
-      },
-      {
-        className: 'named_fun',
-        begin: 'fun\\s+' + BASIC_ATOM_RE + '/\\d+'
-      },
-      {
-        className: 'block_statements',
-        lexems: BASIC_ATOM_RE,
-        keywords: ERLANG_RESERVED,
-        begin: '(fun|receive|if|try|case)', end: 'end',
-        contains: ['comment', 'named_fun', 'quoted_atom', 'block_statements', 'function_call', 'string', 'number', 'tuple', 'variable', 'record_access']
-      },
-      {
-        className: 'variable',
-        begin: '[A-Z][a-zA-Z0-9_]*',
-        relevance: 0
-      },
-      {
-        className: 'function_call',
-        begin: FUNCTION_NAME_RE + '\\(', end: '\\)',
-        returnBegin: true,
-        relevance: 0,
-        contains: ['function_name', 'params_wo_rparen']
-      },
-      {
-        className: 'quoted_atom',
-        begin: '\'', end: '\'',
-        illegal: '\\n',
-        contains: [hljs.BACKSLASH_ESCAPE],
-        relevance: 0
-      },
-      {
-        className: 'pp',
-        begin: '^-', end: '\\.',
-        relevance: 0,
-        excludeEnd: true,
-        returnBegin: true,
-        lexems: '-' + hljs.UNDERSCORE_IDENT_RE,
-        keywords: {
-          '-module':1,
-          '-record':1,
-          '-undef':1,
-          '-export':1,
-          '-ifdef':1,
-          '-ifndef':1,
-          '-author':1,
-          '-copyright':1,
-          '-doc':1,
-          '-vsn':1,
-          '-import': 1,
-          '-include': 1,
-          '-include_lib': 1,
-          '-compile': 1,
-          '-define': 1,
-          '-else': 1,
-          '-endif': 1,
-          '-file': 1,
-          '-behaviour': 1,
-          '-behavior': 1
-        },
-        contains: ['params']
-      },
-      {
-        className: 'record_access',
-        begin: '#', end: '}',
-        illegal: '.',
-        relevance: 0,
-        returnBegin: true,
-        contains: ['record_name', 'field_access']
-      },
+        begin: '\\(', end: '\\)', endsWithParent: true,
+        returnEnd: true
+        // "contains" defined later
+      }
+    ]
+  };
+  var TUPLE = {
+    className: 'tuple',
+    begin: '{', end: '}'
+    // "contains" defined later
+  };
+  var VAR1 = {
+    className: 'variable',
+    begin: '\\b_([A-Z][A-Za-z0-9_]*)?',
+    relevance: 0
+  };
+  var VAR2 = {
+    className: 'variable',
+    begin: '[A-Z][a-zA-Z0-9_]*',
+    relevance: 0
+  };
+  var RECORD_ACCESS = {
+    begin: '#', end: '}',
+    illegal: '.',
+    relevance: 0,
+    returnBegin: true,
+    contains: [
       {
         className: 'record_name',
         begin: '#' + hljs.UNDERSCORE_IDENT_RE,
         relevance: 0
       },
       {
-        className: 'field_access',
         begin: '{', endsWithParent: true,
-        relevance: 0,
-        contains: BASIC_MODES
-      },
-      {
-        className: 'tuple',
-        begin: '{', end: '}',
-        contains: BASIC_MODES
-      },
-      {
-        className: 'params_wo_rparen',
-        begin: '\\(', end: '\\)',
-        returnEnd: true,
-        endsWithParent: true,
-        contains: BASIC_MODES
-      },
-      {
-        className: 'params',
-        begin: '\\(', end: '\\)',
-        endsWithParent: true,
-        contains: BASIC_MODES
+        relevance: 0
+        // "contains" defined later
       }
     ]
+  };
+
+  var BLOCK_STATEMENTS = {
+    lexems: BASIC_ATOM_RE,
+    keywords: ERLANG_RESERVED,
+    begin: '(fun|receive|if|try|case)', end: 'end'
+  };
+  BLOCK_STATEMENTS.contains = [
+    COMMENT,
+    NAMED_FUN,
+    hljs.inherit(hljs.APOS_STRING_MODE, {className: ''}),
+    BLOCK_STATEMENTS,
+    FUNCTION_CALL,
+    hljs.QUOTE_STRING_MODE,
+    hljs.C_NUMBER_MODE,
+    TUPLE,
+    VAR1, VAR2,
+    RECORD_ACCESS
+  ];
+
+  var BASIC_MODES = [
+    COMMENT,
+    NAMED_FUN,
+    BLOCK_STATEMENTS,
+    FUNCTION_CALL,
+    hljs.QUOTE_STRING_MODE,
+    hljs.C_NUMBER_MODE,
+    TUPLE,
+    VAR1, VAR2,
+    RECORD_ACCESS
+  ];
+  FUNCTION_CALL.contains[1].contains = BASIC_MODES;
+  TUPLE.contains = BASIC_MODES;
+  RECORD_ACCESS.contains[1].contains = BASIC_MODES;
+
+  var PARAMS = {
+    className: 'params',
+    begin: '\\(', end: '\\)',
+    endsWithParent: true,
+    contains: BASIC_MODES
+  };
+  return {
+    defaultMode: {
+      lexems: hljs.UNDERSCORE_IDENT_RE,
+      keywords: ERLANG_RESERVED,
+      illegal: '(</|\\*=|\\+=|-=|/=|/\\*|\\*/|\\(\\*|\\*\\))',
+      contains: [
+        {
+          className: 'function',
+          begin: '^' + BASIC_ATOM_RE + '\\(', end: ';|\\.',
+          returnBegin: true,
+          contains: [
+            PARAMS,
+            {
+              className: 'title', begin: BASIC_ATOM_RE
+            },
+            {
+              lexems: BASIC_ATOM_RE,
+              keywords: ERLANG_RESERVED,
+              begin: '->', endsWithParent: true,
+              contains: BASIC_MODES
+            }
+          ]
+        },
+        COMMENT,
+        {
+          className: 'pp',
+          begin: '^-', end: '\\.',
+          relevance: 0,
+          excludeEnd: true,
+          returnBegin: true,
+          lexems: '-' + hljs.UNDERSCORE_IDENT_RE,
+          keywords: {
+            '-module':1,
+            '-record':1,
+            '-undef':1,
+            '-export':1,
+            '-ifdef':1,
+            '-ifndef':1,
+            '-author':1,
+            '-copyright':1,
+            '-doc':1,
+            '-vsn':1,
+            '-import': 1,
+            '-include': 1,
+            '-include_lib': 1,
+            '-compile': 1,
+            '-define': 1,
+            '-else': 1,
+            '-endif': 1,
+            '-file': 1,
+            '-behaviour': 1,
+            '-behavior': 1
+          },
+          contains: [PARAMS]
+        },
+        hljs.C_NUMBER_MODE,
+        hljs.QUOTE_STRING_MODE,
+        RECORD_ACCESS,
+        VAR1, VAR2,
+        TUPLE
+      ]
+    }
   };
 }();
