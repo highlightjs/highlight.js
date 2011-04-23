9220222bfc8efd6e2248e556cd10d671291cce5c
Ivan Sagalaev <maniac@maniac-envy.(none)>
2011-04-22 22:20:27 -0700

    Ini converted to mode-less syntax

--- src/languages/ini.js
+++ src/languages/ini.js
@@ -2,34 +2,32 @@
 Language: Ini
 */
 
-hljs.LANGUAGES.ini =
-{
+hljs.LANGUAGES.ini = {
   case_insensitive: true,
   defaultMode: {
-    contains: ['comment', 'title', 'setting'],
-    illegal: '[^\\s]'
-  },
-  modes: [
-    {
-      className: 'comment',
-      begin: ';', end: '$'
-    },
-    {
-      className: 'title',
-      begin: '\\[', end: '\\]'
-    },
-    {
-      className: 'setting',
-      begin: '^[a-z0-9_\\[\\]]+[ \\t]*=[ \\t]*', end: '$',
-      contains: [{
-          className: 'value',
-          endsWithParent: true,
-          contains: ['string', 'number'],
-          lexems: hljs.IDENT_RE,
-          keywords: {'on': 1, 'off': 1, 'true': 1, 'false': 1, 'yes': 1, 'no': 1}
-      }]
-    },
-    hljs.QUOTE_STRING_MODE,
-    hljs.NUMBER_MODE
-  ]
+    illegal: '[^\\s]',
+    contains: [
+      {
+        className: 'comment',
+        begin: ';', end: '$'
+      },
+      {
+        className: 'title',
+        begin: '\\[', end: '\\]'
+      },
+      {
+        className: 'setting',
+        begin: '^[a-z0-9_\\[\\]]+[ \\t]*=[ \\t]*', end: '$',
+        contains: [
+          {
+            className: 'value',
+            endsWithParent: true,
+            lexems: hljs.IDENT_RE,
+            keywords: {'on': 1, 'off': 1, 'true': 1, 'false': 1, 'yes': 1, 'no': 1},
+            contains: [hljs.QUOTE_STRING_MODE, hljs.NUMBER_MODE]
+          }
+        ]
+      }
+    ]
+  }
 };
