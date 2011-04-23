e78046679f9af5862fce2f0a3ae1b8221011600c
Ivan Sagalaev <maniac@maniac-envy.(none)>
2011-04-23 00:29:28 -0700

    Parser 3 converted to mode-less syntax

--- src/languages/parser3.js
+++ src/languages/parser3.js
@@ -4,51 +4,49 @@ Requires: html-xml.js
 Author: Oleg Volchkov <oleg@volchkov.net>
 */
 
-hljs.LANGUAGES.parser3 = {
-  defaultMode: {
-    contains: ['comment', 'preprocessor', 'title', 'variable', 'keyword', 'number'],
-    subLanguage: 'html'
-  },
-  case_insensitive: false,
+hljs.LANGUAGES.parser3 = function() {
+  var COMMENTED_BLOCK = {
+    begin: '{', end: '}'
+  };
+  COMMENTED_BLOCK.contains = [COMMENTED_BLOCK];
 
-  modes: [
-    {
-      className: 'comment',
-      begin: '^#', end: '$'
-    },
-    {
-      className: 'comment',
-      contains: ['commented_block'],
-      begin: '\\^rem{', end: '}',
-      relevance: 10
-    },
-    {
-      className: 'commented_block',
-      contains: ['commented_block'],
-      begin: '{', end: '}',
-      noMarkup: true
-    },
-    {
-      className: 'preprocessor',
-      begin: '^@(?:BASE|USE|CLASS|OPTIONS)$',
-      relevance: 10
-    },
-    {
-      className: 'title',
-      begin: '@[\\w\\-]+\\[[\\w^;\\-]*\\](?:\\[[\\w^;\\-]*\\])?(?:.*)$'
-    },
-    {
-      className: 'variable',
-      begin: '\\$\\{?[\\w\\-\\.\\:]+\\}?'
-    },
-    {
-      className: 'keyword',
-      begin: '\\^[\\w\\-\\.\\:]+'
-    },
-    {
-      className: 'number',
-      begin: '\\^#[0-9a-fA-F]+'
-    },
-    hljs.C_NUMBER_MODE
-  ]
-};
+  return {
+    defaultMode: {
+      subLanguage: 'html',
+      contains: [
+        {
+          className: 'comment',
+          begin: '^#', end: '$'
+        },
+        {
+          className: 'comment',
+          contains: [COMMENTED_BLOCK],
+          begin: '\\^rem{', end: '}',
+          relevance: 10
+        },
+        {
+          className: 'preprocessor',
+          begin: '^@(?:BASE|USE|CLASS|OPTIONS)$',
+          relevance: 10
+        },
+        {
+          className: 'title',
+          begin: '@[\\w\\-]+\\[[\\w^;\\-]*\\](?:\\[[\\w^;\\-]*\\])?(?:.*)$'
+        },
+        {
+          className: 'variable',
+          begin: '\\$\\{?[\\w\\-\\.\\:]+\\}?'
+        },
+        {
+          className: 'keyword',
+          begin: '\\^[\\w\\-\\.\\:]+'
+        },
+        {
+          className: 'number',
+          begin: '\\^#[0-9a-fA-F]+'
+        },
+        hljs.C_NUMBER_MODE
+      ]
+    }
+  };
+}();
