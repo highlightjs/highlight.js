a7e97b7d83d235bab774237766315b44f246f5a7
Ivan Sagalaev <maniac@maniac-envy.(none)>
2011-04-24 19:35:30 -0700

    Reverting XML "values" definition. The new one was too simple and allowed several values after a single attribute and was breaking Django tags.

--- src/languages/html-xml.js
+++ src/languages/html-xml.js
@@ -13,20 +13,6 @@ hljs.LANGUAGES.xml = function(){
         relevance: 0
       },
       {
-        begin: '=',
-        contains: [
-          {
-            className: 'value', begin: '"', end: '"'
-          },
-          {
-            className: 'value', begin: '\'', end: '\''
-          },
-          {
-            className: 'value', begin: '[^\\s/>]+'
-          }
-        ]
-      }
-      /*{
         begin: '="', returnBegin: true, end: '"',
         contains: [{
             className: 'value',
@@ -46,7 +32,7 @@ hljs.LANGUAGES.xml = function(){
           className: 'value',
           begin: '[^\\s/>]+'
         }]
-      }*/
+      }
     ]
   };
   return {
