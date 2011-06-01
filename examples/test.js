9dc05e754256a0b63494d72f4631b20395b0d0fc
guilin 桂林 <guileen@gmail.com>
2011-06-02 00:03:43 +0800

    add node.js port examples

---
+++ examples/test.js
@@ -0,0 +1,25 @@
+
+
+var hl = require("../highlight.js").Highlight,
+    code_string = "<?php\n"+
+                  "\techo \"Hello world!\";\n"+
+                  "\tfor($i=0;$i<100;$i++){\n"+
+                  "\t\techo \"$i\";\n"+
+                  "\t}\n"+
+                  "?>",
+
+    code_block =  "<p>PHP code:</p>\n"+
+                  "<code><?php\n"+
+                  "\techo \"Hello world!\";\n"+
+                  "\tfor($i=0;$i<100;$i++){\n"+
+                  "\t\techo \"$i\";\n"+
+                  "\t}\n"+
+                  "?></code>",
+
+    html1 = hl(code_string), // convert all
+    html2 = hl(code_string,'  '), // convert with special tab replacer
+    html3 = hl(code_block, false, true); // convert only inside <code/>
+
+console.log(html1);
+console.log(html2);
+console.log(html3);
