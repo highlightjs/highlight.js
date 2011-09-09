b3a865097e16b89d1cd0b105b9526cdd105cb9de
Jeremy Hull <sourdrums@gmail.com>
2011-09-08 20:59:25 -0400

    Clean up language/haskell source
    Didn't really need the built-in for keyword because label class
    highlights all type classes.

--- src/languages/haskell.js
+++ src/languages/haskell.js
@@ -21,8 +21,12 @@ hljs.LANGUAGES.haskell = function(){
   return {
     defaultMode: {
       keywords: {
-        'keyword': {'let': 1,'in': 1,'if': 1,'then': 1,'else': 1,'case': 1,'of': 1,'where': 1,'do': 1,'module': 1,'import': 1, 'hiding': 1,'qualified': 1,'type': 1,'data': 1,'newtype': 1,'deriving': 1,'class': 1,'instance': 1,'null': 1,'not': 1,'as': 1},
-        'built_in': {'Bool': 1,'True': 1,'False': 1,'Int': 1,'Char': 1,'Maybe': 1,'Nothing': 1,'String': 1}
+        'keyword': {
+          'let': 1, 'in': 1, 'if': 1, 'then': 1, 'else': 1, 'case': 1, 'of': 1,
+          'where': 1, 'do': 1, 'module': 1, 'import': 1, 'hiding': 1,
+          'qualified': 1, 'type': 1, 'data': 1, 'newtype': 1, 'deriving': 1,
+          'class': 1, 'instance': 1, 'null': 1, 'not': 1, 'as': 1
+        }
       },
       contains: [
         {
