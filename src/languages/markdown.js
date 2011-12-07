275d9ecaf68b8c2d535f4a95a6aaed651c7264c5
Ivan Sagalaev <maniac@softwaremaniacs.org>
2011-12-07 13:59:20 -0800

    Removed redundant anonymous function and indentation from markdown.js

--- src/languages/markdown.js
+++ src/languages/markdown.js
@@ -4,76 +4,74 @@ Author: John Crepezzi <john.crepezzi@gmail.com>
 Website: http://seejohncode.com/
 */
 
-hljs.LANGUAGES.markdown = function() {
-  return {
-    case_insensitive: true,
-    defaultMode: {
-      contains: [
-        // highlight headers
-        {
-          className: 'header',
-          begin: '^\#{1,3}', end: '$'
-        },
-        {
-          className: 'header',
-          begin: '^.+?\\n[=-]{2,}$'
-        },
-        // inline html
-        {
-          begin: '<', end: '>',
-          subLanguage: 'xml'
-        },
-        // lists (indicators only)
-        {
-          className: 'bullet',
-          begin: '^([*+-]|(\\d+\.))\\s+'
-        },
-        // strong segments
-        {
-          className: 'strong',
-          begin: '[*_]{2}.+?[*_]{2}'
-        },
-        // emphasis segments
-        {
-          className: 'emphasis',
-          begin: '[*_].+?[*_]'
-        },
-        // blockquotes
-        {
-          className: 'blockquote',
-          begin: '^>\\s+', end: '$'
-        },
-        // code snippets
-        {
-          className: 'code',
-          begin: '`.+?`'
-        },
-        {
-          className: 'code',
-          begin: '^    ', end: '$',
-          relevance: 0
-        },
-        // horizontal rules
-        {
-          className: 'horizontal_rule',
-          begin: '^-{3,}', end: '$'
-        },
-        // using links - title and link
-        {
-          begin: '\\[.+?\\]\\(.+?\\)',
-          returnBegin: true,
-          contains: [
-            {
-              className: 'link_label',
-              begin: '\\[.+\\]'
-            },
-            {
-              className: 'link_url',
-              begin: '\\(', end: '\\)'
-            }
-          ]
-        }
-      ]
-    }
-  };
-}();
+hljs.LANGUAGES.markdown = {
+  case_insensitive: true,
+  defaultMode: {
+    contains: [
+      // highlight headers
+      {
+        className: 'header',
+        begin: '^\#{1,3}', end: '$'
+      },
+      {
+        className: 'header',
+        begin: '^.+?\\n[=-]{2,}$'
+      },
+      // inline html
+      {
+        begin: '<', end: '>',
+        subLanguage: 'xml'
+      },
+      // lists (indicators only)
+      {
+        className: 'bullet',
+        begin: '^([*+-]|(\\d+\.))\\s+'
+      },
+      // strong segments
+      {
+        className: 'strong',
+        begin: '[*_]{2}.+?[*_]{2}'
+      },
+      // emphasis segments
+      {
+        className: 'emphasis',
+        begin: '[*_].+?[*_]'
+      },
+      // blockquotes
+      {
+        className: 'blockquote',
+        begin: '^>\\s+', end: '$'
+      },
+      // code snippets
+      {
+        className: 'code',
+        begin: '`.+?`'
+      },
+      {
+        className: 'code',
+        begin: '^    ', end: '$',
+        relevance: 0
+      },
+      // horizontal rules
+      {
+        className: 'horizontal_rule',
+        begin: '^-{3,}', end: '$'
+      },
+      // using links - title and link
+      {
+        begin: '\\[.+?\\]\\(.+?\\)',
+        returnBegin: true,
+        contains: [
+          {
+            className: 'link_label',
+            begin: '\\[.+\\]'
+          },
+          {
+            className: 'link_url',
+            begin: '\\(', end: '\\)'
+          }
+        ]
+      }
+    ]
+  }
+};
