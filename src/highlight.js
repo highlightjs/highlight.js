2400a49250dcec7448ad7b1c4b0ea32c37fed4d8
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-03-29 01:27:14 -0700

    API to enable line nodes

--- src/highlight.js
+++ src/highlight.js
@@ -512,7 +512,7 @@ var hljs = new function() {
   Applies highlighting to a DOM node containing code. Accepts a DOM node and
   two optional parameters for fixMarkup.
   */
-  function highlightBlock(block, tabReplace, useBR) {
+  function highlightBlock(block, tabReplace, useBR, lineNodes) {
     var text = blockText(block, useBR);
     var language = blockLanguage(block);
     var result, pre;
@@ -530,14 +530,14 @@ var hljs = new function() {
       pre.innerHTML = result.value;
       result.value = mergeStreams(original, nodeStream(pre), text);
     }
-
-    var resultPre = document.createElement('pre');
-    resultPre.innerHTML = result.value;
-    var linesPre = document.createElement('pre');
-    var lines = escape(text).replace(/^/gm, '<span class="line"></span>');
-    linesPre.innerHTML = lines;
-    result.value = mergeStreams(nodeStream(linesPre), nodeStream(resultPre), text);
-
+    if (lineNodes) {
+      var resultPre = document.createElement('pre');
+      resultPre.innerHTML = result.value;
+      var linesPre = document.createElement('pre');
+      var lines = escape(text).replace(/^/gm, '<span class="line"></span>');
+      linesPre.innerHTML = lines;
+      result.value = mergeStreams(nodeStream(linesPre), nodeStream(resultPre), text);
+    }
     result.value = fixMarkup(result.value, tabReplace, useBR);
 
     var class_name = block.className;
@@ -582,7 +582,7 @@ var hljs = new function() {
     for (var i = 0; i < pres.length; i++) {
       var code = findCode(pres[i]);
       if (code)
-        highlightBlock(code, hljs.tabReplace);
+        highlightBlock(code, hljs.tabReplace, false, hljs.lineNodes);
     }
   }
 
