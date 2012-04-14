1ea19b88fdec857618b5b9e0560d4f17d5bc5be4
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-04-13 20:27:16 -0700

    Double-apostrophe escape in Bash strings

--- src/languages/bash.js
+++ src/languages/bash.js
@@ -24,6 +24,7 @@ hljs.LANGUAGES.bash = function(){
   var APOS_STRING = {
     className: 'string',
     begin: '\'', end: '\'',
+    contains: [{begin: '\'\''}],
     relevance: 0
   };
   var TEST_CONDITION = {
