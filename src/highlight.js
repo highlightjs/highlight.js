9fa271f50be444172409b7066d81ee757474eecf
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-04-19 22:38:28 -0700

    [Issue #74] Testing beginRe against lexems in `subMode()` should succeed only at the beginning. Thanks to Joe Cheng for the report and the initial patch.

--- src/highlight.js
+++ src/highlight.js
@@ -238,7 +238,8 @@ var hljs = new function() {
 
     function subMode(lexem, mode) {
       for (var i = 0; i < mode.contains.length; i++) {
-        if (mode.contains[i].beginRe.test(lexem)) {
+        var match = mode.contains[i].beginRe.exec(lexem);
+        if (match && match.index == 0) {
           return mode.contains[i];
         }
       }
