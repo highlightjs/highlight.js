b90f7ff550462186bc1ee8a1c068b097b18a0eeb
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-05-09 01:50:08 -0700

    JSON

---
+++ src/languages/json.js
@@ -0,0 +1,44 @@
+/*
+Language: JSON
+Author: Ivan Sagalaev <maniac@softwaremaniacs.org>
+*/
+
+hljs.LANGUAGES.json = function(){
+  var LITERALS = {'literal': {'true': 1, 'false': 1, 'null': 1}};
+  var TYPES = [
+    hljs.QUOTE_STRING_MODE,
+    hljs.C_NUMBER_MODE
+  ];
+  var VALUE_CONTAINER = {
+    className: 'value',
+    end: ',', endsWithParent: true, excludeEnd: true,
+    contains: TYPES,
+    keywords: LITERALS
+  };
+  var OBJECT = {
+    begin: '{', end: '}',
+    contains: [
+      {
+        className: 'key',
+        begin: '\\s*"', end: '"\\s*:\\s*', excludeBegin: true, excludeEnd: true,
+        contains: [hljs.BACKSLASH_ESCAPE],
+        illegal: '\\n',
+        starts: VALUE_CONTAINER
+      }
+    ],
+    illegal: '\\S'
+  };
+  var ARRAY = {
+    begin: '\\[', end: '\\]',
+    contains: [hljs.inherit(VALUE_CONTAINER, {className: null})], // inherit is also a workaround for a bug that makes shared modes with endsWithParent compile only the ending of one of the parents
+    illegal: '\\S'
+  };
+  TYPES.splice(TYPES.length, 0, OBJECT, ARRAY);
+  return {
+    defaultMode: {
+      contains: TYPES,
+      keywords: LITERALS,
+      illegal: '\\S'
+    }
+  };
+}();
