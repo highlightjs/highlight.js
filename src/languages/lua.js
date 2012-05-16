a24643ceedff805254d0297eb1ffe7afad941cf0
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-05-16 15:11:22 -0700

    Converted to the new keyword syntax: lua, matlab, mel

--- src/languages/lua.js
+++ src/languages/lua.js
@@ -26,28 +26,20 @@ hljs.LANGUAGES.lua = function() {
     defaultMode: {
       lexems: hljs.UNDERSCORE_IDENT_RE,
       keywords: {
-        'keyword': {
-          'and': 1, 'break': 1, 'do': 1, 'else': 1, 'elseif': 1, 'end': 1,
-          'false': 1, 'for': 1, 'if': 1, 'in': 1, 'local': 1, 'nil': 1,
-          'not': 1, 'or': 1, 'repeat': 1, 'return': 1, 'then': 1, 'true': 1,
-          'until': 1, 'while': 1
-        },
-        'built_in': {
-          '_G': 1, '_VERSION': 1, 'assert': 1, 'collectgarbage': 1, 'dofile': 1,
-          'error': 1, 'getfenv': 1, 'getmetatable': 1, 'ipairs': 1, 'load': 1,
-          'loadfile': 1, 'loadstring': 1, 'module': 1, 'next': 1, 'pairs': 1,
-          'pcall': 1, 'print': 1, 'rawequal': 1, 'rawget': 1, 'rawset': 1,
-          'require': 1, 'select': 1, 'setfenv': 1, 'setmetatable': 1,
-          'tonumber': 1, 'tostring': 1, 'type': 1, 'unpack': 1, 'xpcall': 1,
-          'coroutine': 1, 'debug': 1, 'io': 1, 'math': 1, 'os': 1, 'package': 1,
-          'string': 1, 'table': 1
-        }
+        keyword:
+          'and break do else elseif end false for if in local nil not or repeat return then ' +
+          'true until while',
+        built_in:
+          '_G _VERSION assert collectgarbage dofile error getfenv getmetatable ipairs load ' +
+          'loadfile loadstring module next pairs pcall print rawequal rawget rawset require ' +
+          'select setfenv setmetatable tonumber tostring type unpack xpcall coroutine debug ' +
+          'io math os package string table'
       },
       contains: COMMENTS.concat([
         {
           className: 'function',
           beginWithKeyword: true, end: '\\)',
-          keywords: {'function': 1},
+          keywords: 'function',
           contains: [
             {
               className: 'title',
