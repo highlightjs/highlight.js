06d95f7d400f82c229906cdf2ca85704980b108b
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-06-13 23:40:01 -0700

    Removed useless intermediate submode from css functions.

--- src/languages/css.js
+++ src/languages/css.js
@@ -6,10 +6,7 @@ function(hljs) {
   var FUNCTION = {
     className: 'function',
     begin: hljs.IDENT_RE + '\\(', end: '\\)',
-    contains: [{
-        endsWithParent: true, excludeEnd: true,
-        contains: [hljs.NUMBER_MODE, hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE]
-    }]
+    contains: [hljs.NUMBER_MODE, hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE]
   };
   return {
     case_insensitive: true,
