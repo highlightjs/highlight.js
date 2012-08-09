c172f225c875a050969e6af7eaa276fe4f232985
Ivan Sagalaev <maniac@softwaremaniacs.org>
2012-08-09 16:08:28 -0700

    Allow dashes in settings names in .ini files

--- src/languages/ini.js
+++ src/languages/ini.js
@@ -18,7 +18,7 @@ function(hljs) {
         },
         {
           className: 'setting',
-          begin: '^[a-z0-9_\\[\\]]+[ \\t]*=[ \\t]*', end: '$',
+          begin: '^[a-z0-9\\[\\]_-]+[ \\t]*=[ \\t]*', end: '$',
           contains: [
             {
               className: 'value',
