3b4dbaf4b0092c2e47c8be316ce94be29607befb
Cédric Néhémie <cedric.nehemie@gmail.com>
2012-10-07 14:55:35 +0200

    Improve the coffeescript language support. Was added:  * regexp sub-components  * string interpolations content  * Ruby-like constants.  * begin and end match for strings, regexps, interpolations and embedded js

--- src/languages/coffeescript.js
+++ src/languages/coffeescript.js
@@ -10,9 +10,10 @@ function(hljs) {
     keyword:
       // JS keywords
       'in if for while finally new do return else break catch instanceof throw try this ' +
-      'switch continue typeof delete debugger super ' +
+      'switch continue typeof delete debugger super' +
       // Coffee keywords
-      'then unless until loop of by when and or is isnt not',
+      'then unless until loop of by when and or is isnt not extends',
+    native: 'npm require console print module exports global window document',
     literal:
       // JS literals
       'true false null undefined ' +
@@ -22,13 +23,165 @@ function(hljs) {
       '__hasProp __extends __slice __bind __indexOf'
   };
   var JS_IDENT_RE = '[A-Za-z$_][0-9A-Za-z$_]*';
+  var JS_CONSTANT_RE = '\\b[A-Z][0-9A-Za-z$_]*';
   var TITLE = {className: 'title', begin: JS_IDENT_RE};
+  var PROPERTY = {
+    className: 'property',
+    begin: '@(' + JS_IDENT_RE + ')*'
+  }
+  var CONSTANT = {
+    className: 'constant',
+    begin: JS_CONSTANT_RE
+  }
+  var OPERATORS = {
+    className: 'operators',
+    begin: '\\+|\\*|\\||/|-|&|=|&&=|==|===|!=|!==|\\|\\|=|\\|\\||&&|\\?'
+  }
+  var PUNCTUATION = {
+    className: 'punctuation',
+    begin: '\\$|\\(|:|\\)|\\{|\\}|\\[|\\]'
+  }
+  var SUBST_PUNCTUATION = {
+    className: 'punctuation',
+    begin: '\\$|\\(|:|\\)|\\[|\\]'
+  }
+  var SINGLE_QUOTE_STRING = {
+    className: 'string',
+    begin: '\'',
+    end: '\'',
+    markBegin: true,
+    markEnd: true,
+    illegal: '\\n',
+    contains: [hljs.BACKSLASH_ESCAPE],
+    relevance: 0
+  }
+  var HEREDOCS_SIMPLE = {
+    className: 'string',
+    begin: "'''",
+    end: "'''",
+    markBegin: true,
+    markEnd: true,
+    contains: [hljs.BACKSLASH_ESCAPE],
+  }
+  var EMBEDDED =  {
+    begin: '`',
+    end: '`',
+    markBegin: true,
+    markEnd: true,
+    className: 'embedded'
+  }
+  var REGEXP_CHAR_GROUP = {
+    className: 'char_group',
+    begin: '\\[',
+    end: '\\]',
+    markBegin: true,
+    markEnd: true,
+    contains: [
+      hljs.BACKSLASH_ESCAPE
+    ]
+  }
+  var REGEXP_RANGE = {
+    className: 'range',
+    begin: '\\{',
+    end: '\\}',
+    markBegin: true,
+    markEnd: true,
+    contains: [
+      hljs.C_NUMBER_MODE,
+      {
+        className: 'punctuation',
+        begin: ','
+      }
+    ]
+  }
+  var REGEXP_OPERATORS = {
+    className: 'regexp_operators',
+    begin: '\\(\\?:|\\(\\?!|\\*|\\+|\\^|\\?|\\$|\\(|\\)'
+  }
+  var REGEXP = {
+    className: 'regexp',
+    // begin: '/\\S(\\\\.|[^\\n])*/[gim]*'
+    begin: '/(?:[\\S])',
+    end: '/[gim]*\\b',
+    illegal: '\\n',
+    returnBegin: true,
+    markBegin: true,
+    markEnd: true,
+    contains: [
+      REGEXP_CHAR_GROUP,
+      hljs.BACKSLASH_ESCAPE,
+      REGEXP_RANGE,
+      REGEXP_OPERATORS
+    ]
+    // markBegin: true,
+    // markEnd: true
+  }
+  var HASH_KEY = {
+    className: 'hash',
+    begin: '@?' + JS_IDENT_RE+'\\s*:',
+    returnBegin: true,
+    end: ':',
+    markEnd: true
+  }
   var SUBST = {
     className: 'subst',
-    begin: '#\\{', end: '}',
+    begin: '#\\{',
+    end: '}',
+    markBegin: true,
+    markEnd: true,
     keywords: KEYWORDS,
-    contains: [hljs.BINARY_NUMBER_MODE, hljs.C_NUMBER_MODE]
-  };
+    contains: [
+      hljs.BINARY_NUMBER_MODE,
+      hljs.C_NUMBER_MODE,
+      SINGLE_QUOTE_STRING,
+      {
+        className: 'string',
+        begin: '"',
+        end: '"',
+        markBegin: true,
+        markEnd: true,
+        contains: [hljs.BACKSLASH_ESCAPE],
+        relevance: 0
+      },
+      REGEXP,
+      OPERATORS,
+      SUBST_PUNCTUATION,
+      CONSTANT,
+      PROPERTY
+    ]
+  }
+  var HEREDOCS_DOUBLE = {
+    className: 'heredocs',
+    begin: '"""',
+    end: '"""',
+    markBegin: true,
+    markEnd: true,
+    contains: [hljs.BACKSLASH_ESCAPE, SUBST],
+  }
+  var DOUBLE_QUOTE_STRING = {
+    className: 'string',
+    begin: '"',
+    end: '"',
+    markBegin: true,
+    markEnd: true,
+    contains: [hljs.BACKSLASH_ESCAPE, SUBST],
+    relevance: 0
+  }
+  var HEREREGEXP = {
+    className: 'hereregexp',
+    begin: '///',
+    end: '///[gim]*',
+    markBegin: true,
+    markEnd: true,
+    contains: [
+      SUBST,
+      hljs.HASH_COMMENT_MODE,
+      REGEXP_CHAR_GROUP,
+      hljs.BACKSLASH_ESCAPE,
+      REGEXP_RANGE,
+      REGEXP_OPERATORS
+    ]
+  }
 
   return {
     keywords: KEYWORDS,
@@ -37,72 +190,72 @@ function(hljs) {
       hljs.BINARY_NUMBER_MODE,
       hljs.C_NUMBER_MODE,
       // Strings
-      hljs.APOS_STRING_MODE,
-      {
-        className: 'string',
-        begin: '"""', end: '"""',
-        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
-      },
-      {
-        className: 'string',
-        begin: '"', end: '"',
-        contains: [hljs.BACKSLASH_ESCAPE, SUBST],
-        relevance: 0
-      },
+      HEREDOCS_DOUBLE,
+      HEREDOCS_SIMPLE,
+      SINGLE_QUOTE_STRING,
+      DOUBLE_QUOTE_STRING,
+      HEREREGEXP,
+      REGEXP,
+      EMBEDDED,
       // Comments
       {
         className: 'comment',
-        begin: '###', end: '###'
+        begin: '###',
+        end: '###',
+        markBegin: true,
+        markEnd: true
       },
       hljs.HASH_COMMENT_MODE,
       {
-        className: 'regexp',
-        begin: '///', end: '///',
-        contains: [hljs.HASH_COMMENT_MODE]
-      },
-      {
-        className: 'regexp', begin: '//[gim]*'
-      },
-      {
-        className: 'regexp',
-        begin: '/\\S(\\\\.|[^\\n])*/[gim]*' // \S is required to parse x / 2 / 3 as two divisions
-      },
-      {
-        begin: '`', end: '`',
-        excludeBegin: true, excludeEnd: true,
-        subLanguage: 'javascript'
-      },
-      {
         className: 'function',
-        begin: JS_IDENT_RE + '\\s*=\\s*(\\(.+\\))?\\s*[-=]>',
+        begin: '(\\([^)]+\\))?\\s*[-=]>',
         returnBegin: true,
+        end: '>',
+        keywords: KEYWORDS,
         contains: [
-          TITLE,
           {
             className: 'params',
-            begin: '\\(', end: '\\)'
+            begin: '\\(',
+            end: '\\)',
+            markBegin: true,
+            markEnd: true,
+            contains: [
+              HEREDOCS_SIMPLE,
+              HEREDOCS_DOUBLE,
+              SINGLE_QUOTE_STRING,
+              DOUBLE_QUOTE_STRING,
+              HEREREGEXP,
+              REGEXP,
+              OPERATORS,
+              CONSTANT,
+              PROPERTY
+            ]
           }
         ]
       },
       {
         className: 'class',
-        beginWithKeyword: true, keywords: 'class',
-        end: '$',
-        illegal: ':',
-        contains: [
-          {
-            beginWithKeyword: true, keywords: 'extends',
-            endsWithParent: true,
-            illegal: ':',
-            contains: [TITLE]
-          },
-          TITLE
-        ]
+        beginWithKeyword: true,
+        keywords: 'class',
+        markBegin: true,
+        markEnd: true,
+        end: '\\s*' + JS_IDENT_RE
+        // illegal: ':',
+        // contains: [
+        //   {
+        //     beginWithKeyword: true, keywords: 'extends',
+        //     endsWithParent: true,
+        //     illegal: ':',
+        //     contains: [TITLE]
+        //   },
+        //   TITLE
+        // ]
       },
-      {
-        className: 'property',
-        begin: '@' + JS_IDENT_RE
-      }
+      OPERATORS,
+      PUNCTUATION,
+      HASH_KEY,
+      CONSTANT,
+      PROPERTY
     ]
   };
 }
