daf016ee1ec67d3c0ee4619dfb8c006d42c81962
Ivan Sagalaev <maniac@softwaremaniacs.org>
2013-01-06 23:59:44 -0800

    Explicitly forbid periods after keywords to avoid detecting keywords in the access chains like `Some.class.other`

--- src/highlight.js
+++ src/highlight.js
@@ -160,7 +160,7 @@ function() {
           });
         }
 
-        mode.lexemsRe = langRe(mode.lexems || hljs.IDENT_RE, true);
+        mode.lexemsRe = langRe(mode.lexems || hljs.IDENT_RE + '(?!\\.)', true);
         if (typeof mode.keywords == 'string') { // string
           flatten('keyword', mode.keywords)
         } else {
@@ -174,7 +174,7 @@ function() {
       }
       if (parent) {
         if (mode.beginWithKeyword) {
-          mode.begin = '\\b(' + keywords.join('|') + ')\\b\\s*';
+          mode.begin = '\\b(' + keywords.join('|') + ')\\b(?!\\.)\\s*';
         }
         mode.beginRe = langRe(mode.begin ? mode.begin : '\\B|\\b');
         if (!mode.end && !mode.endsWithParent)
