9d5b49429610ab2f9552a8e9f463d8775820e36f
Kurt Emch <kurt@kurtemch.com>
2013-01-26 17:32:45 -0500

    stuck on nested syntax

--- src/languages/scss.js
+++ src/languages/scss.js
@@ -1,27 +1,8 @@
 /*
 Language: SCSS
-Requires: css.js
 Author: Kurt Emch <kurt@kurtemch.com>
 Description: SCSS syntax highlighting
 */
-// function(hljs) {
-//   var IDENT_RE = '[a-zA-Z-][a-zA-Z0-9_-]*';
-//   var FUNCTION = {
-//     className: 'function',
-//     begin: IDENT_RE + '\\(', end: '\\)',
-//     contains: ['self', hljs.NUMBER_MODE, hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE]
-//   };
-//   return {
-//     case_insensitive: true,
-//     illegal: '[=/|\']',
-//     contains: [
-//       {
-//         className: 'variable', begin: '\\$[A-Za-z0-9_-]+',
-//         relevance: 0
-//       }
-//     ]
-//   };
-// }
 function(hljs) {
   var IDENT_RE = '[a-zA-Z-][a-zA-Z0-9_-]*';
   var FUNCTION = {
@@ -31,93 +12,92 @@ function(hljs) {
   };
   return {
     case_insensitive: true,
-    illegal: '[=/|\']',
-    contains: [
-      hljs.C_LINE_COMMENT_MODE,
-      hljs.C_BLOCK_COMMENT_MODE,
-      {
-        className: 'id', begin: '\\#[A-Za-z0-9_-]+'
-      },
-      {
-        className: 'class', begin: '\\.[A-Za-z0-9_-]+',
-        relevance: 0
-      },
-      {
-        className: 'variable', begin: '\\$[A-Za-z0-9_-]+',
-        relevance: 10
-      },
-      {
-        className: 'attr_selector',
-        begin: '\\[', end: '\\]',
-        illegal: '$'
-      },
-      {
-        className: 'pseudo',
-        begin: ':(:)?[a-zA-Z0-9\\_\\-\\+\\(\\)\\"\\\']+'
-      },
-      {
-        className: 'at_rule',
-        begin: '@(font-face|page)',
-        lexems: '[a-z-]+',
-        keywords: 'font-face page'
-      },
-      {
-        className: 'at_rule',
-        begin: '@', end: '[{;]', // at_rule eating first "{" is a good thing
-                                 // because it doesn’t let it to be parsed as
-                                 // a rule set but instead drops parser into
-                                 // the default mode which is how it should be.
-        excludeEnd: true,
-        keywords: 'import page media charset',
-        contains: [
-          FUNCTION,
-          hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE,
-          hljs.NUMBER_MODE
-        ]
-      },
-      {
-        className: 'tag', begin: IDENT_RE,
-        relevance: 0
-      },
-      {
-        className: 'rules',
-        begin: '{', end: '}',
-        illegal: '[^\\s]',
-        relevance: 0,
-        contains: [
-          hljs.C_LINE_COMMENT_MODE,
-          hljs.C_BLOCK_COMMENT_MODE,
-          {
-            className: 'rule',
-            begin: '[^\\s]', returnBegin: true, end: ';', endsWithParent: true,
-            contains: [
-              {
-                className: 'attribute',
-                begin: '[A-Z\\_\\.\\-]+', end: ':',
-                excludeEnd: true,
-                illegal: '[^\\s]',
-                starts: {
-                  className: 'value',
-                  endsWithParent: true, excludeEnd: true,
-                  contains: [
-                    FUNCTION,
-                    hljs.NUMBER_MODE,
-                    hljs.QUOTE_STRING_MODE,
-                    hljs.APOS_STRING_MODE,
-                    hljs.C_BLOCK_COMMENT_MODE,
-                    {
-                      className: 'hexcolor', begin: '\\#[0-9A-F]+'
-                    },
-                    {
-                      className: 'important', begin: '!important'
-                    }
-                  ]
-                }
+    keywords:
+      'mixin include for from else if function',   
+    contains: [ {
+      className: 'id', begin: '\\#[A-Za-z0-9_-]+'
+    },
+    {
+      className: 'class', begin: '\\.[A-Za-z0-9_-]+',
+      relevance: 0
+    },    
+    {
+      className: 'attr_selector',
+      begin: '\\[', end: '\\]',
+      illegal: '$'
+    },    
+    {
+      className: 'at_rule',
+      begin: '@(mixin|include)',
+      keywords: 'mixin include',
+      relevance: 10
+    },
+    {
+      className: 'at_rule',
+      begin: '@(font-face|page)',
+      lexems: '[a-z-]+',
+      keywords: 'font-face page'
+    },
+    {
+      className: 'at_rule',
+      begin: '@', end: '[{;]',
+      excludeEnd: true,
+      keywords: 'import page media charset',
+      contains: [
+        FUNCTION,
+        hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE, hljs.NUMBER_MODE
+      ]
+    },
+    {
+      className: 'variable', begin: '\\$[A-Za-z0-9_-]+',
+      relevance: 10
+    },
+    {
+      className: 'tag', begin: IDENT_RE,
+      relevance: 0
+    },
+    {
+      className: 'a', begin: '{', end: '}',
+      relevance: 0,
+      contains: [
+        hljs.C_LINE_COMMENT_MODE,
+        hljs.C_BLOCK_COMMENT_MODE,
+        {
+          className: 'rule',
+          begin: '[^\\s]', returnBegin: true, end: ';', endsWithParent: true,
+          contains: [
+            {
+              className: 'attribute',
+              begin: '[A-Z\\_\\.\\-]+', end: ':',
+              excludeEnd: true,
+              starts: {
+                className: 'value',
+                endsWithParent: true, excludeEnd: true,
+                contains: [
+                  FUNCTION,
+                  hljs.NUMBER_MODE,
+                  hljs.QUOTE_STRING_MODE,
+                  hljs.APOS_STRING_MODE,
+                  hljs.C_BLOCK_COMMENT_MODE,
+                  {
+                    className: 'hexcolor', begin: '\\#[0-9A-F]+'
+                  },
+                  {
+                    className: 'important', begin: '!important'
+                  }
+                ]
               }
-            ]
+            }
+          ]
+        },
+        { // E4X
+            begin: '[\\.\\@\\#\\$]', end: '}',
+            subLanguage: 'css'
           }
-        ]
-      }
+      ]
+    },
+    hljs.C_LINE_COMMENT_MODE,
+    hljs.C_BLOCK_COMMENT_MODE
     ]
   };
 }
