ca64e1ac93b0a886805a215e1729584dbd6e922a
Ivan Sagalaev <maniac@softwaremaniacs.org>
2013-03-23 12:31:10 -0700

    Updates to Bash.
    - built-ins, conditional operators, $(...) in strings
    - changes intelligently derived from Benjamin Pannell's work in
      https://github.com/SPARTAN563/highlight.js/commit/42bd3da3581b74a4e3850feffdcefbd9c5166bcf

--- src/languages/bash.js
+++ src/languages/bash.js
@@ -1,58 +1,68 @@
 /*
 Language: Bash
 Author: vah <vahtenberg@gmail.com>
+Contributrors: Benjamin Pannell <contact@sierrasoftworks.com>
 */
 
 function(hljs) {
   var BASH_LITERAL = 'true false';
-  var BASH_KEYWORD = 'if then else elif fi for break continue while in do done echo exit return set declare';
+  var BASH_KEYWORD = 'if then else elif fi for break continue while in do done exit return set declare case esac export exec';
+  var BASH_BUILTIN = 'printf echo read cd pwd pushd popd dirs let eval unset typeset readonly getopts source shopt caller type hash bind help sudo';
+  var BASH_OPERATOR = '-ne -eq -lt -gt -f -d -e -s -l -a'; // relevance booster
   var VAR1 = {
-    className: 'variable', begin: '\\$[a-zA-Z0-9_#]+'
+    className: 'variable', begin: /\$[\w\d#@][\w\d_]*/
   };
   var VAR2 = {
-    className: 'variable', begin: '\\${([^}]|\\\\})+}'
+    className: 'variable', begin: /\$\{(.*?)\}/
   };
   var QUOTE_STRING = {
     className: 'string',
-    begin: '"', end: '"',
-    illegal: '\\n',
-    contains: [hljs.BACKSLASH_ESCAPE, VAR1, VAR2],
+    begin: /"/, end: /"/,
+    contains: [
+      hljs.BACKSLASH_ESCAPE,
+      VAR1,
+      VAR2,
+      {
+        className: 'title',
+        begin: /\$\(/, end: /\)/,
+        contains: hljs.BACKSLASH_ESCAPE
+      }
+    ],
     relevance: 0
   };
   var APOS_STRING = {
     className: 'string',
-    begin: '\'', end: '\'',
-    contains: [{begin: '\'\''}],
-    relevance: 0
-  };
-  var TEST_CONDITION = {
-    className: 'test_condition',
-    begin: '', end: '',
-    contains: [QUOTE_STRING, APOS_STRING, VAR1, VAR2],
-    keywords: {
-      literal: BASH_LITERAL
-    },
+    begin: /'/, end: /'/,
     relevance: 0
   };
 
   return {
+    lexems: /-?[a-z]+/,
     keywords: {
       keyword: BASH_KEYWORD,
-      literal: BASH_LITERAL
+      literal: BASH_LITERAL,
+      built_in: BASH_BUILTIN,
+      operator: BASH_OPERATOR
     },
     contains: [
       {
         className: 'shebang',
-        begin: '(#!\\/bin\\/bash)|(#!\\/bin\\/sh)',
+        begin: /^#![^\n]+sh\s*$/,
         relevance: 10
       },
-      VAR1,
-      VAR2,
+      {
+        className: 'function',
+        begin: /\w[\w\d_]*\s*\(\s*\)\s*\{/,
+        returnBegin: true,
+        contains: [{className: 'title', begin: /\w[\w\d_]*/}],
+        relevance: 0
+      },
       hljs.HASH_COMMENT_MODE,
+      hljs.NUMBER_MODE,
       QUOTE_STRING,
       APOS_STRING,
-      hljs.inherit(TEST_CONDITION, {begin: '\\[ ', end: ' \\]', relevance: 0}),
-      hljs.inherit(TEST_CONDITION, {begin: '\\[\\[ ', end: ' \\]\\]'})
+      VAR1,
+      VAR2
     ]
   };
 }
