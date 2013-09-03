bf55b340475f4cde39855f5770f098d2d1090c70
Mehdi Dogguy <mehdi@debian.org>
2013-09-04 00:13:31 +0200

    Add support for OCaml (#247)

---
+++ src/languages/ocaml.js
@@ -0,0 +1,49 @@
+/*
+Language: OCaml
+Author: Mehdi Dogguy <mehdi@dogguy.org>
+Description: OCaml language definition.
+*/
+function(hljs) {
+  return {
+    keywords: {
+      keyword:
+        'and as assert asr begin class constraint do done downto else end ' +
+        'exception external false for fun function functor if in include ' +
+        'inherit initializer land lazy let lor lsl lsr lxor match method ' +
+        'mod module mutable new object of open or private rec ref sig struct ' +
+        'then to true try type val virtual when while with parser value',
+      built_in:
+        'bool char float int list unit array exn option int32 int64 nativeint ' +
+        'format4 format6 lazy_t in_channel out_channel string',
+    },
+    contains: [
+      {
+        className: 'string',
+        begin: '"""', end: '"""'
+      },
+      {
+        className: 'comment',
+        begin: '\\(\\*', end: '\\*\\)'
+      },
+      {
+        className: 'class',
+        beginWithKeyword: true, end: '\\(|=|$',
+        keywords: 'type',
+        contains: [
+          {
+            className: 'title',
+            begin: hljs.UNDERSCORE_IDENT_RE
+          }
+        ]
+      },
+      {
+        className: 'annotation',
+        begin: '\\[<', end: '>\\]'
+      },
+      hljs.C_BLOCK_COMMENT_MODE,
+      hljs.inherit(hljs.APOS_STRING_MODE, {illegal: null}),
+      hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null}),
+      hljs.C_NUMBER_MODE
+    ]
+  }
+}
