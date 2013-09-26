3de9592f831f23702555ec4ab4c0fc48dacafca5
EricFromCanada <eric@lassosoft.com>
2013-09-26 12:08:22 -0400

    added [no_square_brackets] support
    Disables square brackets as delimiters after [no_square_brackets]. Note that [/no_square_brackets] isn't (currently) implemented in Lasso.

--- src/languages/lasso.js
+++ src/languages/lasso.js
@@ -6,61 +6,140 @@ Description: Lasso is a language and server platform for database-driven web app
 
 function(hljs) {
   var LASSO_IDENT_RE = '[a-zA-Z_][a-zA-Z0-9_.]*';
-  var LASSO_START = '<\\?(lasso(script)?|=)';
+  var LASSO_ANGLE_RE = '<\\?(lasso(script)?|=)';
+  var LASSO_CLOSE_RE = '\\]|\\?>';
+  var LASSO_KEYWORDS = {
+    literal:
+      'true false none minimal full all void and or not ' +
+      'bw nbw ew new cn ncn lt lte gt gte eq neq rx nrx ft',
+    built_in:
+      'array date decimal duration integer map pair string tag xml null ' +
+      'bytes list queue set stack staticarray tie local var variable ' +
+      'global data self inherited',
+    keyword:
+      'error_code error_msg error_pop error_push error_reset cache ' +
+      'database_names database_schemanames database_tablenames define_tag ' +
+      'define_type email_batch encode_set html_comment handle handle_error ' +
+      'header if inline iterate ljax_target link link_currentaction ' +
+      'link_currentgroup link_currentrecord link_detail link_firstgroup ' +
+      'link_firstrecord link_lastgroup link_lastrecord link_nextgroup ' +
+      'link_nextrecord link_prevgroup link_prevrecord log loop ' +
+      'namespace_using output_none portal private protect records referer ' +
+      'referrer repeating resultset rows search_args search_arguments ' +
+      'select sort_args sort_arguments thread_atomic value_list while ' +
+      'abort case else if_empty if_false if_null if_true loop_abort ' +
+      'loop_continue loop_count params params_up return return_value ' +
+      'run_children soap_definetag soap_lastrequest soap_lastresponse ' +
+      'tag_name ascending average by define descending do equals ' +
+      'frozen group handle_failure import in into join let match max ' +
+      'min on order parent protected provide public require returnhome ' +
+      'skip split_thread sum take thread to trait type where with ' +
+      'yield yieldhome'
+  };
+  var LASSO_NOPROCESS = {
+    className: 'preprocessor',
+    begin: '\\[noprocess\\]',
+    starts: {
+      className: 'markup',
+      end: '\\[/noprocess\\]',
+      returnEnd: true
+    }
+  };
+  var LASSO_START = {
+    className: 'preprocessor',
+    begin: '\\[/noprocess|' + LASSO_ANGLE_RE
+  };
+  var LASSO_CODE = [
+    hljs.C_LINE_COMMENT_MODE,
+    {
+      className: 'javadoc',
+      begin: '/\\*\\*!', end: '\\*/'
+    },
+    hljs.C_BLOCK_COMMENT_MODE,
+    hljs.inherit(hljs.C_NUMBER_MODE, {begin: hljs.C_NUMBER_RE + '|-?(infinity|nan)\\b'}),
+    hljs.inherit(hljs.APOS_STRING_MODE, {illegal: null}),
+    hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null}),
+    {
+      className: 'string',
+      begin: '`', end: '`'
+    },
+    {
+      className: 'variable',
+      begin: '#\\d+|[#$]' + LASSO_IDENT_RE
+    },
+    {
+      className: 'tag',
+      begin: '::', end: LASSO_IDENT_RE
+    },
+    {
+      className: 'attribute',
+      begin: '\\.\\.\\.|-' + hljs.UNDERSCORE_IDENT_RE
+    },
+    {
+      begin: '(->|\\.)\'', end: '\'',
+      illegal: '[\\s\\W]',
+      contains: [
+        {
+          className: 'variable',
+          begin: LASSO_IDENT_RE
+        }
+      ]
+    },
+    {
+      className: 'built_in',
+      begin: '\\.\\.?'
+    },
+    {
+      className: 'class',
+      beginWithKeyword: true, keywords: 'define',
+      excludeEnd: true, end: '\\(|=>',
+      contains: [
+        {
+          className: 'title',
+          begin: hljs.UNDERSCORE_IDENT_RE + '=?'
+        }
+      ]
+    }
+  ];
   return {
     case_insensitive: true,
     lexems: LASSO_IDENT_RE + '|&[lg]t;',
-    keywords: {
-      literal:
-        'true false none minimal full all void and or not ' +
-        'bw nbw ew new cn ncn lt lte gt gte eq neq rx nrx ft',
-      built_in:
-        'array date decimal duration integer map pair string tag xml null ' +
-        'bytes list queue set stack staticarray tie local var variable ' +
-        'global data self inherited',
-      keyword:
-        'error_code error_msg error_pop error_push error_reset cache ' +
-        'database_names database_schemanames database_tablenames define_tag ' +
-        'define_type email_batch encode_set html_comment handle handle_error ' +
-        'header if inline iterate ljax_target link link_currentaction ' +
-        'link_currentgroup link_currentrecord link_detail link_firstgroup ' +
-        'link_firstrecord link_lastgroup link_lastrecord link_nextgroup ' +
-        'link_nextrecord link_prevgroup link_prevrecord log loop ' +
-        'namespace_using output_none portal private protect records referer ' +
-        'referrer repeating resultset rows search_args search_arguments ' +
-        'select sort_args sort_arguments thread_atomic value_list while ' +
-        'abort case else if_empty if_false if_null if_true loop_abort ' +
-        'loop_continue loop_count params params_up return return_value ' +
-        'run_children soap_definetag soap_lastrequest soap_lastresponse ' +
-        'tag_name ascending average by define descending do equals ' +
-        'frozen group handle_failure import in into join let match max ' +
-        'min on order parent protected provide public require returnhome ' +
-        'skip split_thread sum take thread to trait type where with ' +
-        'yield yieldhome'
-    },
+    keywords: LASSO_KEYWORDS,
     contains: [
       {
         className: 'preprocessor',
-        begin: '\\]|\\?>',
+        begin: LASSO_CLOSE_RE,
         relevance: 0,
         starts: {
           className: 'markup',
-          end: '\\[|' + LASSO_START,
+          end: '\\[|' + LASSO_ANGLE_RE,
           returnEnd: true
         }
       },
+      LASSO_NOPROCESS,
+      LASSO_START,
       {
         className: 'preprocessor',
-        begin: '\\[noprocess\\]',
+        begin: '\\[no_square_brackets',
         starts: {
-          className: 'markup',
-          end: '\\[/noprocess\\]',
-          returnEnd: true
+          end: '\\[/no_square_brackets\\]', // not implemented in the language
+          lexems: LASSO_IDENT_RE + '|&[lg]t;',
+          keywords: LASSO_KEYWORDS,
+          contains: [
+            {
+              className: 'preprocessor',
+              begin: LASSO_CLOSE_RE,
+              relevance: 0,
+              starts: {
+                className: 'markup',
+                end: LASSO_ANGLE_RE,
+                returnEnd: true
+              }
+            },
+            LASSO_NOPROCESS,
+            LASSO_START
+          ].concat(LASSO_CODE)
         }
-      },
-      {
-        className: 'preprocessor',
-        begin: '\\[no_square_brackets|\\[/noprocess|' + LASSO_START
       },
       {
         className: 'preprocessor',
@@ -71,51 +150,7 @@ function(hljs) {
         className: 'shebang',
         begin: '^#!.+lasso9\\b',
         relevance: 10
-      },
-      hljs.C_LINE_COMMENT_MODE,
-      {
-        className: 'javadoc',
-        begin: '/\\*\\*!', end: '\\*/'
-      },
-      hljs.C_BLOCK_COMMENT_MODE,
-      hljs.inherit(hljs.C_NUMBER_MODE, {begin: hljs.C_NUMBER_RE + '|-?(infinity|nan)\\b'}),
-      hljs.inherit(hljs.APOS_STRING_MODE, {illegal: null}),
-      hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null}),
-      {
-        className: 'string',
-        begin: '`', end: '`'
-      },
-      {
-        className: 'variable',
-        begin: '#\\d+|[#$]' + LASSO_IDENT_RE
-      },
-      {
-        className: 'tag',
-        begin: '::', end: LASSO_IDENT_RE
-      },
-      {
-        className: 'attribute',
-        begin: '\\.\\.\\.|-' + hljs.UNDERSCORE_IDENT_RE
-      },
-      {
-        className: 'variable',
-        begin: '\\.\'' + LASSO_IDENT_RE + '\''
-      },
-      {
-        className: 'built_in',
-        begin: '\\.\\.?'
-      },
-      {
-        className: 'class',
-        beginWithKeyword: true, keywords: 'define',
-        excludeEnd: true, end: '\\(|=>',
-        contains: [
-          {
-            className: 'title',
-            begin: hljs.UNDERSCORE_IDENT_RE + '=?'
-          }
-        ]
       }
-    ]
+    ].concat(LASSO_CODE)
   };
 }
