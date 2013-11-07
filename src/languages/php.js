68a6f12e963494367657fc6b779a8a46de7655f4
Ivan Sagalaev <maniac@softwaremaniacs.org>
2013-11-07 01:37:55 -0800

    Removed `php_user_filter` from PHP keywords.

--- src/languages/php.js
+++ src/languages/php.js
@@ -34,7 +34,7 @@ function(hljs) {
       'array null if endwhile or const for endforeach self var while isset public ' +
       'protected exit foreach throw elseif include __FILE__ empty require_once do xor ' +
       'return implements parent clone use __CLASS__ __LINE__ else break print eval new ' +
-      'catch __METHOD__ case exception php_user_filter default die require __FUNCTION__ ' +
+      'catch __METHOD__ case exception default die require __FUNCTION__ ' +
       'enddeclare final try this switch continue endfor endif declare unset true false ' +
       'namespace trait goto instanceof insteadof __DIR__ __NAMESPACE__ ' +
       'yield finally',
