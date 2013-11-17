78f53b034e0a670b4e4737d1f85d786235271530
Jeremy Hull <sourdrums@gmail.com>
2013-11-17 02:38:44 -0700

    Change local variable classprefix to buildSpan
    A function used to build a span with or without a closing tag.

--- src/highlight.js
+++ src/highlight.js
@@ -235,7 +235,6 @@ function() {
 
   */
   function highlight(language_name, value, ignore_illegals, continuation) {
-    var classprefix = '<span class="' + hljs.classPrefix;
 
     function subMode(lexem, mode) {
       for (var i = 0; i < mode.contains.length; i++) {
@@ -262,6 +261,15 @@ function() {
     function keywordMatch(mode, match) {
       var match_str = language.case_insensitive ? match[0].toLowerCase() : match[0];
       return mode.keywords.hasOwnProperty(match_str) && mode.keywords[match_str];
+    }
+
+    function buildSpan(classname, insideSpan, leaveOpen) {
+      var openSpan  = '<span class="' + hljs.classPrefix,
+          closeSpan = leaveOpen ? '' : '</span>';
+
+      openSpan += classname + '">';
+
+      return openSpan + insideSpan + closeSpan;
     }
 
     function processKeywords() {
