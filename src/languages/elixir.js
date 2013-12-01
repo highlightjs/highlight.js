bba8941487b08c36d766e53fa9dee009c2f19ad4
Josh Adams <josh@isotope11.com>
2013-12-01 17:32:35 -0600

    Add basic Elixir support
    Copies and tweaks existing Ruby support to give decent first
    approximation of elixir syntax support.  Very minimal changes, there
    will inevitably need to be more.  Have placed a request for a more
    representative code sample, but for now it highlights my example
    perfectly as far as I can see.

---
+++ src/languages/elixir.js
@@ -0,0 +1,137 @@
+/*
+Language: Elixir
+Author: Josh Adams <josh@isotope11.com>
+Based on ruby language support
+*/
+
+function(hljs) {
+  var ELIXIR_IDENT_RE = '[a-zA-Z_][a-zA-Z0-9_]*(\\!|\\?)?';
+  var ELIXIR_METHOD_RE = '[a-zA-Z_]\\w*[!?=]?|[-+~]\\@|<<|>>|=~|===?|<=>|[<>]=?|\\*\\*|[-/+%^&*~`|]|\\[\\]=?';
+  var ELIXIR_KEYWORDS = {
+    keyword:
+      'and false then defined module in return redo if BEGIN retry end for true self when ' +
+      'next until do begin unless END rescue nil else break undef not super class case ' +
+      'require yield alias while ensure elsif or include'
+  };
+  var COMMENTS = [
+    {
+      className: 'comment',
+      begin: '#', end: '$'
+    }
+  ];
+  var SUBST = {
+    className: 'subst',
+    begin: '#\\{', end: '}',
+    lexemes: ELIXIR_IDENT_RE,
+    keywords: ELIXIR_KEYWORDS
+  };
+  var STR_CONTAINS = [hljs.BACKSLASH_ESCAPE, SUBST];
+  var STRINGS = [
+    {
+      className: 'string',
+      begin: '\'', end: '\'',
+      contains: STR_CONTAINS,
+      relevance: 0
+    },
+    {
+      className: 'string',
+      begin: '"', end: '"',
+      contains: STR_CONTAINS,
+      relevance: 0
+    }
+  ];
+  var FUNCTION = {
+    className: 'function',
+    beginWithKeyword: true, end: ' |$|;',
+    keywords: 'def',
+    contains: [
+      {
+        className: 'title',
+        begin: ELIXIR_METHOD_RE,
+        lexemes: ELIXIR_IDENT_RE,
+        keywords: ELIXIR_KEYWORDS
+      },
+      {
+        className: 'params',
+        begin: '\\(', end: '\\)',
+        lexemes: ELIXIR_IDENT_RE,
+        keywords: ELIXIR_KEYWORDS
+      }
+    ].concat(COMMENTS)
+  };
+
+  var ELIXIR_DEFAULT_CONTAINS = COMMENTS.concat(STRINGS.concat([
+    {
+      className: 'class',
+      beginWithKeyword: true, end: '$|;',
+      keywords: 'defclass defmodule',
+      contains: [
+        {
+          className: 'title',
+          begin: '[A-Za-z_]\\w*(::\\w+)*(\\?|\\!)?',
+          relevance: 0
+        },
+        {
+          className: 'inheritance',
+          begin: '<\\s*',
+          contains: [{
+            className: 'parent',
+            begin: '(' + hljs.IDENT_RE + '::)?' + hljs.IDENT_RE
+          }]
+        }
+      ].concat(COMMENTS)
+    },
+    FUNCTION,
+    {
+      className: 'constant',
+      begin: '(\\b[A-Z_]\\w*(.)?)+',
+      relevance: 0
+    },
+    {
+      className: 'symbol',
+      begin: ':',
+      contains: STRINGS.concat([{begin: ELIXIR_METHOD_RE}]),
+      relevance: 0
+    },
+    {
+      className: 'symbol',
+      begin: ELIXIR_IDENT_RE + ':',
+      relevance: 0
+    },
+    {
+      className: 'number',
+      begin: '(\\b0[0-7_]+)|(\\b0x[0-9a-fA-F_]+)|(\\b[1-9][0-9_]*(\\.[0-9_]+)?)|[0_]\\b',
+      relevance: 0
+    },
+    {
+      className: 'variable',
+      begin: '(\\$\\W)|((\\$|\\@\\@?)(\\w+))'
+    },
+    { // regexp container
+      begin: '(' + hljs.RE_STARTERS_RE + ')\\s*',
+      contains: COMMENTS.concat([
+        {
+          className: 'regexp',
+          begin: '/', end: '/[a-z]*',
+          illegal: '\\n',
+          contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+        },
+        {
+          className: 'regexp',
+          begin: '%r\\[', end: '\\][a-z]*',
+          illegal: '\\n',
+          contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+        }
+      ]),
+      relevance: 0
+    }
+  ]));
+  SUBST.contains = ELIXIR_DEFAULT_CONTAINS;
+  FUNCTION.contains[1].contains = ELIXIR_DEFAULT_CONTAINS;
+
+  return {
+    lexemes: ELIXIR_IDENT_RE,
+    keywords: ELIXIR_KEYWORDS,
+    contains: ELIXIR_DEFAULT_CONTAINS
+  };
+}
