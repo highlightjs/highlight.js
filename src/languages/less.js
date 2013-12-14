4fe7385aee2186f54ec178873cf8e468db53c440
Calvin Juarez <calvin.juarez@gmail.com>
2013-12-13 20:25:21 -0700

    [less.js] clean up comments

--- src/languages/less.js
+++ src/languages/less.js
@@ -126,61 +126,8 @@ function(hljs) {
           less.VARIABLE
         ]
       }
-    ],
-/*
-    starts: {
-      className: 'params',
-      begin: /./, end: /\)/,
-      endsWithParent: true, excludeEnd: true,
-      contains: [
-        hljs.NUMBER_MODE,
-        hljs.APOS_STRING_MODE,
-        hljs.QUOTE_STRING_MODE,
-        less.VARIABLE
-      ]
-    }
-*/
-  }
-  
-/*
-  var FUNCTION = {
-    className: 'function',
-    begin: '(escape|e|%|unit|color|data-uri|' +
-      'ceil|floor|percentage|round|sqrt|abs|sin|asin|cos|acos|tan|atan|pi|pow|mod|convert|unit|' + // math
-      'rgb|rgba|argb|hsl|hsla|hsv|hsva|hue|saturation|lightness|hsvhue|hsvsaturation|hsvvalue|' + // color
-      'red|green|blue|alpha|luma|saturate|desaturate|lighten|darken|fadein|fadeout|fade|spin|' +
-      'mix|tint|shade|greyscale|contrast|multiply|' +
-      'iscolor|isnumber|isstring|iskeyword|isurl|ispixel|ispercentage|isem|isunit)\\(', end: '\\)', // type
-    lexemes: '[a-z-\\%]+',
-    keywords: 'escape e % unit color data-uri ' + // math
-      'ceil floor percentage round sqrt abs sin asin cos acos tan atan pi pow mod convert unit ' + // color
-      'rgb rgba argb hsl hsla hsv hsva hue saturation lightness hsvhue hsvsaturation hsvvalue ' +
-      'red green blue alpha luma saturate desaturate lighten darken fadein fadeout fade spin ' +
-      'mix tint shade greyscale contrast multiply ' +
-      'iscolor isnumber isstring iskeyword isurl ispixel ispercentage isem isunit', // type
-    contains: ['self', VARIABLE, hljs.NUMBER_MODE, hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE]
-  };
-  var MIXIN = {
-    className: 'mixin',
-    begin: '(\\.|\\#)' + IDENT_RE + '\\.*\\(', end: '\\)',
-    relevance: 0,
-    contains: [
-      'self',
-      LESS,
-      FUNCTION,
-      VARIABLE,
-      hljs.NUMBER_MODE,
-      hljs.APOS_STRING_MODE,
-      hljs.QUOTE_STRING_MODE
     ]
-  };
-  var AT_RULE = {
-    className: 'at_rule',
-    begins: '@(charset|font-face|import|keyframes|media|namespace|page|region|supports|viewport)',
-    lexemes: '[a-z-]+',
-    keywords: 'charset font-face import keyframes media namespace page region supports viewport'
   }
-*/
   return {
     case_insensitive: true,
     keywords: {
@@ -264,74 +211,6 @@ function(hljs) {
         relevance: 0,
         contains: [less.VARIABLE]
       }
-/*
-      {
-        className: 'at_rule',
-        begin: '@(font-face|page)',
-        lexemes: '[a-z-]+',
-        keywords: 'font-face page'
-      },
-      {
-        className: 'at_rule',
-        begin: '@', end: '[{;]', // at_rule eating first "{" is a good thing
-                                 // because it doesn’t let it to be parsed as
-                                 // a rule set but instead drops parser into
-                                 // the default mode which is how it should be.
-        contains: [
-          {
-            className: 'keyword',
-            begin: /\S+/
-          },
-          {
-            begin: /\s/, endsWithParent: true, excludeEnd: true,
-            relevance: 0,
-            contains: [
-              FUNCTION,
-              hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE,
-              hljs.NUMBER_MODE
-            ]
-          }
-        ]
-      },
-      {
-        className: 'rules',
-        begin: '{', end: '}',
-        illegal: '[^\\s]',
-        relevance: 0,
-        contains: [
-          hljs.C_BLOCK_COMMENT_MODE,
-          {
-            className: 'rule',
-            begin: '[^\\s]', returnBegin: true, end: ';', endsWithParent: true,
-            contains: [
-              {
-                className: 'attribute',
-                begin: '[A-Z\\_\\.\\-]+', end: ':',
-                excludeEnd: true,
-                illegal: '[^\\s]',
-                starts: {
-                  className: 'value',
-                  endsWithParent: true, excludeEnd: true,
-                  contains: [
-                    FUNCTION,
-                    hljs.NUMBER_MODE,
-                    hljs.QUOTE_STRING_MODE,
-                    hljs.APOS_STRING_MODE,
-                    hljs.C_BLOCK_COMMENT_MODE,
-                    {
-                      className: 'hexcolor', begin: '#[0-9A-Fa-f]+'
-                    },
-                    {
-                      className: 'important', begin: '!important'
-                    }
-                  ]
-                }
-              }
-            ]
-          }
-        ]
-      }
-*/
     ]
   };
 }
