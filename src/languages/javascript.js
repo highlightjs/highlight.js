60ce92d51cb18529ed30e5471ec74caa47f55d7a
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-01-08 14:49:38 -0800

    Added "class" keyword to JavaScript. Fixes #353.

--- src/languages/javascript.js
+++ src/languages/javascript.js
@@ -9,7 +9,7 @@ function(hljs) {
       keyword:
         'in if for while finally var new function do return void else break catch ' +
         'instanceof with throw case default try this switch continue typeof delete ' +
-        'let yield const',
+        'let yield const class',
       literal:
         'true false null undefined NaN Infinity',
       built_in:
