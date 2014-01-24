55e481a2a27744c998fe22156a3cd4028a8dbb16
Oleg Efimov <efimovov@gmail.com>
2014-01-24 08:56:50 +0400

    Simplify protobuf function mode definitiand and show keywords+beginKeywords issue

--- src/languages/protobuf.js
+++ src/languages/protobuf.js
@@ -28,8 +28,9 @@ function(hljs) {
       },
       {
         className: 'function',
-        begin: /^\s*rpc\s+/, excludeBegin: true,
-        end: /\(/, excludeEnd: true
+        beginKeywords: 'rpc',
+        end: /;/, excludeEnd: true,
+        keywords: 'returns'
       },
       {
         className: 'constant',
