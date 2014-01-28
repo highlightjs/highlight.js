8dc0d60c3031f84677dcbb9831f7e769799b9e66
neor-ru <info@neor.ru>
2014-01-28 05:30:07 -0800

    SQL: many keywords and functions
    extended list of keywords and functions.
    MySQL 5 syntax

--- src/languages/sql.js
+++ src/languages/sql.js
@@ -9,7 +9,11 @@ function(hljs) {
     contains: [
       {
         className: 'operator',
-        begin: '\\b(begin|end|start|commit|rollback|savepoint|lock|alter|create|drop|rename|call|delete|do|handler|insert|load|replace|select|truncate|update|set|show|pragma|grant|merge)\\b(?!:)', // negative look-ahead here is specifically to prevent stomping on SmallTalk
+        begin: '\\b(begin|end|start|commit|rollback|savepoint|lock|alter|create|drop|rename|call|'+
+        		'delete|do|handler|insert|load|replace|select|truncate|update|set|show|pragma|grant|'+
+        		'merge|describe|use|explain|help|declare|prepare|execute|deallocate|savepoint|release|'+
+        		'unlock|purge|reset|change|stop|analyze|cache|flush|optimize|repair|kill|'+
+        		'install|uninstall|checksum|restore|check|backup)\\b(?!:)', // negative look-ahead here is specifically to prevent stomping on SmallTalk
         end: ';', endsWithParent: true,
         keywords: {
           keyword: 'all partial global month current_timestamp using go revoke smallint ' +
@@ -32,8 +36,43 @@ function(hljs) {
             'update catalog user sql date on identity timezone_hour natural whenever interval ' +
             'work order cascade diagnostics nchar having left call do handler load replace ' +
             'truncate start lock show pragma exists number trigger if before after each row ' +
-            'merge matched database limit',
-          aggregate: 'count sum min max avg ifnull'
+            'merge matched database limit use explain help processlist function return returns ' +
+            'concat status declare rename event index logfile server tablespace xml infile lines ' +
+            'fields columns tables table ignore partition terminated enclosed escaped identified ' +
+            'data concurrent low_priority high_priority starting delayed distinctrow rollup ' +
+            'dumpfile share mode straight_join sql_small_result sql_big_result sql_buffer_result ' +
+            'sql_cache sql_no_cache sql_calc_found_rows outfile optionally force repeat ' +
+            'consistent snapshot chain release savepoint unlock serializable binary purge reset ' +
+            'change stop binary logs master slave relaylog binlog events hosts password old_password ' +
+            'analyze cache flush optimize repair kill install uninstall plugin query ' +
+            'errors warnings checksum authors contributors des_key_file user_resources leaves ' +
+            'restore check backup no_write_to_binlog upgrade quick fast medium extended changed ' +
+            'databases engine engines grants innodb mutex profile profiles triggers variables ' +
+            'string aggregate soname between regexp',
+          aggregate: 'count avg abs acos adddate addtime aes_decrypt aes_encrypt ' +
+            'ascii asin atan2 atan benchmark bin binary bit_and bit_count bit_length bit_or bit_xor ' +
+            'case ceil ceiling char_length char character_length charset coalesce coercibility ' +
+            'collation compress concat_ws concat connection_id conv convert_tz convert cos cot ' +
+            'crc32 curdate current_date current_time current_timestamp current_user curtime ' +
+            'database date_add date_format date_sub date datediff day dayname dayofmonth dayofweek dayofyear ' +
+            'decode default degrees des_decrypt des_encrypt div elt encode encrypt exp export_set ' +
+            'extract field find_in_set floor format found_rows from_base64 from_days from_unixtime ' +
+            'get_format get_lock greatest group_concat gtid_subset gtid_subtract hex hour if ' +
+            'ifnull in inet_aton inet_ntoa inet6_aton inet6_ntoa insert instr interval ' +
+            'is_free_lock is_ipv4_compat is_ipv4_compat is_ipv4_mapped is_ipv4 is_not_null is_not ' +
+            'is_used_lock is isnull last_day last_insert_id lcase least left lenght like ln ' +
+            'load_file localtime localtimestamp locate log10 log2 log lower lpad ltrim make_set ' +
+            'makedate maketime master_pos_wait match max md5 microsecond mid min minute mod ' +
+            'month monthname name_const now nullif oct octet_length old_password ord password ' +
+            'period_add period_diff pi position pow power procedure_analyze quarter quote radians ' +
+            'rand release_lock repeat replace reverse right rlike round row_count rpad rtrim ' +
+            'schema sec_to_time second session_user sha sha1 sha2 sign sin sleep soundex sounds_like '+
+            'space sqrt std stddev_pop stddev_samp stddev str_to_date strcmp subdate substr subtring_index '+
+            'substring subtime sum sysdate system_user tan time_format time_to_sec time timediff timestamp '+
+            'timestampadd timestampdiff to_base64 to_days to_seconds trim truncate ucase uncompress '+
+            'uncompressed_length unhex unix_timestamp upped user utc_date utc_time utc_timestamp '+
+            'uuid_short uuid validate_password_strength values var_pop var_samp variance version '+
+            'week weekday weekofyear weight_string xor year yearweek'
         },
         contains: [
           {
