aadf24b9a91c81bc7bfb4b28659d5afa61dbf30b
Matteo Figus <matteofigus@gmail.com>
2014-03-09 23:35:11 +0000

    Gherkin definition

--- src/languages/gherkin.js
+++ src/languages/gherkin.js
@@ -5,34 +5,34 @@
  */
 
 function (hljs) {
-    return {
-        keywords: 'Feature Background Ability Business\ Need Scenario Scenarios Scenario\ Outline Scenario\ Template Examples Given And Then But When',
-        contains: [
-            {
-                className: 'keyword',
-                begin: '\\*'
-            },
-            {
-                className: 'comment',
-                begin: '@[^@\r\n\t ]+', end: '$'
-            },
-            {
-                className: 'string',
-                begin: '\\|', end: '\\$',
-                relevance: 10
-            },
-            {
-                className: 'variable',
-                begin: '<', end: '>',
-            },
-            hljs.HASH_COMMENT_MODE,
-            {
-                className: 'string',
-                begin: '"""', end: '"""',
-                relevance: 0
-            },
-            hljs.APOS_STRING_MODE,
-            hljs.QUOTE_STRING_MODE
-        ]
-    }
+  return {
+    keywords: 'Feature Background Ability Business\ Need Scenario Scenarios Scenario\ Outline Scenario\ Template Examples Given And Then But When',
+    contains: [
+      {
+        className: 'keyword',
+        begin: '\\*'
+      },
+      {
+        className: 'comment',
+        begin: '@[^@\r\n\t ]+', end: '$'
+      },
+      {
+        className: 'string',
+        begin: '\\|', end: '\\$',
+        relevance: 10
+      },
+      {
+        className: 'variable',
+        begin: '<', end: '>',
+      },
+      hljs.HASH_COMMENT_MODE,
+      {
+        className: 'string',
+        begin: '"""', end: '"""',
+        relevance: 0
+      },
+      hljs.APOS_STRING_MODE,
+      hljs.QUOTE_STRING_MODE
+    ]
+  };
 }
