48f9a430b7141f1efbdb1c9e250918fc7380ce6f
Erik Osheim <d_m@plastic-idolatry.com>
2014-07-21 16:15:30 -0400

    Restructuring scala.js based around @isagalaev's comments.
    This commit removes a lot of the complex structure, specifically around
    parameter lists and class definitions -- which were not totally correct
    and introcued other problems.
    
    It also adds support for unicode method/type names when necessary, and
    correctly tags type names and type arguments with the 'type' className.

--- src/languages/scala.js
+++ src/languages/scala.js
@@ -21,82 +21,55 @@ function(hljs) {
     begin: '\'\\w[\\w\\d_]*(?!\')'
   };
 
-  /* TODO: would be nice to use a better className that 'literal'
-   * but 'type' etc don't seem to work
-   */
   var TYPE = {
-    className: 'literal',
-    begin: '[A-Z][A-Za-z0-9_]*',
-    relevantce: 0
+    className: 'type',
+    begin: '\\b[A-Z][A-Za-z0-9_]*'
   };
 
-  var TPARAMS = {
-    className: 'params',
-    begin: /\[/, end: /\]/,
-    contains: [STRING, TYPE]
-  };
-
-  var PARAMS = {
-    className: 'params',
-    begin: /\(/, end: /\)/,
-    contains: [STRING, TYPE]
-  };
-
-  var FUNC_CLASS_PROTO = {
-    end: /[:={]|extends/,
-    illegal: /[${=;\n]/,
-    contains: [hljs.UNDERSCORE_TITLE_MODE, PARAMS, TPARAMS]
-  };
-  var KEYW = {
-    className: 'keyword',
-    begin: '(type|yield|lazy|override|def|with|val|var|false|true|sealed|abstract|private|trait|object|null|if|forSome|for|while|throw|finally|protected|extends|import|final|return|else|break|new|catch|super|class|case|package|default|try|this|match|continue|throws)(?![A-Za-z0-9_])'
-  };
-  var BAREWORD = {
-    className: 'xyz',
-    begin: '[a-z_][A-Za-z0-9_]*',
+  var NAME = {
+    className: 'title',
+    begin: /[^0-9\n\t "'(),.`{}\[\]:][^\n\t "'(),.`{}\[\]:]+|[^0-9\n\t "'(),.`{}\[\]:=]/,
     relevance: 0
+  }
+
+  var CLASS = {
+    className: 'class',
+    beginKeywords: 'class object trait type',
+    end: /[:={\[(\n;]/,
+    contains: [{className: 'keyword', beginKeywords: 'extends with', relevance: 10}, NAME]
   };
+
+  var METHOD = {
+    className: 'function',
+    beginKeywords: 'def',
+    end: /[:={\[(\n;]/,
+    contains: [NAME]
+  };
+
+  var JAVADOC = {
+    className: 'javadoc',
+    begin: '/\\*\\*', end: '\\*/',
+    contains: [{
+      className: 'javadoctag',
+      begin: '@[A-Za-z]+'
+    }],
+    relevance: 10
+  };
+
   return {
+    keywords: {
+      literal: 'true false null',
+      keyword: 'type yield lazy override def with val var sealed abstract private trait object if forSome for while throw finally protected extends import final return else break new catch super class case package default try this match continue throws'
+    },
     contains: [
-      {
-        className: 'javadoc',
-        begin: '/\\*\\*', end: '\\*/',
-        contains: [{
-          className: 'javadoctag',
-          begin: '@[A-Za-z]+'
-        }],
-        relevance: 10
-      },
-      hljs.C_LINE_COMMENT_MODE, hljs.C_BLOCK_COMMENT_MODE,
-      STRING, hljs.QUOTE_STRING_MODE,
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.C_BLOCK_COMMENT_MODE,
+      STRING,
+      hljs.QUOTE_STRING_MODE,
       SYMBOL,
       TYPE,
-      hljs.inherit(FUNC_CLASS_PROTO, {className: 'function', beginKeywords: 'def'}),
-      hljs.inherit(FUNC_CLASS_PROTO, {className: 'class', beginKeywords: 'class'}),
-      KEYW,
-      BAREWORD,
-      {
-        className: 'class',
-        begin: '((case )?class |object |trait )', // beginKeywords won't work because a single "case" shouldn't start this mode
-        end: '({|$)', excludeEnd: true,
-        illegal: ':',
-        keywords: 'case class trait object',
-        contains: [
-          {
-            beginKeywords: 'extends with',
-            relevance: 10
-          },
-          hljs.UNDERSCORE_TITLE_MODE,
-          {
-            className: 'params',
-            begin: '\\(', end: '\\)',
-            contains: [
-              hljs.QUOTE_STRING_MODE, STRING,
-              ANNOTATION
-            ]
-          }
-        ]
-      },
+      METHOD,
+      CLASS,
       hljs.C_NUMBER_MODE,
       ANNOTATION
     ]
