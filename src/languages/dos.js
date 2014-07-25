54dbe2106cdd3fb1ab49e7a1772ea15fcd51b258
Anton Kochkov <anton.kochkov@gmail.com>
2014-07-25 17:06:52 +0400

    Added labels, functions for CMD/BAT files

--- src/languages/dos.js
+++ src/languages/dos.js
@@ -4,14 +4,30 @@ Author: Alexander Makarov (http://rmcreative.ru/)
 */
 
 function(hljs) {
+  var COMMENTS = {
+    className: 'comment',
+    begin: '@?rem', end: '$',
+	  relevance: 10
+  };
+  var LABEL = {
+	  className: 'label',
+	  begin: '^\\s*[A-Za-z._?][A-Za-z0-9_$#@~.?]*(:|\\s+label)',
+	  relevance: 0
+  };
   return {
     aliases: ['bat', 'cmd'],
     case_insensitive: true,
     keywords: {
-      flow: 'if else goto for in do call exit not exist errorlevel defined equ neq lss leq gtr geq',
+      flow: 'if else goto for in do call exit not exist errorlevel defined',
+      operator: 'equ neq lss leq gtr geq',
       keyword: 'shift cd dir echo setlocal endlocal set pause copy',
       stream: 'prn nul lpt3 lpt2 lpt1 con com4 com3 com2 com1 aux',
-      winutils: 'ping net ipconfig taskkill xcopy ren del'
+      winutils: 'ping net ipconfig taskkill xcopy ren del',
+      built_in: 'append assoc at attrib break cacls cd chcp chdir chkdsk|10 chkntfs cls cmd color ' +
+        'comp compact convert date dir diskcomp diskcopy|10 doskey erase fs ' +
+        'find findstr|10 format ftype graftabl help keyb label md mkdir mode more move path ' +
+        'pause print popd pushd promt rd recover rem rename replace restore rmdir shift' +
+        'sort start subst time title tree type ver verify vol',
     },
     contains: [
       {
@@ -24,13 +40,19 @@ function(hljs) {
         className: 'envvar', begin: '![^ ]+?!'
       },
       {
+        className: 'function',
+        begin: LABEL.begin, end: 'goto:eof',
+        contains: [
+          hljs.inherit(hljs.TITLE_MODE, {begin: '([_a-zA-Z]\\w*\\.)*([_a-zA-Z]\\w*:)?[_a-zA-Z]\\w*'}),
+          COMMENTS
+        ].concat(COMMENTS),
+        relevance: 10
+      },
+      {
         className: 'number', begin: '\\b\\d+',
         relevance: 0
       },
-      {
-        className: 'comment',
-        begin: '@?rem', end: '$'
-      }
+      COMMENTS
     ]
   };
 }
