e528af1b77bf884dda23cbf8eb48aedb43053071
Jeremy Hull <sourdrums@gmail.com>
2014-08-07 18:18:04 -0700

    Add utility module for test suite

---
+++ test/utility.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var fs   = require('fs');
+var path = require('path');
+
+// Build a path relative to `test/`
+function buildPath() {
+  var args  = [].slice.call(arguments, 0),
+      paths = [__dirname].concat(args);
+
+  return path.join.apply(this, paths);
+};
+exports.buildPath = buildPath;
+
+exports.languagesList = function() {
+  var langPath  = buildPath('language'),
+      languages = fs.readdirSync(langPath);
+
+  return languages.map(function(language) {
+    return language.split('.')[0];
+  });
+};
