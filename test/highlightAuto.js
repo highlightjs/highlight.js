f5ef29c72b2c531d20097d6c080dc525e6ed0909
Jeremy Hull <sourdrums@gmail.com>
2014-08-08 22:21:53 -0700

    Pull out function for repeated autodetection tests

--- test/highlightAuto.js
+++ test/highlightAuto.js
@@ -4,27 +4,23 @@ var fs      = require('fs');
 var hljs    = require('../build');
 var utility = require('./utility');
 
+function testAutoDetection(language) {
+  it('should be detected as ' + language, function() {
+    var filename     = language + '.txt',
+        languagePath = utility.buildPath('language', filename),
+        content      = fs.readFileSync(languagePath, 'utf-8'),
+
+        expected = language,
+        actual   = hljs.highlightAuto(content).language;
+
+    actual.should.equal(expected);
+  });
+}
+
 describe('hljs', function() {
   describe('.highlightAuto', function() {
-    var language, filename,
+    var languages = utility.languagesList();
 
-        languages = utility.languagesList(),
-        length    = languages.length;
-
-    for(var i = 0; i < length; i++) {
-      language = languages[i];
-      filename = language + '.txt';
-
-      it('should be detected as ' + language, function() {
-        var languagePath = utility.buildPath('language', filename),
-
-            expected     = language,
-            content      = fs.readFileSync(languagePath, 'utf-8'),
-
-            actual       = hljs.highlightAuto(content).language;
-
-        actual.should.equal(expected);
-      });
-    }
+    languages.forEach(testAutoDetection);
   });
 });
