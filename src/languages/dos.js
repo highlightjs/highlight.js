685e07fe49a6cc92ca998cb953502e3c2ae1caa7
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-08-10 15:36:54 -0700

    Reset keyword relevance in DOS.

--- src/languages/dos.js
+++ src/languages/dos.js
@@ -24,9 +24,9 @@ function(hljs) {
       keyword: 'shift cd dir echo setlocal endlocal set pause copy',
       stream: 'prn nul lpt3 lpt2 lpt1 con com4 com3 com2 com1 aux',
       winutils: 'ping net ipconfig taskkill xcopy ren del',
-      built_in: 'append assoc at attrib break cacls cd chcp chdir chkdsk|10 chkntfs cls cmd color ' +
-        'comp compact convert date dir diskcomp diskcopy|10 doskey erase fs ' +
-        'find findstr|10 format ftype graftabl help keyb label md mkdir mode more move path ' +
+      built_in: 'append assoc at attrib break cacls cd chcp chdir chkdsk chkntfs cls cmd color ' +
+        'comp compact convert date dir diskcomp diskcopy doskey erase fs ' +
+        'find findstr format ftype graftabl help keyb label md mkdir mode more move path ' +
         'pause print popd pushd promt rd recover rem rename replace restore rmdir shift' +
         'sort start subst time title tree type ver verify vol',
     },
@@ -46,8 +46,7 @@ function(hljs) {
         contains: [
           hljs.inherit(hljs.TITLE_MODE, {begin: '([_a-zA-Z]\\w*\\.)*([_a-zA-Z]\\w*:)?[_a-zA-Z]\\w*'}),
           COMMENT
-        ],
-        relevance: 10
+        ]
       },
       {
         className: 'number', begin: '\\b\\d+',
