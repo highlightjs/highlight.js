2b7129bcad0de8f6081f17a427aaaad61d754da6
Kassio Borges <kassioborgesm@gmail.com>
2014-08-14 20:56:38 -0300

    Fix erb syntax.

--- src/languages/erb.js
+++ src/languages/erb.js
@@ -1,18 +1,21 @@
 /*
-Language: ERB (Embedded Ruby)
-Requires: xml.js, ruby.js
-Author: Lucas Mazza <lucastmazza@gmail.com>
-*/
+ Language: ERB (Embedded Ruby)
+ Requires: xml.js, ruby.js
+ Authors: Lucas Mazza <lucastmazza@gmail.com>
+          Kassio Borges <kassioborgesm@gmail.com>
+ */
 function(hljs) {
   return {
     aliases: ['erb'],
     case_insensitive: true,
-    subLanguage: 'xml', subLanguageMode: 'continuous',
     contains: [
       {
-        begin: '<%[%=-]?', end: '[%-]?%>',
-        subLanguage: 'ruby', subLanguageMode: 'continuous'
-      },
+        relevance: 10,
+        begin: '<%[%#=-]?', end: '[%-]?%>',
+        subLanguage: 'ruby',
+        excludeBegin: true,
+        excludeEnd: true
+      }
     ]
   };
 }
