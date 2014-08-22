0df8ce576cf56d340f39adc8001d3f3dc48b3346
Jeremy Hull <sourdrums@gmail.com>
2014-08-21 17:48:01 -0700

    Add browser build type
    This is used for browser, amd, and part of the cdn build.

---
+++ tools/browser.js
@@ -0,0 +1,82 @@
+'use strict';
+
+var _    = require('lodash');
+var path = require('path');
+
+var utility  = require('./utility');
+
+module.exports = function(commander) {
+  var amdArgs, hljsExt, output, requiresTask, tasks,
+      replace           = utility.replace,
+      regex             = utility.regex,
+      replaceClassNames = utility.replaceClassNames,
+
+      readArgs     = utility.languagesGlob(commander.args),
+      replaceArgs  = replace(regex.header, ''),
+      templateArgs = { _: 'hljs.registerLanguage(\'<%= name %>\',' +
+                          ' <%= content %>);\n'
+                     , highlight: 'var hljs = new <%= content %>();\n'
+                     };
+
+  tasks = {
+    startlog: { task: ['log', 'Building highlight.js pack file.'] },
+    read: { requires: 'startlog', task: ['glob', readArgs] },
+    reorder: { requires: 'read', task: 'reorderDeps' },
+    replace: { requires: 'reorder', task: ['replace', replaceArgs] },
+    template: { requires: 'replace', task: ['template', templateArgs] },
+    concat: { requires: 'template', task: 'concat' }
+  };
+  requiresTask = 'concat';
+
+  if(commander.target === 'amd') {
+    tasks.amdlog = {
+      requires: requiresTask,
+      task: ['log', 'Adding AMD wrapper.']
+    };
+
+    amdArgs = 'define(function() {\n<%= content %>\nreturn hljs;\n});';
+    tasks.amd = { requires: 'amdlog', task: ['template', amdArgs] };
+    requiresTask = 'amd';
+  }
+
+  if(commander.compress || commander.target === 'cdn') {
+    tasks.compresslog = {
+      requires: requiresTask,
+      task: ['log', 'Compressing highlight.js pack file.']
+    };
+
+    tasks.replace2 = {
+      requires: 'compresslog',
+      task: [ 'replaceSkippingStrings'
+            , replace(regex.replaces, replaceClassNames)
+            ]
+    };
+
+    tasks.replace3 = {
+      requires: 'replace2',
+      task: ['replace', replace(regex.classname, '$1.className')]
+    };
+
+    tasks.minify  = { requires: 'replace3', task: 'jsminify' };
+    requiresTask  = 'minify';
+  }
+
+  tasks.writelog = {
+    requires: requiresTask,
+    task: ['log', 'Writing highlight.js pack file.']
+  };
+
+  hljsExt = commander.target === 'cdn' ? 'min' : 'pack';
+  output  = path.join(dir.build, 'highlight.' + hljsExt + '.js');
+
+  tasks.write = {
+    requires: 'writelog',
+    task: ['write', output]
+  };
+
+  if(commander.target === 'browser') {
+    tasks = _.merge(utility.copyDocs(), tasks);
+  }
+
+  return tasks;
+};
