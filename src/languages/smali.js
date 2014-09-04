31bf12751ae2532121701ff0ce19ba1fc1dab6c8
titze <dennis.titze@gmail.com>
2014-09-04 11:00:17 +0200

    autohighlight corrected

--- src/languages/smali.js
+++ src/languages/smali.js
@@ -5,59 +5,81 @@ Description: Basic Smali highlighting
 */
 
 function(hljs) {
-  var instruction = ['add', 'aget', 'and', 'aput', 'array', 'check', 'cmp', 'cmpg', 'cmpl', 'const', 'div', 'double', 'execute', 'fill', 'filled', 'float', 'goto', 'goto/16', 'goto/32', 'if', 'iget', 'instance', 'int', 'invoke', 'iput', 'long', 'monitor', 'move', 'mul', 'neg', 'new', 'nop', 'not', 'or', 'packed', 'rem', 'return', 'sget', 'shl', 'shr', 'sparse', 'sput', 'sub', 'throw', 'ushr', 'xor'];
-  var keywords = ['transient', 'constructor', 'abstract', 'final', 'synthetic', 'public', 'private', 'protected', 'static', 'bridge', 'system']
+  var smali_instr_low_prio = ['add', 'and', 'cmp', 'cmpg', 'cmpl', 'const', 'div', 'double', 'float', 'goto', 'if', 'int', 'long', 'move', 'mul', 'neg', 'new', 'nop', 'not', 'or', 'rem', 'return', 'shl', 'shr', 'sput', 'sub', 'throw', 'ushr', 'xor'];
+  var smali_instr_high_prio = ['aget', 'aput', 'array', 'check', 'execute', 'fill', 'filled', 'goto/16', 'goto/32', 'iget', 'instance', 'invoke', 'iput', 'monitor', 'packed', 'sget', 'sparse'];
+  var smali_keywords = ['transient', 'constructor', 'abstract', 'final', 'synthetic', 'public', 'private', 'protected', 'static', 'bridge', 'system'];
   return {
     aliases: ['smali'],
     contains: [
       {
         className: 'string',
         begin: '"', end: '"',
-        relevance: 10
+        relevance: 0
       },
       {
         className: 'comment',
         begin: '#', end: '$',
+        relevance: 0
       },
       {
         className: 'keyword',
         begin: '\\s*\\.end\\s[a-zA-Z0-9]*',
+        relevance: 1
       },
       {
         className: 'keyword',
         begin: '^[ ]*\\.[a-zA-Z]*',
+        relevance: 0
       },
       {
         className: 'keyword',
         begin: '\\s:[a-zA-Z_0-9]*',
+        relevance: 0
       },
       {
         className: 'keyword',
-        begin: '\\s('+keywords.join('|')+')',
+        begin: '\\s('+smali_keywords.join('|')+')',
+        relevance: 1
       },
       {
         className: 'keyword',
         begin: '\\[',
+        relevance: 0
       },
       {
         className: 'instruction',
-        begin: '\\s('+instruction.join('|')+')((\-|/)[a-zA-Z0-9]+)*\\s',
+        begin: '\\s('+smali_instr_low_prio.join('|')+')\\s',
+        relevance: 1
+      },
+      {
+        className: 'instruction',
+        begin: '\\s('+smali_instr_low_prio.join('|')+')((\\-|/)[a-zA-Z0-9]+)+\\s',
+        relevance: 10
+      },
+      {
+        className: 'instruction',
+        begin: '\\s('+smali_instr_high_prio.join('|')+')((\\-|/)[a-zA-Z0-9]+)*\\s',
+        relevance: 10
       },
       {
         className: 'class',
         begin: 'L[^\(;:\n]*;',
+        relevance: 0
       },
       {
         className: 'function',
         begin: '( |->)[^(\n ;"]*\\(',
+        relevance: 0
       },
       {
         className: 'function',
         begin: '\\)',
+        relevance: 0
       },
       {
         className: 'variable',
-        begin: '[vp][0-9]+'
+        begin: '[vp][0-9]+',
+        relevance: 0
       }
     ]
   };
