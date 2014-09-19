7aca33422b7d35b8cc394605ed9eb932b16cde2a
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-09-19 12:01:30 -0700

    Don't recognize words after -> in PHP as variables.
    Fixes #567. See the discussion in the ticket.

--- src/languages/php.js
+++ src/languages/php.js
@@ -6,7 +6,7 @@ Contributors: Evgeny Stepanischev <imbolk@gmail.com>, Ivan Sagalaev <maniac@soft
 
 function(hljs) {
   var VARIABLE = {
-    className: 'variable', begin: '(\\$|->)+[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*'
+    className: 'variable', begin: '\\$+[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*'
   };
   var PREPROCESSOR = {
     className: 'preprocessor', begin: /<\?(php)?|\?>/
@@ -64,6 +64,10 @@ function(hljs) {
       },
       PREPROCESSOR,
       VARIABLE,
+      {
+        // swallow class members to avoid parsing them as keywords
+        begin: /->+[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/
+      },
       {
         className: 'function',
         beginKeywords: 'function', end: /[;{]/, excludeEnd: true,
