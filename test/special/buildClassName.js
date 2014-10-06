941e3d269f92d0a65c3fabcdd9020c4b97887fd6
Oleg Efimov <efimovov@gmail.com>
2014-10-06 23:44:22 +0400

    More test cases for buildClassName() as suggested by @isagalaev

--- test/special/buildClassName.js
+++ test/special/buildClassName.js
@@ -3,25 +3,34 @@
 var fs      = require('fs');
 var utility = require('../utility');
 
-var block_with_hljs_class, block_without_hljs_class;
-
 describe('block class names', function() {
-  before(function() {
-    block_with_hljs_class    = document.getElementById('with-hljs-class');
-    block_without_hljs_class = document.getElementById('without-hljs-class');
-  });
-
   it('should add language class name to block', function() {
-    var expected = 'hljs xml',
-        actual   = block_without_hljs_class.className;
+    var expected = 'some-class hljs xml',
+        actual   = document.getElementById('without-hljs-class').className;
 
     actual.should.equal(expected);
   });
 
-  it('do not clutter block class name', function() {
-    var expected = 'hljs xml',
-      actual   = block_with_hljs_class.className;
+  describe('do not clutter block class name', function() {
+    it('first', function () {
+      var expected = 'hljs some-class xml',
+        actual = document.getElementById('with-hljs-class-first').className;
 
-    actual.should.equal(expected);
+      actual.should.equal(expected);
+    });
+
+    it('last', function () {
+      var expected = 'some-class hljs xml',
+        actual = document.getElementById('with-hljs-class-last').className;
+
+      actual.should.equal(expected);
+    });
+
+    it('spaces around', function () {
+      var expected = 'hljs some-class xml',
+        actual = document.getElementById('with-hljs-class-spaces-around').className;
+
+      actual.should.equal(expected);
+    });
   });
 });
