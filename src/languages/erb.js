421213a1472a2506f8f8e0014d115979b3aa05c9
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-10-09 00:01:21 -0700

    Resolving ERB/VBScript incompatibility.
    - VBScript is no longer unconditionally bound two <% %> in xml.js
    - We now have two bridge languages mixing Ruby and VBScript
      respectively in HTML. Those compete on usual detection terms.

--- src/languages/erb.js
+++ src/languages/erb.js
@@ -3,17 +3,19 @@ Language: ERB (Embedded Ruby)
 Requires: xml.js, ruby.js
 Author: Lucas Mazza <lucastmazza@gmail.com>
 Contributors: Kassio Borges <kassioborgesm@gmail.com>
+Description: "Bridge" language defining fragments of Ruby in HTML within <% .. %>
 */
 
 function(hljs) {
   return {
-    aliases: ['erb'],
-    case_insensitive: true,
     subLanguage: 'xml', subLanguageMode: 'continuous',
     contains: [
       {
-        relevance: 10,
-        begin: '<%[%#=-]?', end: '[%-]?%>',
+        className: 'comment',
+        begin: '<%#', end: '%>',
+      },
+      {
+        begin: '<%[%=-]?', end: '[%-]?%>',
         subLanguage: 'ruby',
         excludeBegin: true,
         excludeEnd: true
