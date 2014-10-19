2366bb9d9853fcc4f80e4b7bb761bbb6087db12b
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-10-18 22:17:04 -0700

    Illegalize "|" in Delphi.

--- src/languages/delphi.js
+++ src/languages/delphi.js
@@ -50,7 +50,7 @@ function(hljs) {
   return {
     case_insensitive: true,
     keywords: KEYWORDS,
-    illegal: /("|\$[G-Zg-z]|\/\*|<\/)/,
+    illegal: /"|\$[G-Zg-z]|\/\*|<\/|\|/,
     contains: [
       COMMENT, hljs.C_LINE_COMMENT_MODE,
       STRING, CHAR_STRING,
