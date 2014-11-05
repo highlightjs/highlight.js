9e98b5cefcfafe9acd76b6057b7903da9b1bc248
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-11-05 15:29:37 -0800

    Draft implementation of category switching and on-demand highlighting.

--- demo/detection.js
+++ demo/detection.js
@@ -1,12 +1,42 @@
+function initHighlighting(div) {
+  var code = div.find('code');
+  if (code.hasClass('hljs')) {
+    return;
+  }
+  code = code.get(0);
+  hljs.highlightBlock(code);
+  if (!div.hasClass(code.result.language)) {
+    div.addClass('fail');
+  }
+}
+
 $(document).ready(function() {
-  $('#autotest div').each(function(i, div) {
-    div = $(div);
-    if (hljs.getLanguage(div.attr('class'))) {
-      div.show();
-    }
-    var language = div.find('code').get(0).result.language;
-    if (!div.hasClass(language)) {
-      div.addClass('fail');
+
+  var categories = {};
+  $('#languages div').each(function(i, div) {
+    var category_str = $(div).data('category');
+    if (category_str) {
+      category_str.split(' ').forEach(function(c) {
+        categories[c] = true;
+      });
     }
   });
+  var ul = $('#categories');
+  Object.keys(categories).forEach(function(c) {
+    ul.append('<li><a href="#">' + c + '</a></li>');
+  });
+  $('#categories li a').click(function(e) {
+    e.preventDefault();
+    var category = $(this).text();
+    $('#languages div').each(function(i, div) {
+      div = $(div);
+      var category_str = div.data('category');
+      if (category_str && category_str.split(' ').indexOf(category) != -1) {
+        initHighlighting(div);
+        div.show();
+      } else {
+        div.hide();
+      }
+    });
+  })
 });
