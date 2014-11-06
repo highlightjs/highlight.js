e93c7c8003bb6fc36c42aec9bbd82505f7542dff
Ivan Sagalaev <maniac@softwaremaniacs.org>
2014-11-05 16:50:31 -0800

    'other' category.

--- demo/demo.js
+++ demo/demo.js
@@ -14,23 +14,25 @@ function initCategories() {
   var categories = {};
   $('#languages div').each(function(i, div) {
     var category_str = $(div).data('category');
-    if (category_str) {
-      category_str.split(' ').forEach(function(c) {
-        categories[c] = true;
-      });
+    if (!category_str) {
+      category_str = 'other';
+      $(div).data('category', category_str);
     }
+    category_str.split(' ').forEach(function(c) {
+      categories[c] = (categories[c] || 0) + 1;
+    });
   });
   var ul = $('#categories');
   Object.keys(categories).forEach(function(c) {
-    ul.append('<li><a href="#">' + c + '</a></li>');
+    ul.append('<li><a href="#" data-category="' + c + '">' + c + ' (' + categories[c] +')</a></li>');
   });
   $('#categories li a').click(function(e) {
     e.preventDefault();
-    var category = $(this).text();
+    var category = $(this).data('category');
     $('#languages div').each(function(i, div) {
       div = $(div);
       var category_str = div.data('category');
-      if (category_str && category_str.split(' ').indexOf(category) != -1) {
+      if (category_str.split(' ').indexOf(category) != -1) {
         highlightDiv(div);
         div.show();
       } else {
