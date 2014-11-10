bbe604cb2c11c6e7f1e5d91508bc9cdce6e92ab6
nwin <nwin@users.noreply.github.com>
2014-11-10 08:41:14 +0100

    Make block comment highlighting more future proof.

--- src/languages/rust.js
+++ src/languages/rust.js
@@ -5,14 +5,8 @@ Contributors: Roman Shmatov <romanshmatov@gmail.com>
 */
 
 function(hljs) {
-  var RUST_BLOCK_COMMENT_MODE = {
-    className: 'comment',
-    begin: '/\\*', end: '\\*/',
-  };
-  RUST_BLOCK_COMMENT_MODE.contains = [
-    RUST_BLOCK_COMMENT_MODE, 
-    hljs.PHRASAL_WORDS_MODE
-  ];
+  var BLOCK_COMMENT = hljs.inherit(hljs.C_BLOCK_COMMENT_MODE);
+  BLOCK_COMMENT.contains.push('self');
   return {
     aliases: ['rs'],
     keywords: {
@@ -36,7 +30,7 @@ function(hljs) {
     illegal: '</',
     contains: [
       hljs.C_LINE_COMMENT_MODE,
-      RUST_BLOCK_COMMENT_MODE,
+      BLOCK_COMMENT,
       hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null}),
       {
         className: 'string',
