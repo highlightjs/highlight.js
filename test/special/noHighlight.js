4e866cd96e911dfefe852629886310ced249900f
Oleg Efimov <efimovov@gmail.com>
2014-12-14 20:44:25 +0300

    Added test for no-highlight (mind the dash) class handling and typo fix

--- test/special/noHighlight.js
+++ test/special/noHighlight.js
@@ -3,24 +3,32 @@
 describe('no highlighting', function() {
   it('should keep block unchanged (nohighlight)', function() {
     var expected = '&lt;div id="contents"&gt;\n  ' +
-                   '&lt;p&gt;Hello, World!c\n&lt;/div&gt;',
+                   '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
         actual   = document.querySelector('.nohighlight').innerHTML;
+
+    actual.should.equal(expected);
+  });
+
+  it('should keep block unchanged (no-highlight)', function() {
+    var expected = '&lt;div id="contents"&gt;\n  ' +
+                   '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+        actual   = document.querySelector('.no-highlight').innerHTML;
 
     actual.should.equal(expected);
   });
 
   it('should keep block unchanged (plain)', function() {
       var expected = '&lt;div id="contents"&gt;\n  ' +
-      '&lt;p&gt;Hello, World!c\n&lt;/div&gt;',
-      actual   = document.querySelector('.plain').innerHTML;
+                     '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+          actual   = document.querySelector('.plain').innerHTML;
 
       actual.should.equal(expected);
   });
 
   it('should keep block unchanged (text)', function() {
       var expected = '&lt;div id="contents"&gt;\n  ' +
-      '&lt;p&gt;Hello, World!c\n&lt;/div&gt;',
-      actual   = document.querySelector('.text').innerHTML;
+                     '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+          actual   = document.querySelector('.text').innerHTML;
 
       actual.should.equal(expected);
   });
