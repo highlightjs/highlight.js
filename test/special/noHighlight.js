bfe00cbde4866ddffbab688204f7cc13a1973ca8
Niklas Keller <me@kelunik.com>
2014-12-14 16:20:41 +0100

    Add text, plain to ignored languages (tests) #430

--- test/special/noHighlight.js
+++ test/special/noHighlight.js
@@ -1,12 +1,28 @@
 'use strict';
 
 describe('no highlighting', function() {
-  it('should keep block unchanged', function() {
+  it('should keep block unchanged (nohighlight)', function() {
     var expected = '&lt;div id="contents"&gt;\n  ' +
                    '&lt;p&gt;Hello, World!c\n&lt;/div&gt;',
         actual   = document.querySelector('.nohighlight').innerHTML;
 
     actual.should.equal(expected);
+  });
+
+  it('should keep block unchanged (plain)', function() {
+      var expected = '&lt;div id="contents"&gt;\n  ' +
+      '&lt;p&gt;Hello, World!c\n&lt;/div&gt;',
+      actual   = document.querySelector('.plain').innerHTML;
+
+      actual.should.equal(expected);
+  });
+
+  it('should keep block unchanged (text)', function() {
+      var expected = '&lt;div id="contents"&gt;\n  ' +
+      '&lt;p&gt;Hello, World!c\n&lt;/div&gt;',
+      actual   = document.querySelector('.text').innerHTML;
+
+      actual.should.equal(expected);
   });
 
   it('should skip pre tags without a child code tag', function() {
