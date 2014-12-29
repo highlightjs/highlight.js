9eb9fcf04029582a0cd9d38b9c963d0d403f695e
Andrew Farmer <ahfarmer@gmail.com>
2014-12-29 01:56:44 -0800

    add myself to contributors

--- src/languages/objectivec.js
+++ src/languages/objectivec.js
@@ -1,7 +1,7 @@
 /*
 Language: Objective C
 Author: Valerii Hiora <valerii.hiora@gmail.com>
-Contributors: Angel G. Olloqui <angelgarcia.mail@gmail.com>, Matt Diephouse <matt@diephouse.com>
+Contributors: Angel G. Olloqui <angelgarcia.mail@gmail.com>, Matt Diephouse <matt@diephouse.com>, Andrew Farmer <ahfarmer@gmail.com>
 Category: common
 */
 
