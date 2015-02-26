8babb0575c571561ea12db426747afc5287452a2
Igal Tabachnik <hmemcpy@gmail.com>
2015-02-26 11:15:56 +0200

    Adding support for highlighting the C# keywords: dynamic and 'when', which is a part of the new C# 6 syntax for exception filters

--- src/languages/cs.js
+++ src/languages/cs.js
@@ -7,9 +7,9 @@ Category: common
 function(hljs) {
   var KEYWORDS =
     // Normal keywords.
-    'abstract as base bool break byte case catch char checked const continue decimal ' +
+    'abstract as base bool break byte case catch char checked const continue decimal dynamic ' +
     'default delegate do double else enum event explicit extern false finally fixed float ' +
-    'for foreach goto if implicit in int interface internal is lock long null ' +
+    'for foreach goto if implicit in int interface internal is lock long null when ' +
     'object operator out override params private protected public readonly ref sbyte ' +
     'sealed short sizeof stackalloc static string struct switch this true try typeof ' +
     'uint ulong unchecked unsafe ushort using virtual volatile void while async ' +
