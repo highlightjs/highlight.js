c16459b7f07e3e6faf79bf4f4effcfdaad129b86
Marius Schulz <marius.schulz@me.com>
2015-04-05 23:55:13 +0200

    Only highlight params if there are any

--- src/languages/javascript.js
+++ src/languages/javascript.js
@@ -74,7 +74,7 @@ function(hljs) {
           hljs.inherit(hljs.TITLE_MODE, {begin: /[A-Za-z$_][0-9A-Za-z$_]*/}),
           {
             className: 'params',
-            begin: /\(/, end: /\)/,
+            begin: /\((?!\s*\))/, end: /\)/,
             excludeBegin: true,
             excludeEnd: true,
             contains: [
