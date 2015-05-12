1b74c3964b8a72e7ad17f7e24378477b3876f817
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-05-12 15:44:22 -0700

    Resetting relevances in Ceylon.

--- src/languages/ceylon.js
+++ src/languages/ceylon.js
@@ -6,12 +6,12 @@ function(hljs) {
   // 2.3. Identifiers and keywords
   var KEYWORDS =
     'assembly module package import alias class interface object given value ' +
-    'assign void function new of extends satisfies|10 abstracts in out return ' +
+    'assign void function new of extends satisfies abstracts in out return ' +
     'break continue throw assert dynamic if else switch case for while try ' +
-    'catch finally then let this outer super is exists|10 nonempty|10';
+    'catch finally then let this outer super is exists nonempty';
   // 7.4.1 Declaration Modifiers
   var DECLARATION_MODIFIERS =
-    'shared|10 abstract formal|10 default|10 actual|10 variable late native deprecated' +
+    'shared abstract formal default actual variable late native deprecated' +
     'final sealed annotation suppressWarnings small';
   // 7.4.2 Documentation
   var DOCUMENTATION =
@@ -31,21 +31,19 @@ function(hljs) {
         className: 'string',
         begin: '"""',
         end: '"""',
-        relevance: 5
+        relevance: 10
       },
       {
         // string literal or template
         className: 'string',
         begin: '"|``',
         end: '"|``',
-        relevance: 0
       },
       {
         // character literal
         className: 'string',
         begin: "'",
         end: "'",
-        relevance: 0
       },
       {
         // numeric literal
@@ -56,8 +54,7 @@ function(hljs) {
       {
         // compiler annotation
         className: 'annotation',
-        begin: '@[a-z]\\w*(?:\\:\"[^\"]*\")?',
-        relevance: 5
+        begin: '@[a-z]\\w*(?:\\:\"[^\"]*\")?'
       }
     ]
   };
