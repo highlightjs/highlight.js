b8f7ede6bd99d8c936aa568f8325a004cafdb214
Oleg Efimov <efimovov@gmail.com>
2015-05-17 17:51:12 +0300

    Inform 7 formatting

--- src/languages/inform7.js
+++ src/languages/inform7.js
@@ -1,26 +1,28 @@
 /*
 Language: Inform 7
 Author: Bruno Dias <bruno.r.dias@gmail.com>
-Description: Language definition for Inform 7, a DSL for writing parser
-interactive fiction.
+Description: Language definition for Inform 7, a DSL for writing parser interactive fiction.
 */
 
 function(hljs) {
-  var START_BRACKET = '\\['
-  var END_BRACKET = '\\]'
+  var START_BRACKET = '\\[';
+  var END_BRACKET = '\\]';
   return {
-    aliases: ['i7', 'Inform 7'],
+    aliases: ['i7'],
     case_insensitive: true,
     keywords: {
       // Some keywords more or less unique to I7, for relevance.
-
-      kind: 'thing|10 room|10 person|10 man|10 woman|10 animal|10 container'
-          + ' supporter|10 backdrop|10 door|10',
-      characteristic: 'scenery|10 open closed|10 locked|10 inside|10 gender|10',
-      verb: 'is are say|10 understand|10',
-      misc_keyword: 'kind|10 of rule|10'
+      keyword:
+        // kind:
+        'thing|10 room|10 person|10 man|10 woman|10 animal|10 container ' +
+        'supporter|10 backdrop|10 door|10 ' +
+        // characteristic:
+        'scenery|10 open closed|10 locked|10 inside|10 gender|10 ' +
+        // verb:
+        'is are say|10 understand|10 ' +
+        // misc keyword:
+        'kind|10 of rule|10'
     },
-
     contains: [
       {
         className: 'string',
@@ -30,7 +32,8 @@ function(hljs) {
           {
             className: 'subst',
             begin: START_BRACKET, end: END_BRACKET
-          }]
+          }
+        ]
       },
       {
         className: 'title',
@@ -51,12 +54,12 @@ function(hljs) {
         begin: '^\\b(Check|Carry out|Report|Instead of|To|Rule|When|Before|After)',
         end: ':',
         contains: [
-        {
-          //Rule name
-          begin: '\\b\\(This',
-          end: '\\)',
-          relevance: 10
-        }
+          {
+            //Rule name
+            begin: '\\b\\(This',
+            end: '\\)',
+            relevance: 10
+          }
         ],
         relevance: 10
       },
