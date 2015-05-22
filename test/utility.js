4e7cb79ea09bec5449b274db99a7187473595cdd
Jeremy Hull <sourdrums@gmail.com>
2015-05-22 15:49:04 -0700

    Clean up utility by using lodash functions

--- test/utility.js
+++ test/utility.js
@@ -1,15 +1,14 @@
 'use strict';
 
+var _    = require('lodash');
 var path = require('path');
 
 // Build a path relative to `test/`
 exports.buildPath = function() {
-  var args  = [].slice.call(arguments, 0),
+  var args  = _.slice(arguments, 0),
       paths = [__dirname].concat(args);
 
   return path.join.apply(this, paths);
 };
 
-exports.numberToString = function(number) {
-  return number.toString();
-};
+exports.numberToString = _.method('toString');
