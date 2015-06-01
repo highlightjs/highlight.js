b11bfc48498f0b9a2877a14235cfff97cd2887c1
Jeremy Hull <sourdrums@gmail.com>
2015-06-01 14:53:29 -0700

    Remove repetition from expected strings

--- test/special/noHighlight.js
+++ test/special/noHighlight.js
@@ -7,36 +7,37 @@ describe('no highlighting', function() {
     var testHTML = document.querySelectorAll('#no-highlight pre'),
         getHTML  = _.property('children[0].innerHTML');
 
-    this.blocks = _.map(testHTML, getHTML);
+    this.blocks   = _.map(testHTML, getHTML);
+    this.expected = {
+      html:   '&lt;div id="contents"&gt;\n  ' +
+              '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+      python: 'for x in [1, 2, 3]: count(x)'
+    };
   });
 
   it('should keep block unchanged (nohighlight)', function() {
-    var expected = '&lt;div id="contents"&gt;\n  ' +
-                   '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+    var expected = this.expected.html,
         actual   = this.blocks[0];
 
     actual.should.equal(expected);
   });
 
   it('should keep block unchanged (no-highlight)', function() {
-    var expected = '&lt;div id="contents"&gt;\n  ' +
-                   '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+    var expected = this.expected.html,
         actual   = this.blocks[1];
 
     actual.should.equal(expected);
   });
 
   it('should keep block unchanged (plain)', function() {
-    var expected = '&lt;div id="contents"&gt;\n  ' +
-                   '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+    var expected = this.expected.html,
         actual   = this.blocks[2];
 
     actual.should.equal(expected);
   });
 
   it('should keep block unchanged (text)', function() {
-    var expected = '&lt;div id="contents"&gt;\n  ' +
-                   '&lt;p&gt;Hello, World!\n&lt;/div&gt;',
+    var expected = this.expected.html,
         actual   = this.blocks[3];
 
     actual.should.equal(expected);
@@ -50,21 +51,21 @@ describe('no highlighting', function() {
   });
 
   it('should keep block unchanged (unsupported language)', function() {
-    var expected = 'for x in [1, 2, 3]: count(x)',
+    var expected = this.expected.python,
         actual   = this.blocks[5];
 
     actual.should.equal(expected);
   });
 
   it('should keep block unchanged (unsupported lang)', function() {
-    var expected = 'for x in [1, 2, 3]: count(x)',
+    var expected = this.expected.python,
         actual   = this.blocks[6];
 
     actual.should.equal(expected);
   });
 
   it('should keep block unchanged (unsupported prefixed language)', function() {
-    var expected = 'for x in [1, 2, 3]: count(x)',
+    var expected = this.expected.python,
         actual   = this.blocks[7];
 
     actual.should.equal(expected);
