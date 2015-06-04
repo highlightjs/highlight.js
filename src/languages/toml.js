74479397aa38e682f0ff4e200b769e5958fe5066
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-06-04 15:12:38 +0200

    Add more information

--- src/languages/toml.js
+++ src/languages/toml.js
@@ -5,29 +5,51 @@ Category: config
 */
 
 function(hljs) {
-    var VARIABLE = {
-		className: "variable",
-		variants: [{
-			begin: /\$[\w\d#@][\w\d_]*/
-		}, {
-			begin: /\$\{(.*?)}/
-		}]
-	};
-	var STRING = {
-		className: "string",
-		begin: /"/,
-		end: /"/
-	};
-	var PREPROC = {
-		className: "preprocessor",
-		begin: /\[[^\"]+\]/
-	};
-	return {
-		contains: [
-		  hljs.HASH_COMMENT_MODE,
-		  VARIABLE,
-		  STRING,
-		  PREPROC
-		]
-	};
+  var VARIABLE = {
+    className: "variable",
+    variants: [{
+      begin: /\$[\w\d"][\w\d_]*/
+    }, {
+      begin: /\$\{(.*?)}/
+    }]
+  };
+  var STRING = {
+    className: "string",
+    variants: [
+      {
+        begin: "'''", end: "'''",
+        contains: [hljs.BACKSLASH_ESCAPE]
+      }, {
+        begin: '"""', end: '"""',
+        contains: [hljs.BACKSLASH_ESCAPE]
+      }, {
+        begin: '"', end: '"',
+        contains: [hljs.BACKSLASH_ESCAPE]
+      }, {
+        begin: "'", end: "'",
+        contains: [hljs.BACKSLASH_ESCAPE]
+      }
+    ]
+    //begin: /("""|'''|"|')(?:[^"'\\]|\\.|\\\n)*("""|'''|"|')/
+  };
+  var TABLE = {
+    className: "table",
+    begin: /^([\s]+)?[\[]+(.*?)[\]]+/
+  };
+  var NUMBER = {
+    className: "number",
+    begin: /([\+\-]+)?[\d]+_[\d_]+/
+  };
+  return {
+    keywords: ["true", "false"],
+    contains: [
+      hljs.HASH_COMMENT_MODE,
+      hljs.NUMBER_MODE,
+      NUMBER,
+      hljs.QUOTE_STRING_MODE,
+      VARIABLE,
+      STRING,
+      TABLE
+    ]
+  };
 }
