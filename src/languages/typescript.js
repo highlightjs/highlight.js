ff0b0536ad1c0f552e9a7b4d1dd2cbc706995c3d
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-06-30 11:59:48 -0700

    Fixed TypeScript keywords.
    Namely, extends, constructor, interface.

--- src/languages/typescript.js
+++ src/languages/typescript.js
@@ -10,8 +10,8 @@ function(hljs) {
     keyword:
       'in if for while finally var new function|0 do return void else break catch ' +
       'instanceof with throw case default try this switch continue typeof delete ' +
-      'let yield const class public private get set super interface extends' +
-      'static constructor implements enum export import declare type protected',
+      'let yield const class public private get set super ' +
+      'static implements enum export import declare type protected',
     literal:
       'true false null undefined NaN Infinity',
     built_in:
@@ -86,8 +86,7 @@ function(hljs) {
       },
       {
         className: 'constructor',
-        begin: 'constructor', end: /\{/, excludeEnd: true,
-        keywords: KEYWORDS,
+        beginKeywords: 'constructor', end: /\{/, excludeEnd: true,
         relevance: 10
       },
       {
@@ -96,7 +95,8 @@ function(hljs) {
       },
       {
         className: 'interface',
-        beginKeywords: 'interface', end: /\{/, excludeEnd: true
+        beginKeywords: 'interface', end: /\{/, excludeEnd: true,
+        keywords: 'interface extends'
       },
       {
         begin: /\$[(.]/ // relevance booster for a pattern common to JS libs: `$(something)` and `$.something`
