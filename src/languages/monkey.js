69ae47b76a7c2843941b0fc9e11cc058652db1fd
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-07-14 20:05:57 -0700

    Updated monkey.js to new class names.

--- src/languages/monkey.js
+++ src/languages/monkey.js
@@ -19,7 +19,8 @@ function(hljs) {
     keywords: {
       keyword: 'public private property continue exit extern new try catch ' +
         'eachin not abstract final select case default const local global field ' +
-        'end if then else elseif endif while wend repeat until forever for to step next return module inline throw',
+        'end if then else elseif endif while wend repeat until forever for ' +
+        'to step next return module inline throw import',
 
       built_in: 'DebugLog DebugStop Error Print ACos ACosr ASin ASinr ATan ATan2 ATan2r ATanr Abs Abs Ceil ' +
         'Clamp Clamp Cos Cosr Exp Floor Log Max Max Min Min Pow Sgn Sgn Sin Sinr Sqrt Tan Tanr Seed PI HALFPI TWOPI',
@@ -54,21 +55,16 @@ function(hljs) {
         ]
       },
       {
-        className: 'variable',
+        className: 'built_in',
         begin: '\\b(self|super)\\b'
       },
       {
-        className: 'preprocessor',
-        beginKeywords: 'import',
-        end: '$'
-      },
-      {
-        className: 'preprocessor',
+        className: 'meta',
         begin: '\\s*#', end: '$',
         keywords: 'if else elseif endif end then'
       },
       {
-        className: 'pi',
+        className: 'meta',
         begin: '^\\s*strict\\b'
       },
       {
