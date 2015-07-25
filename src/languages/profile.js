686bc091eb478d471a6093bf5e9ed3f8675bd1cb
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-07-25 14:00:30 -0700

    Updated profile.js to new class names.

--- src/languages/profile.js
+++ src/languages/profile.js
@@ -9,25 +9,15 @@ function(hljs) {
     contains: [
       hljs.C_NUMBER_MODE,
       {
-        className: 'built_in',
-        begin: '{', end: '}$',
-        excludeBegin: true, excludeEnd: true,
-        contains: [hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE],
-        relevance: 0
-      },
-      {
-        className: 'filename',
         begin: '[a-zA-Z_][\\da-zA-Z_]+\\.[\\da-zA-Z_]{1,3}', end: ':',
         excludeEnd: true
       },
       {
-        className: 'header',
         begin: '(ncalls|tottime|cumtime)', end: '$',
         keywords: 'ncalls tottime|10 cumtime|10 filename',
         relevance: 10
       },
       {
-        className: 'summary',
         begin: 'function calls', end: '$',
         contains: [hljs.C_NUMBER_MODE],
         relevance: 10
@@ -35,11 +25,9 @@ function(hljs) {
       hljs.APOS_STRING_MODE,
       hljs.QUOTE_STRING_MODE,
       {
-        className: 'function',
+        className: 'string',
         begin: '\\(', end: '\\)$',
-        contains: [
-          hljs.UNDERSCORE_TITLE_MODE
-        ],
+        excludeBegin: true, excludeEnd: true,
         relevance: 0
       }
     ]
