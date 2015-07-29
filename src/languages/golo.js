92de8316c1338fe03b6c5c826596ff693337b25f
Oleg Efimov <o.efimov@corp.badoo.com>
2015-07-29 23:09:49 +0300

    Style fixes for Golo language

--- src/languages/golo.js
+++ src/languages/golo.js
@@ -2,19 +2,20 @@
 Language: Golo
 Author: Philippe Charriere <ph.charriere@gmail.com>
 Description: a lightweight dynamic language for the JVM, see http://golo-lang.org/
-Category: dynamic language for Java
 */
 
 function(hljs) {
     return {
       keywords: {
-          literal : 'true false null',
-          keyword: 'println readln print import module function local return let var ' +
+        keyword:
+          'println readln print import module function local return let var ' +
           'while for foreach times in case when match with break continue ' +
-          'augment augmentation each find filter reduce' + 
-          'if then else otherwise try catch finally raise throw orIfNull ',
-          typename:
-          'DynamicObject|10 DynamicVariable struct Observable map set vector list array'
+          'augment augmentation each find filter reduce ' +
+          'if then else otherwise try catch finally raise throw orIfNull',
+        typename:
+          'DynamicObject|10 DynamicVariable struct Observable map set vector list array',
+        literal:
+          'true false null'
       },
       contains: [
         hljs.HASH_COMMENT_MODE,
@@ -26,5 +27,3 @@ function(hljs) {
       ]
     }
 }
-
-      
