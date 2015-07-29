09bc7e8c0ec3810b299f71b4909e48e498e1b9a2
Jeremy Hull <sourdrums@gmail.com>
2015-07-28 18:25:40 -0700

    Move build target description to a separate line

--- tools/build.js
+++ tools/build.js
@@ -8,43 +8,46 @@
 // Build Targets
 // -------------
 //
-// * browser - The default target. This will package up the core
-//             `highlight.js` along with all the language definitions into
-//             the file `highlight.pack.js` -- which will be compressed
-//             without including the option to disable it. It also builds
-//             the documentation for our readthedocs page, mentioned above,
-//             along with a local instance of the demo at:
+// * browser
 //
-//             <https://highlightjs.org/static/demo/>.
+//   The default target. This will package up the core `highlight.js` along
+//   with all the language definitions into the file `highlight.pack.js` --
+//   which will be compressed without including the option to disable it. It
+//   also builds the documentation for our readthedocs page, mentioned
+//   above, along with a local instance of the demo at:
 //
-// * cdn - This will package up the core `highlight.js` along with all the
-//         language definitions into the file `highlight.min.js` and
-//         compresses all languages and styles into separate files. Since
-//         the target is for CDNs -- like cdnjs and jsdelivr -- it doesn't
-//         matter if you put the option to disable compression, this target
-//         is always be compressed. Do keep in mind that we don't keep the
-//         build results in the main repository; however, there is a
-//         separate repository for those that want the CDN builds without
-//         using a third party site or building it themselves. For those
-//         curious, head over to:
+//   <https://highlightjs.org/static/demo/>.
 //
-//         <https://github.com/highlightjs/cdn-release>
+// * cdn
 //
-// * node - This build will transform the library into a CommonJS module. It
-//          includes the generation of an `index.js` file that will be the
-//          main file imported for use with Node.js or browserify. Do note
-//          that this includes all languages by default and it might be too
-//          heavy to use for browserify. Luckily, you can easily do two
-//          things to make the build smaller; either specify the specific
-//          language/category you need or you can use the browser or cdn
-//          builds and it will work like any CommonJS file. Also with the
-//          CommonJS module, it includes the documentation for our
-//          readthedocs page and the uncompressed styles. Getting this build
-//          is pretty easy as it is the one that gets published to npm with
-//          the standard `npm install highlight.js`, but you can also visit
-//          the package page at:
+//   This will package up the core `highlight.js` along with all the
+//   language definitions into the file `highlight.min.js` and compresses
+//   all languages and styles into separate files. Since the target is for
+//   CDNs -- like cdnjs and jsdelivr -- it doesn't matter if you put the
+//   option to disable compression, this target is always be compressed. Do
+//   keep in mind that we don't keep the build results in the main
+//   repository; however, there is a separate repository for those that want
+//   the CDN builds without using a third party site or building it
+//   themselves. For those curious, head over to:
 //
-//          <https://www.npmjs.com/package/highlight.js>
+//   <https://github.com/highlightjs/cdn-release>
+//
+// * node
+//
+//   This build will transform the library into a CommonJS module. It
+//   includes the generation of an `index.js` file that will be the main
+//   file imported for use with Node.js or browserify. Do note that this
+//   includes all languages by default and it might be too heavy to use for
+//   browserify. Luckily, you can easily do two things to make the build
+//   smaller; either specify the specific language/category you need or you
+//   can use the browser or cdn builds and it will work like any CommonJS
+//   file. Also with the CommonJS module, it includes the documentation for
+//   our readthedocs page and the uncompressed styles. Getting this build is
+//   pretty easy as it is the one that gets published to npm with the
+//   standard `npm install highlight.js`, but you can also visit the package
+//   page at:
+//
+//   <https://www.npmjs.com/package/highlight.js>
 //
 // All build targets will end up in the `build` directory.
 'use strict';
