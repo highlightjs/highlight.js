488b1f6965287c9fe6e6e6351cc7bf6bcfeadd6a
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-08-22 15:02:52 -0700

    Update smali.js to new class names

--- src/languages/smali.js
+++ src/languages/smali.js
@@ -25,43 +25,28 @@ function(hljs) {
       ),
       {
         className: 'keyword',
-        begin: '\\s*\\.end\\s[a-zA-Z0-9]*',
-        relevance: 1
+        variants: [
+          {begin: '\\s*\\.end\\s[a-zA-Z0-9]*'},
+          {begin: '^[ ]*\\.[a-zA-Z]*', relevance: 0},
+          {begin: '\\s:[a-zA-Z_0-9]*', relevance: 0},
+          {begin: '\\s(' + smali_keywords.join('|') + ')'}
+        ]
       },
       {
-        className: 'keyword',
-        begin: '^[ ]*\\.[a-zA-Z]*',
-        relevance: 0
-      },
-      {
-        className: 'keyword',
-        begin: '\\s:[a-zA-Z_0-9]*',
-        relevance: 0
-      },
-      {
-        className: 'keyword',
-        begin: '\\s('+smali_keywords.join('|')+')',
-        relevance: 1
-      },
-      {
-        className: 'keyword',
-        begin: '\\[',
-        relevance: 0
-      },
-      {
-        className: 'instruction',
-        begin: '\\s('+smali_instr_low_prio.join('|')+')\\s',
-        relevance: 1
-      },
-      {
-        className: 'instruction',
-        begin: '\\s('+smali_instr_low_prio.join('|')+')((\\-|/)[a-zA-Z0-9]+)+\\s',
-        relevance: 10
-      },
-      {
-        className: 'instruction',
-        begin: '\\s('+smali_instr_high_prio.join('|')+')((\\-|/)[a-zA-Z0-9]+)*\\s',
-        relevance: 10
+        className: 'built_in',
+        variants : [
+          {
+            begin: '\\s('+smali_instr_low_prio.join('|')+')\\s'
+          },
+          {
+            begin: '\\s('+smali_instr_low_prio.join('|')+')((\\-|/)[a-zA-Z0-9]+)+\\s',
+            relevance: 10
+          },
+          {
+            begin: '\\s('+smali_instr_high_prio.join('|')+')((\\-|/)[a-zA-Z0-9]+)*\\s',
+            relevance: 10
+          },
+        ]
       },
       {
         className: 'class',
@@ -69,19 +54,7 @@ function(hljs) {
         relevance: 0
       },
       {
-        className: 'function',
-        begin: '( |->)[^(\n ;"]*\\(',
-        relevance: 0
-      },
-      {
-        className: 'function',
-        begin: '\\)',
-        relevance: 0
-      },
-      {
-        className: 'variable',
         begin: '[vp][0-9]+',
-        relevance: 0
       }
     ]
   };
