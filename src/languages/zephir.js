3216a860a3c90489d2d06bc2cb0c1c1b54d208e7
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-10-27 15:02:10 -0700

    Fix indentation in zephir.js

--- src/languages/zephir.js
+++ src/languages/zephir.js
@@ -23,15 +23,15 @@ function(hljs) {
     aliases: ['zep'],
     case_insensitive: true,
     keywords:
-    'and include_once list abstract global private echo interface as static endswitch ' +
-    'array null if endwhile or const for endforeach self var let while isset public ' +
-    'protected exit foreach throw elseif include __FILE__ empty require_once do xor ' +
-    'return parent clone use __CLASS__ __LINE__ else break print eval new ' +
-    'catch __METHOD__ case exception default die require __FUNCTION__ ' +
-    'enddeclare final try switch continue endfor endif declare unset true false ' +
-    'trait goto instanceof insteadof __DIR__ __NAMESPACE__ ' +
-    'yield finally int uint long ulong char uchar double float bool boolean string' +
-    'likely unlikely',
+      'and include_once list abstract global private echo interface as static endswitch ' +
+      'array null if endwhile or const for endforeach self var let while isset public ' +
+      'protected exit foreach throw elseif include __FILE__ empty require_once do xor ' +
+      'return parent clone use __CLASS__ __LINE__ else break print eval new ' +
+      'catch __METHOD__ case exception default die require __FUNCTION__ ' +
+      'enddeclare final try switch continue endfor endif declare unset true false ' +
+      'trait goto instanceof insteadof __DIR__ __NAMESPACE__ ' +
+      'yield finally int uint long ulong char uchar double float bool boolean string' +
+      'likely unlikely',
     contains: [
       hljs.C_LINE_COMMENT_MODE,
       hljs.HASH_COMMENT_MODE,
