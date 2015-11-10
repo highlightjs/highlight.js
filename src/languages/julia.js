2b65ac9314407f323d08d14d4d18fbdddf501a48
Kenta Sato <bicycle1885@gmail.com>
2015-11-11 03:20:48 +0900

    add built-in names introduced in Julia v0.4

--- src/languages/julia.js
+++ src/languages/julia.js
@@ -6,14 +6,14 @@ Author: Kenta Sato <bicycle1885@gmail.com>
 function(hljs) {
   // Since there are numerous special names in Julia, it is too much trouble
   // to maintain them by hand. Hence these names (i.e. keywords, literals and
-  // built-ins) are automatically generated from Julia (v0.3.0) itself through
-  // following scripts for each.
+  // built-ins) are automatically generated from Julia (v0.3.0 and v0.4.1)
+  // itself through following scripts for each.
 
   var KEYWORDS = {
     // # keyword generator
-    // println("\"in\",")
+    // println("in")
     // for kw in Base.REPLCompletions.complete_keyword("")
-    //     println("\"$kw\",")
+    //     println(kw)
     // end
     keyword:
       'in abstract baremodule begin bitstype break catch ccall const continue do else elseif end export ' +
@@ -21,40 +21,47 @@ function(hljs) {
       'typealias using while',
 
     // # literal generator
-    // println("\"true\",\n\"false\"")
+    // println("true")
+    // println("false")
     // for name in Base.REPLCompletions.completions("", 0)[1]
     //     try
     //         s = symbol(name)
     //         v = eval(s)
     //         if !isa(v, Function) &&
     //            !isa(v, DataType) &&
+    //            !isa(v, IntrinsicFunction) &&
     //            !issubtype(typeof(v), Tuple) &&
-    //            !isa(v, UnionType) &&
+    //            !isa(v, Union) &&
     //            !isa(v, Module) &&
     //            !isa(v, TypeConstructor) &&
+    //            !isa(v, TypeVar) &&
     //            !isa(v, Colon)
-    //             println("\"$name\",")
+    //             println(name)
     //         end
     //     end
     // end
     literal:
-      'true false ANY ARGS CPU_CORES C_NULL DL_LOAD_PATH DevNull ENDIAN_BOM ENV I|0 Inf Inf16 Inf32 ' +
+      // v0.3
+      'true false ARGS CPU_CORES C_NULL DL_LOAD_PATH DevNull ENDIAN_BOM ENV I|0 Inf Inf16 Inf32 ' +
       'InsertionSort JULIA_HOME LOAD_PATH MS_ASYNC MS_INVALIDATE MS_SYNC MergeSort NaN NaN16 NaN32 OS_NAME QuickSort ' +
       'RTLD_DEEPBIND RTLD_FIRST RTLD_GLOBAL RTLD_LAZY RTLD_LOCAL RTLD_NODELETE RTLD_NOLOAD RTLD_NOW RoundDown ' +
       'RoundFromZero RoundNearest RoundToZero RoundUp STDERR STDIN STDOUT VERSION WORD_SIZE catalan cglobal e|0 eu|0 ' +
-      'eulergamma golden im nothing pi γ π φ',
+      'eulergamma golden im nothing pi γ π φ ' +
+      // v0.4 (diff)
+      'Inf64 NaN64 RoundNearestTiesAway RoundNearestTiesUp ',
 
     // # built_in generator:
     // for name in Base.REPLCompletions.completions("", 0)[1]
     //     try
     //         v = eval(symbol(name))
-    //         if isa(v, DataType)
-    //             println("\"$name\",")
+    //         if isa(v, DataType) || isa(v, TypeVar)
+    //             println(name)
     //         end
     //     end
     // end
     built_in:
-      'ASCIIString AbstractArray AbstractRNG AbstractSparseArray Any ArgumentError Array Associative Base64Pipe ' +
+      // v0.3
+      'ANY ASCIIString AbstractArray AbstractRNG AbstractSparseArray Any ArgumentError Array Associative Base64Pipe ' +
       'Bidiagonal BigFloat BigInt BitArray BitMatrix BitVector Bool BoundsError Box CFILE Cchar Cdouble Cfloat Char ' +
       'CharString Cint Clong Clonglong ClusterManager Cmd Coff_t Colon Complex Complex128 Complex32 Complex64 ' +
       'Condition Cptrdiff_t Cshort Csize_t Cssize_t Cuchar Cuint Culong Culonglong Cushort Cwchar_t DArray DataType ' +
@@ -70,7 +77,15 @@ function(hljs) {
       'SymTridiagonal Symbol SymbolNode Symmetric SystemError Task TextDisplay Timer TmStruct TopNode Triangular ' +
       'Tridiagonal Type TypeConstructor TypeError TypeName TypeVar UTF16String UTF32String UTF8String UdpSocket ' +
       'Uint Uint128 Uint16 Uint32 Uint64 Uint8 UndefRefError UndefVarError UniformScaling UnionType UnitRange ' +
-      'Unsigned Vararg VersionNumber WString WeakKeyDict WeakRef Woodbury Zip'
+      'Unsigned Vararg VersionNumber WString WeakKeyDict WeakRef Woodbury Zip ' +
+      // v0.4 (diff)
+      'AbstractChannel AbstractFloat AbstractString AssertionError Base64DecodePipe Base64EncodePipe BufferStream ' +
+      'CapturedException CartesianIndex CartesianRange Channel Cintmax_t CompositeException Cstring Cuintmax_t ' +
+      'Cwstring Date DateTime Dims Enum GenSym GlobalRef HTML InitError InvalidStateException Irrational LinSpace ' +
+      'LowerTriangular NullException Nullable OutOfMemoryError Pair PartialQuickSort Pipe RandomDevice ' +
+      'ReadOnlyMemoryError ReentrantLock Ref RemoteException SegmentationFault SerializationState SimpleVector ' +
+      'TCPSocket Text Tuple UDPSocket UInt UInt128 UInt16 UInt32 UInt64 UInt8 UnicodeError Union UpperTriangular ' +
+      'Val Void WorkerConfig '
   };
 
   // ref: http://julia.readthedocs.org/en/latest/manual/variables/#allowed-variable-names
