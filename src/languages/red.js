229bc60588995b804077a3a110ceee28d10b0cf9
Oldes <oldes.huhuman@gmsil.com>
2015-11-20 12:04:38 +0100

    Red language: adding one more illegal to resolve conflict with Monkey

--- src/languages/red.js
+++ src/languages/red.js
@@ -153,7 +153,7 @@ function(hljs) {
         'random absolute add divide multiply negate remainder subtract pick reverse '+
         'select find'
     },
-    illegal: /(\/\*|\/\/|%{|[a-zA-Z\&],|\$\()/,
+    illegal: /(\/\*|\/\/|%{|[a-zA-Z\&],|\$\()|\'\s/,
     contains: [
       STRING, STRING_MULTILINE, FILE, URL, EMAIL, TAG, REFINEMENT, DATATYPE, LIT_WORD,
       BINARY2, BINARY16, BINARY64, 
