f3b4c0acdb219d0b896237703b503f1990693d58
Ivan Sagalaev <maniac@softwaremaniacs.org>
2015-11-25 16:17:57 -0800

    Category "markup"
    Includes markdown, asciidoc, tex

--- src/languages/markdown.js
+++ src/languages/markdown.js
@@ -3,7 +3,7 @@ Language: Markdown
 Requires: xml.js
 Author: John Crepezzi <john.crepezzi@gmail.com>
 Website: http://seejohncode.com/
-Category: common
+Category: common, markup
 */
 
 function(hljs) {
