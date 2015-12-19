highlightjs/highlight.js

commit 560d953b2d0e68b611bed7d291750128aef6db83

Illegalized `#` in a bunch of languages.
This helps to diverge detection of languages with #-comments and
those that can't have this symbol at all.

src/languages/javascript.js
@@ -111,6 +111,7 @@ function(hljs) {
           hljs.UNDERSCORE_TITLE_MODE
         ]
       }
-    ]
+    ],
+    illegal: /#/
   };
 }

src/languages/actionscript.js
@@ -74,6 +74,7 @@ function(hljs) {
           }
         ]
       }
-    ]
+    ],
+    illegal: /#/
   };
 }

src/languages/aspectj.js
@@ -16,7 +16,7 @@ function (hljs) {
   var SHORTKEYS = 'get set args call';
   return {
     keywords : KEYWORDS,
-    illegal : /<\//,
+    illegal : /<\/|#/,
     contains : [
       hljs.COMMENT(
         '/\\*\\*',

src/languages/groovy.js
@@ -88,6 +88,7 @@ function(hljs) {
                 className: 'label', begin: '^\\s*[A-Za-z0-9_$]+:',
                 relevance: 0
             },
-        ]
+        ],
+        illegal: /#/
     }
 }

src/languages/java.js
@@ -36,7 +36,7 @@ function(hljs) {
   return {
     aliases: ['jsp'],
     keywords: KEYWORDS,
-    illegal: /<\//,
+    illegal: /<\/|#/,
     contains: [
       hljs.COMMENT(
         '/\\*\\*',

src/languages/ruleslanguage.js
@@ -55,8 +55,12 @@ function(hljs) {
       hljs.APOS_STRING_MODE,
       hljs.QUOTE_STRING_MODE,
       hljs.C_NUMBER_MODE,
-      { className: 'array',
-        begin: '\#[a-zA-Z\ \.]+'
+      {
+        className: 'array',
+        variants: [
+          {begin: '#\\s+[a-zA-Z\\ \\.]*', relevance: 0}, // looks like #-comment
+          {begin: '#[a-zA-Z\\ \\.]+'}
+        ]
       }
     ]
   };

commit 27b3adc121605be8f598c2326db01aaa7cc29964

Allow sheband headers in javascript, fixes #1027
This was broken in 560d953b2d0e68b611bed7d291750128aef6db83
for few languages, but I think it is helpful only in javascript.

src/languages/javascript.js
@@ -32,6 +32,10 @@ function(hljs) {
         relevance: 10,
         begin: /^\s*['"]use (strict|asm)['"]/
       },
+      {
+        className: 'meta',
+        begin: /^#!/, end: /$/
+      },
       hljs.APOS_STRING_MODE,
       hljs.QUOTE_STRING_MODE,
       { // template string
@@ -108,6 +112,6 @@ function(hljs) {
         beginKeywords: 'constructor', end: /\{/, excludeEnd: true
       }
     ],
-    illegal: /#/
+    illegal: /#(?!!)/
   };
 }