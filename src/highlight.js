7d7923e03bd1ac81ea0659beb29c8ac87a84ecba
Jeremy Hull <sourdrums@gmail.com>
2016-01-27 23:10:32 -0700

    Change to checking if `globalObject` is falsey

--- src/highlight.js
+++ src/highlight.js
@@ -7,14 +7,13 @@ https://highlightjs.org/
 
   // Find the global object for export to both the browser and web workers.
   var globalObject = typeof window == 'object' && window ||
-                     typeof self == 'object' && self ||
-                     this;
+                     typeof self == 'object' && self;
 
   // Setup highlight.js for different environments. First is Node.js or
   // CommonJS.
   if(typeof exports !== 'undefined') {
     factory(exports);
-  } else {
+  } else if(globalObject) {
     // Export hljs globally even when using AMD for cases when this script
     // is loaded with others that may still expect a global hljs.
     globalObject.hljs = factory({});
