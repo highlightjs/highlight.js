2946a046fb16cfe44741b032d45e231ea5129aaa
Ivan Sagalaev <maniac@softwaremaniacs.org>
2016-03-15 12:56:39 -0700

    Add `move` keyword to Rust

--- src/languages/rust.js
+++ src/languages/rust.js
@@ -28,7 +28,7 @@ function(hljs) {
         'alignof as be box break const continue crate do else enum extern ' +
         'false fn for if impl in let loop match mod mut offsetof once priv ' +
         'proc pub pure ref return self Self sizeof static struct super trait true ' +
-        'type typeof unsafe unsized use virtual while where yield ' +
+        'type typeof unsafe unsized use virtual while where yield move ' +
         'int i8 i16 i32 i64 ' +
         'uint u8 u32 u64 ' +
         'float f32 f64 ' +
