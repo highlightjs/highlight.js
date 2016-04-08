98d06de1bb29fc84c13fc88662e957988310d0a8
tajmone <tajmone@libero.it>
2016-04-08 21:10:01 +0200

    Fixed Procedures Declarations
    Now Procedure declarations employ correctly the function tag, as well as
    keywords, return type definitions (as `type`), and Proc. name (as
    `title`).

--- src/languages/purebasic.js
+++ src/languages/purebasic.js
@@ -3,7 +3,7 @@ Language: PureBASIC
 Author: Tristano Ajmone <tajmone@gmail.com>
 Description: Syntax highlighting for PureBASIC (v.5). No inline ASM highlighting. First release (v.1.0), April 2016.
 Credits: I've taken inspiration from the PureBasic language file for GeSHi, created by Gustavo Julio Fiorenza (GuShH).
-Category: misc
+Category: misc aaa
 */
 
 // Base deafult colors in PB IDE: background: #FFFFDF; foreground: #000000;
@@ -19,41 +19,50 @@ function(hljs) {
 			className: 'symbol',
 			begin: '#[a-zA-Z_]\\w*\\$?'
 	};
-	var PROCEDURES_RHS = { // PB IDE color: #006666 (Blue Stone)
-		// ")"
-		className: 'function',
-		begin: '\\)',
-		relevance: 0
-	};
 
   return {
 		aliases: ['pb', 'pbi'],
 		keywords: {
-			keyword: // PB IDE color: #006666 (Blue Stone)
+			keyword: // PB IDE color: #006666 (Blue Stone) + Bold
 				// The following keywords list was taken and adapted from GuShH's PureBasic language file for GeSHi...
 				'And As Break CallDebugger Case CompilerCase CompilerDefault CompilerElse CompilerEndIf CompilerEndSelect ' +
-				'CompilerError CompilerIf CompilerSelect Continue Data DataSection EndDataSection Debug DebugLevel Declare ' +
-				'DeclareCDLL DeclareDLL Default Define Dim DisableASM DisableDebugger DisableExplicit Else ElseIf EnableASM ' +
+				'CompilerError CompilerIf CompilerSelect Continue Data DataSection EndDataSection Debug DebugLevel ' +
+				'Default Define Dim DisableASM DisableDebugger DisableExplicit Else ElseIf EnableASM ' +
 				'EnableDebugger EnableExplicit End EndEnumeration EndIf EndImport EndInterface EndMacro EndProcedure ' +
 				'EndSelect EndStructure EndStructureUnion EndWith Enumeration Extends FakeReturn For Next ForEach ' +
 				'ForEver Global Gosub Goto If Import ImportC IncludeBinary IncludeFile IncludePath Interface Macro ' +
-				'NewList Not Or Procedure ProcedureC ProcedureCDLL ProcedureDLL ProcedureReturn Protected Prototype ' +
+				'NewList Not Or ProcedureReturn Protected Prototype ' +
 				'PrototypeC Read ReDim Repeat Until Restore Return Select Shared Static Step Structure StructureUnion ' +
-				'Swap To Wend While With XIncludeFile XOr'
+				'Swap To Wend While With XIncludeFile XOr ' +
+				'Procedure ProcedureC ProcedureCDLL ProcedureDLL Declare DeclareC DeclareCDLL DeclareDLL '
 		},
 		contains: [
 			// COMMENTS | PB IDE color: #00AAAA (Persian Green)			
 			hljs.COMMENT(';', '$', {relevance: 0}),
 			
-			// PROCEDURES_LHS | PB IDE color: #006666 (Blue Stone)
-			{
+			{ // PROCEDURES DEFINITIONS | PB IDE color: #006666 (Blue Stone)
 				className: 'function',
-				begin: '[a-zA-Z_]\\w*\\s*\\(',
-				relevance: 0,
+				begin: '\\b(Procedure|Declare)(C|CDLL|DLL)?\\b',
+				end: '\\(',
+				excludeEnd: true,
+	            returnBegin: true,
+				relevance: 10,
+				contains: [
+					{ // PROCEDURE KEYWORDS | PB IDE color: #006666 (Blue Stone) + Bold
+						className: 'keyword',
+						begin: '(Procedure|Declare)(C|CDLL|DLL)?',
+						excludeEnd: true,
+					},
+					{ // PROCEDURE RETURN TYPE SETTING | PB IDE color: #000000 (Black)
+						className: 'type',
+						begin: '\\.\\w*',
+						// end: ' ',
+					},
+					hljs.UNDERSCORE_TITLE_MODE,
+				],
 			},
 			STRINGS,
-			CONSTANTS,
-			PROCEDURES_RHS
+			CONSTANTS
 		]
 	};
 }
