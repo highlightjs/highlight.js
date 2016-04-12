b7c6b4bd7ebf1537e80384d3831c50638b17b077
Ivan Sagalaev <maniac@softwaremaniacs.org>
2016-04-12 14:01:16 -0700

    Use a plain string for PureBasic keywords

--- src/languages/purebasic.js
+++ src/languages/purebasic.js
@@ -21,20 +21,18 @@ function(hljs) {
 
   return {
     aliases: ['pb', 'pbi'],
-    keywords: {
-      keyword: // PB IDE color: #006666 (Blue Stone) + Bold
-        // The following keywords list was taken and adapted from GuShH's PureBasic language file for GeSHi...
-        'And As Break CallDebugger Case CompilerCase CompilerDefault CompilerElse CompilerEndIf CompilerEndSelect ' +
-        'CompilerError CompilerIf CompilerSelect Continue Data DataSection EndDataSection Debug DebugLevel ' +
-        'Default Define Dim DisableASM DisableDebugger DisableExplicit Else ElseIf EnableASM ' +
-        'EnableDebugger EnableExplicit End EndEnumeration EndIf EndImport EndInterface EndMacro EndProcedure ' +
-        'EndSelect EndStructure EndStructureUnion EndWith Enumeration Extends FakeReturn For Next ForEach ' +
-        'ForEver Global Gosub Goto If Import ImportC IncludeBinary IncludeFile IncludePath Interface Macro ' +
-        'NewList Not Or ProcedureReturn Protected Prototype ' +
-        'PrototypeC Read ReDim Repeat Until Restore Return Select Shared Static Step Structure StructureUnion ' +
-        'Swap To Wend While With XIncludeFile XOr ' +
-        'Procedure ProcedureC ProcedureCDLL ProcedureDLL Declare DeclareC DeclareCDLL DeclareDLL '
-    },
+    keywords: // PB IDE color: #006666 (Blue Stone) + Bold
+      // The following keywords list was taken and adapted from GuShH's PureBasic language file for GeSHi...
+      'And As Break CallDebugger Case CompilerCase CompilerDefault CompilerElse CompilerEndIf CompilerEndSelect ' +
+      'CompilerError CompilerIf CompilerSelect Continue Data DataSection EndDataSection Debug DebugLevel ' +
+      'Default Define Dim DisableASM DisableDebugger DisableExplicit Else ElseIf EnableASM ' +
+      'EnableDebugger EnableExplicit End EndEnumeration EndIf EndImport EndInterface EndMacro EndProcedure ' +
+      'EndSelect EndStructure EndStructureUnion EndWith Enumeration Extends FakeReturn For Next ForEach ' +
+      'ForEver Global Gosub Goto If Import ImportC IncludeBinary IncludeFile IncludePath Interface Macro ' +
+      'NewList Not Or ProcedureReturn Protected Prototype ' +
+      'PrototypeC Read ReDim Repeat Until Restore Return Select Shared Static Step Structure StructureUnion ' +
+      'Swap To Wend While With XIncludeFile XOr ' +
+      'Procedure ProcedureC ProcedureCDLL ProcedureDLL Declare DeclareC DeclareCDLL DeclareDLL',
     contains: [
       // COMMENTS | PB IDE color: #00AAAA (Persian Green)
       hljs.COMMENT(';', '$', {relevance: 0}),
