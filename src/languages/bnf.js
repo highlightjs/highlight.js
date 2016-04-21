17e4b66f9a08dc1e5f292f6ae55e62645b2dae91
Ivan Sagalaev <maniac@softwaremaniacs.org>
2016-04-21 12:45:49 -0700

    Attributes only on the left of ::= in BNF

--- src/languages/bnf.js
+++ src/languages/bnf.js
@@ -16,13 +16,21 @@ function(hljs){
       },
       // Specific
       {
-        begin: /::=/
-      },
-      // Common
-      hljs.C_LINE_COMMENT_MODE,
-      hljs.C_BLOCK_COMMENT_MODE,
-      hljs.APOS_STRING_MODE,
-      hljs.QUOTE_STRING_MODE
+        begin: /::=/,
+        starts: {
+          end: /$/,
+          contains: [
+            {
+              begin: /</, end: />/
+            },
+            // Common
+            hljs.C_LINE_COMMENT_MODE,
+            hljs.C_BLOCK_COMMENT_MODE,
+            hljs.APOS_STRING_MODE,
+            hljs.QUOTE_STRING_MODE
+          ]
+        }
+      }
     ]
   };
 }
