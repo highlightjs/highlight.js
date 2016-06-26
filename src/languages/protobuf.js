5036ecdadc467410e22a36fb14ddf3f0f6660d7e
Connor Peet <connor@peet.io>
2016-06-26 16:49:18 -0700

    Fix "class" detection in protobuf syntax

--- src/languages/protobuf.js
+++ src/languages/protobuf.js
@@ -8,9 +8,10 @@ Category: protocols
 function(hljs) {
   return {
     keywords: {
-      keyword: 'package import option optional required repeated group',
+      keyword: 'package import option optional required ' +
+        'repeated group oneof reserved',
       built_in: 'double float int32 int64 uint32 uint64 sint32 sint64 ' +
-        'fixed32 fixed64 sfixed32 sfixed64 bool string bytes',
+        'fixed32 fixed64 sfixed32 sfixed64 bool string bytes map',
       literal: 'true false'
     },
     contains: [
@@ -19,7 +20,7 @@ function(hljs) {
       hljs.C_LINE_COMMENT_MODE,
       {
         className: 'class',
-        beginKeywords: 'message enum service', end: /\{/,
+        begin: /(^|})\s*(message)|(enum)|(service)'/, end: /\{/,
         illegal: /\n/,
         contains: [
           hljs.inherit(hljs.TITLE_MODE, {
