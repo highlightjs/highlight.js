72135e4fa335dcbb4407893eeb6ff5d572506d67
Alex <mckibbenta@gmail.com>
2016-07-01 08:19:47 -0400

    Fixed auto detection resolving ActionScript as ABNF
    Of course, ABNF is now detected as INI. But the refactoring done for
    this is still an improvement.

--- src/languages/abnf.js
+++ src/languages/abnf.js
@@ -7,65 +7,79 @@ Author: Alex McKibben <alex@nullscope.net>
 
 function(hljs) {
     var keywords = [
-        "ALPHA",
-        "DIGIT",
-        "HEXDIG",
-        "DQUOTE",
-        "SP",
-        "HTAB",
-        "WSP",
-        "LWSP",
-        "VCHAR",
-        "CHAR",
-        "OCTET",
-        "CTL",
-        "CR",
-        "LF",
-        "CRLF",
-        "BIT"
+        "ALPHA|10",
+        "DIGIT|10",
+        "HEXDIG|10",
+        "DQUOTE|10",
+        "SP|10",
+        "HTAB|10",
+        "WSP|10",
+        "LWSP|10",
+        "VCHAR|10",
+        "CHAR|10",
+        "OCTET|10",
+        "CTL|10",
+        "CR|10",
+        "LF|10",
+        "CRLF|10",
+        "BIT|10"
     ];
-
-    var ruleMode = {
-        className: "type",
-        begin: "\\b(?!\\b" + keywords.join("|") + "\\b)[a-zA-Z][a-zA-Z0-9-]+\\b",
-        illegal: /_\W/
-    };
 
     var terminalBinaryMode = {
         className: "built_in",
-        begin: /%b[0-1]+/
+        begin: /%b[0-1]+-{0,1}[0-1]*/,
+        relevance: 10
     };
 
     var terminalDecimalMode = {
         className: "built_in",
-        begin: /%d[0-9]+/
+        begin: /%d[0-9]+-{0,1}[0-9]*/,
+        relevance: 10
     };
 
     var terminalHexadecimalMode = {
         className: "built_in",
-        begin: /%x[0-9A-F]+/
+        begin: /%x[0-9A-F]+-{0,1}[0-9A-F]*/,
+        relevance: 10
     };
 
-    var terminalUpperValueRangeMode = {
-        className: "built_in",
-        begin: /-/, end: /[0-9A-F]+/,
-        excludeBegin: true,
-        relevance: 0
+    var commentMode = hljs.COMMENT(";", "$");
+
+    var ruleNameMode = {
+        className: "type",
+        begin: "\\b(?!\\b" + keywords.join("|") + "\\b)[a-zA-Z][a-zA-Z0-9-]+\\b",
+        relevance: 10
+    };
+
+    var ruleNameDeclarationMode = {
+        className: "type",
+        begin: /[a-zA-Z][a-zA-Z0-9-]+/,
+        starts: {
+            end: /=/,
+            excludeEnd: true,
+            illegal: /\s/,
+            starts: {
+                end: /$/,
+                keywords: keywords.join(" "),
+                contains: [
+                    commentMode,
+                    ruleNameMode,
+                    terminalBinaryMode,
+                    terminalDecimalMode,
+                    terminalHexadecimalMode,
+                    hljs.QUOTE_STRING_MODE,
+                    hljs.NUMBER_MODE
+                ],
+                relevance: 10
+            },
+            relevance: 0
+        }
     };
 
     return {
-        lexemes: /[a-zA-Z]+/,
-        keywords: keywords.join(" "),
         contains: [
-            ruleMode,
-            terminalBinaryMode,
-            terminalDecimalMode,
-            terminalHexadecimalMode,
-            terminalUpperValueRangeMode,
-            hljs.QUOTE_STRING_MODE,
-            hljs.NUMBER_MODE,
-            hljs.COMMENT(";", "$")
-        ],
-        illegal: /\W(?==)/
+            commentMode,
+            ruleNameDeclarationMode
+        ]
     };
 }
