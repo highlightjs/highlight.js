1eadb2fb8f6278f3f0c409cef8962cda4bfb2a26
Alex <mckibbenta@gmail.com>
2016-07-19 08:19:03 -0400

    Implemented comment highlighting.

--- src/languages/ebnf.js
+++ src/languages/ebnf.js
@@ -6,6 +6,7 @@ Author: Alex McKibben <alex@nullscope.net>
 function(hljs) {
     return {
         contains: [
+            hljs.COMMENT(/\(\*/, /\*\)/),
             // terminals
             hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE
         ]
