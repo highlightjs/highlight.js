69be3f0929800d1ec17f89fcb64f6fc77db81174
Alex <mckibbenta@gmail.com>
2016-07-19 22:07:54 -0400

    Implemented special sequence mode.

--- src/languages/ebnf.js
+++ src/languages/ebnf.js
@@ -4,8 +4,14 @@ Author: Alex McKibben <alex@nullscope.net>
 */
 
 function(hljs) {
+    var specialSequenceMode = {
+        className: "meta",
+        begin: /\?.*\?/
+    };
+
     return {
         contains: [
+            specialSequenceMode,
             hljs.COMMENT(/\(\*/, /\*\)/),
             // terminals
             hljs.APOS_STRING_MODE, hljs.QUOTE_STRING_MODE
