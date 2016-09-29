0a046463a819358bb0962c38bc00c301e276ec32
Derp McDerpson <DerpfacePython@users.noreply.github.com>
2016-09-30 09:16:23 +1000

    Created syntax highlighter for Logicode

---
+++ src/languages/logicode.js
@@ -0,0 +1,35 @@
+function(hljs) {
+  var KEYWORDS = {
+    keyword: "cond circ var out",
+    built-in: "! | & < > @"
+  },
+  var NUMBER = {
+    className: "number",
+    begin: "[01]+"
+  };
+  var TITLE = hljs.inherit(hljs.TITLE_MODE, {begin: JS_IDENT_RE});
+  var PARAMS = {
+    className: 'params',
+    begin: '\\([^\\(]', returnBegin: true,
+    contains: [{
+      begin: /\(/, end: /\)/,
+      keywords: KEYWORDS
+    }]
+  };
+  var FUNCTION = {
+    className: "function",
+    begin: "circ",
+    end: "->",
+    contains: [TITLE, PARAMS],
+    relevance: 10
+  };
+  return {
+    aliases: ["lgc"],
+    keywords: KEYWORDS,
+    contains: [
+      hljs.HASH_COMMENT_MODE,
+      NUMBER,
+      FUNCTION
+    ]
+  }
+}
