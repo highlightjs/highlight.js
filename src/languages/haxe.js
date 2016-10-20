795698d519e270728d45e6196622f62b7f7b4f40
Kenton Hamaluik <hamaluik@ualberta.ca>
2016-10-20 16:45:49 -0600

    starting to make highlighting a bit more sane

--- src/languages/haxe.js
+++ src/languages/haxe.js
@@ -7,22 +7,21 @@ function(hljs) {
   var IDENT_RE = '[a-zA-Z_$][a-zA-Z0-9_$]*';
   var IDENT_FUNC_RETURN_TYPE_RE = '([*]|[a-zA-Z_$][a-zA-Z0-9_$]*)';
 
-  var HAXE_TYPES = 'Int Float String Bool Dynamic Void Array';
+  var HAXE_BASIC_TYPES = 'Int Float String Bool Dynamic Void Array ';
 
   return {
     aliases: ['hx'],
     keywords: {
       keyword:
-        'break callback case cast catch class continue default do dynamic else enum extends extern ' +
-        'for function here if implements import in inline interface never new override package private ' +
-        'public return static super switch this throw trace try typedef untyped using var while ' +
-        HAXE_TYPES,
-      literal:
-        'true false null',
-      built_in:
+        'package import using extern class',
+      type:
         'Array ArrayAccess Class Date DateTools EReg Enum EnumValue IntIterator ' +
         'Iterable Iterator Lambda List Map Math Null Reflect Single Std StringBuf StringTools Sys ' +
-        'Type UInt ValueType Xml XmlType'
+        'Type UInt ValueType Xml XmlType' + HAXE_BASIC_TYPES,
+      built_in:
+        'trace',
+      literal:
+        'true false null',
     },
     contains: [
       hljs.APOS_STRING_MODE,
@@ -30,47 +29,24 @@ function(hljs) {
       hljs.C_LINE_COMMENT_MODE,
       hljs.C_BLOCK_COMMENT_MODE,
       hljs.C_NUMBER_MODE,
-      {
-        className: 'class',
-        beginKeywords: 'class interface', end: '{', excludeEnd: true,
-        contains: [
-          {
-            beginKeywords: 'extends implements'
-          },
-          hljs.TITLE_MODE
-        ]
-      },
-      {
-        className: 'meta',
+      { className: 'meta',
         begin: '#', end: '$',
         keywords: {'meta-keyword': 'if else elseif end error'}
       },
-      {
-        className: 'function',
-        beginKeywords: 'function', end: '[{;]', excludeEnd: true,
+      { className: 'enum',
+        beginKeywords: 'enum', end: '{'
+      },
+      { className: 'abstract',
+        beginKeywords: 'abstract', end: '{',
         keywords: {
-          keyword: 'function ' + HAXE_TYPES
-        },
-        illegal: '\\S',
-        contains: [
-          hljs.TITLE_MODE,
-          {
-            className: 'params',
-            begin: '\\(', end: '\\)',
-            keywords: {
-              keyword: HAXE_TYPES
-            },
-            contains: [
-              hljs.APOS_STRING_MODE,
-              hljs.QUOTE_STRING_MODE,
-              hljs.C_LINE_COMMENT_MODE,
-              hljs.C_BLOCK_COMMENT_MODE
-            ]
-          },
-          {
-            begin: ':\\s*' + IDENT_FUNC_RETURN_TYPE_RE
-          }
-        ]
+          keyword: 'abstract from to'
+        }
+      },
+      { className: 'class',
+        beginKeywords: 'class interface', end: '{', excludeEnd: true,
+        keywords: {
+          keyword: 'extends implements'
+        }
       }
     ]
   };
