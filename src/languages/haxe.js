aa850d93f0a76d5a98c943a4ed3f48328cb0ae9b
Kenton Hamaluik <hamaluik@ualberta.ca>
2016-10-26 17:56:32 -0600

    fixed troublesome Haxe function types

--- src/languages/haxe.js
+++ src/languages/haxe.js
@@ -46,7 +46,7 @@ function(hljs) {
         keywords: {'meta-keyword': 'if else elseif end error'}
       },
       { className: 'type', // function types
-        begin: ':[ \t]*', end: '[^A-Za-z0-9_ \t(\\->)]',
+        begin: ':[ \t]*', end: '[^A-Za-z0-9_ \t\\->]',
         excludeBegin: true, excludeEnd: true,
       },
       { className: 'type', // types
