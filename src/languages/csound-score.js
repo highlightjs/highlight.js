c619f84cfe38cac8aff31e95e381c5af7b7546f8
Nathan Whetsell <nathan.whetsell@gmail.com>
2017-01-25 06:24:35 -0500

    Correctly highlight w and z in scores

--- src/languages/csound-score.js
+++ src/languages/csound-score.js
@@ -19,7 +19,17 @@ function(hljs) {
         className: 'keyword',
         // This avoids detecting ABNF as Csound scores.
         relevance: 0,
-        begin: /(?:[nN]|[pP])[pP]|[abCefimnqstvwxyz]/
+        begin: /(?:[nN]|[pP])[pP]|[abCefimnqstvxyz]/
+        // There is also a w statement that is generated internally and should
+        // not be used; see https://github.com/csound/csound/issues/750.
+      },
+      {
+        // z is a constant equal to 800,000,000,000. 800 billion seconds is
+        // about 25,367.8 years. See also
+        // https://csound.github.io/docs/manual/ScoreTop.html and
+        // https://github.com/csound/csound/search?q=stof+path%3AEngine+filename%3Asread.c.
+        className: 'built_in',
+        begin: /z/
       },
       Csound.NUMBER_MODE,
       {
