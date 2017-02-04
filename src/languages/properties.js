45749671558de7e5efa94299208410118a2cb94e
error418 <gerbigmic+github@googlemail.com>
2017-02-04 01:12:41 +0100

    code cleanup format code according to other files and remove obsolete code

--- src/languages/properties.js
+++ src/languages/properties.js
@@ -7,37 +7,35 @@ Category: common, config
 */
 
 function(hljs) {
-	return {
-		contains: [
-			hljs.COMMENT(
-				'^\\s*[\\!#]',
-				'$'
-			),
-			{
-				className: 'variable',
-				begin: /^[^:=]+/m,
-				illegal: ".*\\S\\s\\S.*",
-				relevance: 1
-			},
-			{
-				className: 'string',
-				begin: /[=:]\s*/,
-				end: /[^\\]$/,
-				excludeBegin: true,
-				relevance: 0,
-				contains: [
-					{
-						className: 'symbol',
-						begin: /\\(t|n|r|u[0-9A-Fa-f]{4})/
-					},
-					{
-						className: 'meta',
-						begin: /\\$/,
-						relevance: 1
-					},
-					"self"
-				]
-			}
-		]
-	};
+  return {
+    contains: [
+      hljs.COMMENT(
+        '^\\s*[\\!#]',
+        '$'
+      ),
+      {
+        className: 'variable',
+        begin: /^[^:=]+/m,
+        illegal: ".*\\S\\s\\S.*"
+      },
+      {
+        className: 'string',
+        begin: /[=:]\s*/,
+        end: /[^\\]$/,
+        excludeBegin: true,
+        relevance: 0,
+        contains: [
+          {
+            className: 'symbol',
+            begin: /\\(t|n|r|u[0-9A-Fa-f]{4})/
+          },
+          {
+            className: 'meta',
+            begin: /\\$/
+          },
+          "self"
+        ]
+      }
+    ]
+  };
 }
