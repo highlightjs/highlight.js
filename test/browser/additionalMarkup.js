1424faa19a57541d31a5ceac1976b3e76b710c3b
Aleksandr Zaytsev <ztsv@yandex-team.ru>
2017-03-17 12:00:17 +0300

    Escape quotes

--- test/browser/additionalMarkup.js
+++ test/browser/additionalMarkup.js
@@ -5,13 +5,11 @@ let jsdomEnv = bluebird.promisify(require('jsdom').env);
 let utility  = require('../utility');
 let glob     = bluebird.promisify(require('glob'));
 
-const text   = 'var say = "Hello";';
+const text   = `{"<a class="show-popup" data-attr="{&#34;popup-content&#34;:<div class=\&#34;popup-content\&#34;>html value inside data attr</div>&#34;}">key</a>": "value"}`;
 const html   = `<pre><code class="javascript">${text}</code></pre>`;
-const expect = '<span class="hljs-keyword">' +
-  'var</span> say = <span class="hljs-string">' +
-  '"Hello"</span>;';
+const expect = `{<span class="hljs-string">"</span><a class="show-popup" data-attr="{&quot;popup-content&quot;:<div class=&quot;popup-content&quot;>html value inside data attr</div>&quot;}"><span class="hljs-string">key</span></a><span class="hljs-string">"</span>: <span class="hljs-string">"value"</span>}`;
 
-describe('plain browser', function() {
+describe('additional markup inside code block', function() {
   before(function() {
     // Will match both `highlight.pack.js` and `highlight.min.js`
     const filepath = utility.buildPath('..', 'build', 'highlight.*.js');
--- test/browser/index.js
+++ test/browser/index.js
@@ -1,14 +1,7 @@
 'use strict';
 
 describe('browser build', function() {
-  before(function() {
-    this.text   = 'var say = "Hello";';
-    this.html   = `<pre><code class="javascript">${this.text}</code></pre>`;
-    this.expect = '<span class="hljs-keyword">' +
-                  'var</span> say = <span class="hljs-string">' +
-                  '"Hello"</span>;';
-  });
-
   require('./plain');
   require('./worker');
+  require('./additionalMarkup');
 });
