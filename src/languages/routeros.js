0cfc64723e9ffdd34d2ed7a7efd847fd5ee6d37a
divan1 <ivan_div@mail.ru>
2017-05-09 20:46:37 +0500

    Correcting some inaccuracies

--- src/languages/routeros.js
+++ src/languages/routeros.js
@@ -20,11 +20,11 @@ function(hljs) {
   var GLOBAL_COMMANDS = 'global local beep delay put len typeof pick log time set find environment terminal error execute parse resolve toarray tobool toid toip toip6 tonum tostr totime';
 
   // Common commands: Following commands available from most sub-menus:
-  var COMMON_COMMANDS = 'add remove enable disable set get print export edit find run';
+  var COMMON_COMMANDS = 'add remove enable disable set get print export edit find run debug error info warning';
 
   var LITERALS = 'true false yes no nothing nil null';
 
-  var OBJECTS = 'traffic-flow traffic-generator firewall scheduler aaa accounting address-list address align area bandwidth-server bfd bgp bridge client clock community config connection console customer default dhcp-client dhcp-server discovery dns e-mail ethernet filter firewall firmware gps graphing group hardware health hotspot identity igmp-proxy incoming instance interface ip ipsec ipv6 irq l2tp-server lcd ldp logging mac-server mac-winbox mangle manual mirror mme mpls nat nd neighbor network note ntp ospf ospf-v3 ovpn-server page peer pim ping policy pool port ppp pppoe-client pptp-server prefix profile proposal proxy queue radius resource rip ripng route routing screen script security-profiles server service service-port settings shares smb sms sniffer snmp snooper socks sstp-server system tool tracking type upgrade upnp user-manager users user vlan secret vrrp watchdog web-access wireless pptp pppoe lan wan layer7-protocol lease simple';
+  var OBJECTS = 'traffic-flow traffic-generator firewall scheduler aaa accounting address-list address align area bandwidth-server bfd bgp bridge client clock community config connection console customer default dhcp-client dhcp-server discovery dns e-mail ethernet filter firewall firmware gps graphing group hardware health hotspot identity igmp-proxy incoming instance interface ip ipsec ipv6 irq l2tp-server lcd ldp logging mac-server mac-winbox mangle manual mirror mme mpls nat nd neighbor network note ntp ospf ospf-v3 ovpn-server page peer pim ping policy pool port ppp pppoe-client pptp-server prefix profile proposal proxy queue radius resource rip ripng route routing screen script security-profiles server service service-port settings shares smb sms sniffer snmp snooper socks sstp-server system tool tracking type upgrade upnp user-manager users user vlan secret vrrp watchdog web-access wireless pptp pppoe lan wan layer7-protocol lease simple raw';
 
   // print parameters
   // Several parameters are available for print command:
@@ -94,8 +94,8 @@ function(hljs) {
         className: 'keyword',
         relevance: 10,
         variants: [
-          { begin: ':(' + STATEMENTS.split(' ').join('|') + ')',},
-          { begin: ':(' + GLOBAL_COMMANDS.split(' ').join('|') + ')',},
+          { begin: ':(' + STATEMENTS.split(' ').join('|') + ')\\s',},
+          { begin: ':(' + GLOBAL_COMMANDS.split(' ').join('|') + ')\\s',},
         ],
       },
 
