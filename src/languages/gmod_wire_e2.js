8b5015fd9acd366bb1e441e9bd643f2fd16498a7
revosw <revosw@gmail.com>
2017-06-24 13:09:33 +0200

    Update gmod_wire_e2.js

--- src/languages/gmod_wire_e2.js
+++ src/languages/gmod_wire_e2.js
@@ -7,6 +7,7 @@ Description: A language used in the Wiremod extension for Garry's Mod
 
 function(hljs) {
   return {
+    alias: ["e2", "wire_e2", "gmod_wire_e2", "expression2", "exp2", "expr2"],
     keywords: 'if|0 elseif|0 else|0 for|0 foreach|0 while|0 break|0 continue|0 local|0 switch|0 case|0 default|0 function|0 return|0',
     contains: [
       {
@@ -29,7 +30,7 @@ function(hljs) {
           {begin: /@(inputs|outputs)/},
           {begin: /@trigger/, end: /none|all|$/}
         ],
-        relevance: 0
+        relevance: 10
         //begin: /@(name[^#]*|model.*|inputs|outputs|persist|trigger( +(all|none))?|autoupdate.*)/
       },
       {
