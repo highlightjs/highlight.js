4f24baa0a1d50bf9bbdd2146a335ea4a5ffad805
Bill Wagner <wiwagn@microsoft.com>
2017-07-06 13:47:08 -0400

    Create an ANTLR highlighter

---
+++ src/languages/antlr.js
@@ -0,0 +1,31 @@
+/*
+Language: Antlr
+Author: Bill Wagner <wiwagn@microsoft.com>
+Website: http://thebillwagner.com/
+Description: Language definition for grammar described using ANTLR
+*/
+
+function(hljs) {
+    var ruleDeclarationRegEx = "^[a-z][a-z_]*";
+
+  return {
+    keywords: ['parser', 'grammar', 'options'],
+    aliases: ['antlr'],
+    case_insensitive: false,
+    illegal: '<!--|-->|</|::|`|\\s=\\s',
+    contains: [
+      hljs.C_BLOCK_COMMENT_MODE,
+      hljs.C_LINE_COMMENT_MODE,
+      hljs.HASH_COMMENT_MODE,
+      hljs.APOS_STRING_MODE,
+      {
+         className: 'title',
+         begin: '^' + hljs.UNDERSCORE_IDENT_RE,
+         relevance: 0
+      },
+      {
+          begin: ';|\\||:' // relevance booster
+      }
+    ]
+  };
+}
