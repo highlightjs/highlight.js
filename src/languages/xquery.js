616d95136bd9b180ce46e641ab2a372dce37aeb3
duncdrum <d.paterson@me.com>
2017-11-22 19:17:50 +0100

    op and fn work its a hack

--- src/languages/xquery.js
+++ src/languages/xquery.js
@@ -39,16 +39,18 @@ function(hljs) {
       end: /(?:a(?:cos|sin|tan[2]?)|cos|exp[10]*|log[10]*|pi|pow|sin|sqrt|tan)\b/
     }, {
       begin: /\bop\:/,
-      end: /(?:(?:add|subtract|divide)\-(?:dayTimeDuration.*|yearMonthDuration.*|dateTimes|dates|times)(?:(?:to|from)\-(?:date(?:Time)*|time))*|(?:boolean|date(?:Time)*|time|(?:base64|hex)Binary)\-(?:equal|(?:greater|less)\-than)|(?:duration|gMonth(?:Day)*|gYear(?:Month)*|gDay|NOTATION|QName)\-equal|numeric\-(?:add|divide|equal|(?:greater|less)\-than|integer\-divide|mod|multiply|subtract|unary\-minus|unary\-plus)|(?:dayTimeDuration|yearMonthDuration)\-(?:greater|less)\-than|multiply\-(?:dayTimeDuration|yearMonthDuration)|same\-key)\b/
+      end: /\(/,
+      excludeEnd: true
     }, {
-      begin: /\b(?:fn\:|(?!^$))/,
-      end: /(?:abs|accumulator\-(?:after|before)|adjust\-(?:date(?:Time)*|time)\-to\-timezone|analyze\-string|apply|available\-(?:environment\-variables|system\-properties)|avg|base\-uri|boolean|ceiling|codepoint[s]*\-(?:equal|to\-string)|collation\-key|collection|compare|concat|contains(?:\-token)*|copy\-of|count|current(?:\-date(?:Time)*|\-time|\-grou(?:ping\-key)*|\-output\-uri|\-merge\-(?:group|key))*|data|dateTime|day[s]*\-from\-(?:date(?:Time)*|duration)|deep\-equal|default\-(?:collation|language)|distinct\-values|document(?:\-uri)*|doc(?:\-available)*|element\-(?:available|with\-id)|empty|encode\-for\-uri|ends\-with|environment\-variable|error|escape\-html\-uri|exactly\-one|exists|false|filter|floor|fold\-(?:left|right)|for\-each(?:\-pair)*|format\-(?:date(?:Time)*|time|integer|number)|function\-(?:arity|available|lookup|name)|generate\-id|has\-children|head|hours\-from\-(?:dateTime|duration|time)|id(?:ref)*|implicit\-timezone|in\-scope\-prefixes|inde[x]\-of|innermost|insert\-before|iri\-to\-uri|json\-(?:doc|to\-xml)|key|lang|last|load\-xquery\-module|local\-name(?:\-from\-QName)*|(?:lower|upper)\-case|matches|max|minutes\-from\-(?:dateTime|duration|time)|min|month[s]*\-from\-(?:date(?:Time)*|duration)|name(?:space\-uri(?:\-for\-prefix|\-from\-QName)*)*|nilled|node\-name|normalize\-(?:space|unicode)|not|number|one\-or\-more|outermost|parse\-(?:ietf\-date|json)|path|position|(?:prefix\-from\-)*QName|random\-number\-generator|regex\-group|remove|replace|resolve\-(?:QName|uri)|reverse|root|round(?:\-half\-to\-even)*|seconds\-from\-(?:dateTime|duration|time)|snapshot|sort|starts\-with|static\-base\-uri|stream\-available|string(?:\-join|\-length|\-to\-codepoints)*|subsequence|substring(?:\-after|\-before)*|sum|system\-property|tail|timezone\-from\-(?:date(?:Time)*|time)|tokenize|trace|transform|translate|true|type\-available|unordered|unparsed\-(?:entity|text)*(?:\-(?:public\-id|uri|available|lines))*|uri\-collection|xml\-to\-json|year[s]*\-from\-(?:date(?:Time)*|duration)|zero\-or\-one)\b/
+      begin: /\bfn\:/,
+      end: /\(/,
+      excludeEnd: true
     }
   ]
   };
 
   var VAR = {
-    begin: /\b\$[A-Za-z0-9_\-]+/
+    begin: /\b\$[A-Za-z0-9_\-]+\b/
   };
 
   var SYMBOL = 'le gt eq => div idiv';
