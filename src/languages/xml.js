70d9cac8f21030a8515715408ac3e27bb70406db
Bruno Philipe <git@bruno.ph>
2017-12-02 20:54:31 +0100

    Fix PHP strings backslash escape inside XML

--- src/languages/xml.js
+++ src/languages/xml.js
@@ -56,15 +56,18 @@ function(hljs) {
       {
         className: 'meta',
         begin: /<\?xml/, end: /\?>/, relevance: 10
-      }, {
+      },
+      {
         begin: /<\?(php)?/, end: /\?>/,
         subLanguage: 'php',
         contains: [
+          // We don't want the php closing tag ?> to close the PHP block when
+          // inside any of the following blocks:
           {begin: '/\\*', end: '\\*/', skip: true},
           {begin: 'b"', end: '"', skip: true},
           {begin: 'b\'', end: '\'', skip: true},
-          hljs.inherit(hljs.APOS_STRING_MODE, {illegal: null, className: null, skip: true}),
-          hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null, className: null, skip: true})
+          hljs.inherit(hljs.APOS_STRING_MODE, {illegal: null, className: null, contains: null, skip: true}),
+          hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null, className: null, contains: null, skip: true})
         ]
       },
       {
