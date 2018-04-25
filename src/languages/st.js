f0096e5b225a804bc1c5ceabef97e0f83d719abe
Sergey Romanov <sergey.romanov@gmail.com>
2018-04-25 12:03:19 +0600

    Change some elements to titles and keywords from built_in

--- src/languages/st.js
+++ src/languages/st.js
@@ -11,11 +11,11 @@ function(hljs) {
         keywords: {
             keyword: 'if then end_if elsif else case of end_case ' +
             'to do by while repeat end_while end_repeat for end_for ' + 
-            'var var_global end_var var_input var_out var_output var_in_out var_temp var_interval var_external ' +
-            'or and not xor ',
-            class: 'program end_program function end_function function_block end_function_block configuration end_configuration action end_action transition end_transition type end_type struct end_struct',
+            'or and not xor constant return exit at retain non_retain task with until',
+            title: 'program end_program function end_function function_block end_function_block configuration end_configuration action end_action transition end_transition type end_type struct end_struct ' +
+            'var var_global end_var var_input var_out var_output var_in_out var_temp var_interval var_external ',
             literal: 'false true null ',
-            built_in: 'constant return exit at retain non_retain array pointer int sint dint lint usint uint udint ulint real lreal time date time_of_day date_and_time dt tod string bool byte world dworld lworld task with until',
+            built_in: 'array pointer int sint dint lint usint uint udint ulint real lreal time date time_of_day date_and_time dt tod string bool byte world dworld lworld',
             function: 'mod abs acos asin atan cos exp expt ln log sin sqrt tan sel max min limit mux shl shr rol ror indexof sizeof adr adrinst bitadr add mul div sub trunc move',
         },
         contains: [
