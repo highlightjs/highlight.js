989ce252177d9ebbd881289be3f49500a072dad1
Sergey Romanov <sergey.romanov@gmail.com>
2018-04-30 18:02:50 +0600

    new keywords

--- src/languages/st.js
+++ src/languages/st.js
@@ -15,7 +15,7 @@ function(hljs) {
             'to do by while repeat end_while end_repeat for end_for from ' + 
             'or and not xor constant return exit at retain non_retain task with until',
             title: 'program end_program function end_function function_block end_function_block configuration end_configuration action end_action transition end_transition type end_type struct end_struct step end_step initial_step' +
-            'var var_global end_var var_input var_out var_output var_in_out var_temp var_interval var_external ',
+            'var var_global end_var var_input var_out var_output var_in_out var_temp var_interval var_external var_access var_config',
             literal: 'false true null ',
             built_in: 'array pointer int sint dint lint usint uint udint ulint real lreal time date time_of_day date_and_time dt tod string bool byte world dworld lworld',
             function: 'mod abs acos asin atan cos exp expt ln log sin sqrt tan sel max min limit mux shl shr rol ror indexof sizeof adr adrinst bitadr add mul div sub trunc move',
