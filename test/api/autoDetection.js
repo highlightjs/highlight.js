53118b3ea8db5af84597d9c5bd973bb6867a20f3
Egor Rogov <e.rogov@postgrespro.ru>
2018-10-16 16:01:05 +0300

    Fix autoDetection to accept language aliases (#1873)
    * Fix for #1868

--- test/api/autoDetection.js
+++ test/api/autoDetection.js
@@ -3,33 +3,33 @@
 let hljs   = require('../../build');
 let should = require('should');
 
-describe('.getLanguage()', function() {
+describe('.autoDetection()', function() {
   it('should get an existing language', function() {
-    const result = hljs.getLanguage('python');
+    const result = hljs.autoDetection('python');
 
     result.should.be.instanceOf(Object);
   });
 
   it('should get an existing language by alias', function() {
-    const result = hljs.getLanguage('py');
+    const result = hljs.autoDetection('py');
 
     result.should.be.instanceOf(Object);
   });
 
   it('should be case insensitive', function() {
-    const result = hljs.getLanguage('pYTHOn');
+    const result = hljs.autoDetection('pYTHOn');
 
     result.should.be.instanceOf(Object);
   });
 
   it('should return undefined', function() {
-    const result = hljs.getLanguage('-impossible-');
+    const result = hljs.autoDetection('-impossible-');
 
     should.strictEqual(result, undefined);
   });
 
   it('should not break on undefined', function() {
-    const result = hljs.getLanguage(undefined);
+    const result = hljs.autoDetection(undefined);
 
     should.strictEqual(result, undefined);
   });
