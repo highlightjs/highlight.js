f8f81e62d99d7286d710588869889dd09e3bdfa3
Johannes Müller <straightshoota@gmail.com>
2018-10-21 18:52:47 +0200

    Use hljs.RE_STARTERS_RE

--- src/languages/crystal.js
+++ src/languages/crystal.js
@@ -7,8 +7,6 @@ function(hljs) {
   var INT_SUFFIX = '(_*[ui](8|16|32|64|128))?';
   var FLOAT_SUFFIX = '(_*f(32|64))?';
   var CRYSTAL_IDENT_RE = '[a-zA-Z_]\\w*[!?=]?';
-  var RE_STARTER = '!=|!==|%(?!})|%=|&|&&|&=|\\*|\\*=|\\+|\\+=|,|-|-=|/=|:|;|<<|<<=|<=|<|===|==|=|>>>=|>>=|>=|>>>|' +
-    '>>|>|\\[|\\{|\\(|\\^|\\^=|\\||\\|=|\\|\\||~|//=|\\n';
   var CRYSTAL_METHOD_RE = '[a-zA-Z_]\\w*[!?=]?|[-+~]\\@|<<|>>|=~|===?|<=>|[<>]=?|\\*\\*|[-/+%^&*~|]|//|//=|&[-+*]=?|&\\*\\*|\\[\\][=?]?';
   var CRYSTAL_PATH_RE = '[A-Za-z_]\\w*(::\\w+)*(\\?|\\!)?';
   var CRYSTAL_KEYWORDS = {
@@ -68,7 +66,7 @@ function(hljs) {
     relevance: 0,
   };
   var REGEXP = {
-    begin: '(' + RE_STARTER + ')\\s*',
+    begin: '(?!%})(' + hljs.RE_STARTERS_RE + '|\\n)\\s*',
     contains: [
       {
         className: 'regexp',
