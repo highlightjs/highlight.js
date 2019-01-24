4e7ad8d679a40742e690058ce691298b807caad0
Kevin Moore <kevmoo@users.noreply.github.com>
2019-01-23 19:25:13 -0800

    refactor(dart): Update file description (#1948)
    No longer trying to be a JS-replacement. Updated URL

--- src/languages/dart.js
+++ src/languages/dart.js
@@ -2,7 +2,7 @@
 Language: Dart
 Requires: markdown.js
 Author: Maxim Dikun <dikmax@gmail.com>
-Description: Dart is a JavaScript replacement language developed by Google. For more information see http://dartlang.org/
+Description: Dart a modern, object-oriented language developed by Google. For more information see https://www.dartlang.org/
 Category: scripting
 */
 
