bc52787345ab871a2108e75964b71c55a5fad184
Greg Cline <gregrcline@gmail.com>
2019-01-27 11:58:13 -0500

    Got class highlighting working as I would like and added additional tests

--- src/languages/pony.js
+++ src/languages/pony.js
@@ -50,8 +50,13 @@ function(hljs) {
 
   var CLASS = {
     className: 'class',
-    beginKeywords: 'class actor object', end: '$',
+    beginKeywords: 'class actor object primitive', end: '$',
     contains: [
+      {
+        className: 'keyword',
+        begin: 'is'
+      },
+      TYPE_NAME,
       hljs.TITLE_MODE,
       hljs.C_LINE_COMMENT_MODE
     ]
