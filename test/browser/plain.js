5ab5ed8518d896618e4e787a19a758ba7d3fe3ae
Kagami Sascha Rosylight <saschanaz@outlook.com>
2019-02-26 22:21:14 +0900

    load manually

--- test/browser/plain.js
+++ test/browser/plain.js
@@ -4,6 +4,7 @@ let bluebird = require('bluebird');
 let { JSDOM } = require('jsdom');
 let utility  = require('../utility');
 let glob     = bluebird.promisify(require('glob'));
+let fs       = require('fs');
 
 describe('plain browser', function() {
   before(function() {
@@ -11,8 +12,9 @@ describe('plain browser', function() {
     const filepath = utility.buildPath('..', 'build', 'highlight.*.js');
 
     return glob(filepath)
-      .then(hljsPath => hljsPath.map(file => `<script src="${file}"></script>`).join(""))
-      .then(hljsScript => new JSDOM(hljsScript + this.html, { resources: "usable", runScripts: "dangerously" }))
+      .then(hljsPath => hljsPath.map(path => fs.readFileSync(path, 'utf8')))
+      .then(hljsFiles => hljsFiles.map(file => `<script>${file}</script>`).join(""))
+      .then(hljsScript => new JSDOM(hljsScript + this.html, { runScripts: "dangerously" }))
       .then(({ window }) => {
         this.block = window.document.querySelector('pre code');
         this.hljs  = window.hljs;
