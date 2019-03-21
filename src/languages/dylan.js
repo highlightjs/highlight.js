8099701c36959da48969d6ca455328aa41f0e9ae
Peter Hull <peterhull90@hotmail.com>
2019-03-21 08:01:51 +0000

    Add support for Dylan language

---
+++ src/languages/dylan.js
@@ -0,0 +1,59 @@
+/*
+Language: Dylan
+Author: P Hull <peterhull90@gmail.com>
+Category: functional
+*/
+
+function(hljs) {
+    var DYLAN_CORE_WORDS = ["define", "end", "handler", "let", "local", "macro", "otherwise"],
+        DYLAN_BEGIN_WORDS = ["begin", "block", "case", "for", "if", "method",
+            "select", "unless", "until", "while"
+        ],
+        DYLAN_FUNCTION_WORDS = [],
+        DYLAN_DEFINE_BODY_WORDS = ["class", "library", "method", "module"],
+        DYLAN_DEFINE_LIST_WORDS = ["constant", "variable", "domain"],
+        DYLAN_RESERVED_WORDS = [].concat(DYLAN_CORE_WORDS.map(word => word + '|10'),
+            DYLAN_BEGIN_WORDS,
+            DYLAN_FUNCTION_WORDS,
+            DYLAN_DEFINE_BODY_WORDS.map(word => word + '|10'),
+            DYLAN_DEFINE_LIST_WORDS
+        ),
+        DYLAN_HASH_WORDS = ["#t", "#f", "#next", "#rest", "#key", "#all-keys", "#include"],
+        DYLAN_WORD = '[a-z\-+\*/^=#!%$_><@\?~][a-z0-9\-+\*/^=#!%$_><@\?~]+',
+        KEYWORDS = {
+            literal: DYLAN_HASH_WORDS.join(" "),
+            keyword: DYLAN_RESERVED_WORDS.join(" ")
+        },
+        DYLAN_CODE = {
+            case_insensitive: true,
+            className: 'dylan',
+            lexemes: DYLAN_WORD,
+            keywords: KEYWORDS,
+            contains: [{
+                    className: 'class',
+                    begin: '<' + DYLAN_WORD + '>'
+                },
+                {
+                    className: 'symbol',
+                    begin: '#',
+                    end: /[ \(\)\{\}\[\]]/
+                },
+                {
+                    className: 'symbol',
+                    begin: DYLAN_WORD + ':'
+                },
+                {
+                    className: 'string',
+                    begin: '\'\\\\?.',
+                    end: '\'',
+                    illegal: '.'
+                },
+                hljs.C_NUMBER_MODE,
+                hljs.QUOTE_STRING_MODE,
+                hljs.C_LINE_COMMENT_MODE,
+                hljs.C_BLOCK_COMMENT_MODE
+            ]
+        };
+
+    return DYLAN_CODE;
+}
