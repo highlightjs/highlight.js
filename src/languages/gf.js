facf8c470d321c38d10a4653d271ef18da636ce4
John J. Camilleri <john@digitalgrammars.com>
2019-04-29 14:25:30 +0200

    Add some relevance annotations, now passes tests

--- src/languages/gf.js
+++ src/languages/gf.js
@@ -22,20 +22,22 @@ function(hljs) {
 
   var PRAGMA = {
     className: 'meta',
-    begin: '--#', end: '$'
+    begin: '--#', end: '$',
+    relevance: 0
   };
 
   var SYMBOL = {
     className: 'symbol',
-    begin: '=>|->|:|=|\\.|\\+|\\*|\!|\\||\\\\'
+    begin: '=>|->|:|=|\\.|\\+|\\*|\!|\\||\\\\',
+    relevance: 0
   };
 
   return {
     aliases: ['gf'],
     keywords:
       'abstract concrete interface instance resource incomplete of with open in ' +
-      'cat fun def data lincat lin lindef linref printname printname param oper flags ' +
-      'table pre case variants let in where ',
+      'cat fun def data lincat|10 lin lindef|10 linref|10 printname printname param oper flags ' +
+      'table|10 pre case variants let in where ',
     contains: [
       PRAGMA,
       COMMENT,
@@ -45,7 +47,7 @@ function(hljs) {
       hljs.NUMBER_MODE,
       hljs.inherit(hljs.TITLE_MODE, {begin: '^[_a-zA-Z][\\w\']*'}),
 
-      {begin: '{\s*s\s*:'} // No markup, relevance booster
+      {begin: '{\s*s\s*:', relevance: 10} // No markup, relevance booster
     ]
   };
 }
