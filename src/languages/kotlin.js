52925a627f20cbc28bc4eaeffd1824244e6e895d
Chuck Jazdzewski <chuckj@google.com>
2019-08-23 02:15:11 -0700

    fix(kotlin): Support substitutions with nested substitutions (#2112)

--- src/languages/kotlin.js
+++ src/languages/kotlin.js
@@ -38,7 +38,7 @@ function(hljs) {
   // for string templates
   var SUBST = {
     className: 'subst',
-    begin: '\\${', end: '}', contains: [hljs.APOS_STRING_MODE, hljs.C_NUMBER_MODE]
+    begin: '\\${', end: '}', contains: [hljs.C_NUMBER_MODE]
   };
   var VARIABLE = {
     className: 'variable', begin: '\\$' + hljs.UNDERSCORE_IDENT_RE
@@ -65,6 +65,7 @@ function(hljs) {
       }
     ]
   };
+  SUBST.contains.push(STRING)
 
   var ANNOTATION_USE_SITE = {
     className: 'meta', begin: '@(?:file|property|field|get|set|receiver|param|setparam|delegate)\\s*:(?:\\s*' + hljs.UNDERSCORE_IDENT_RE + ')?'
