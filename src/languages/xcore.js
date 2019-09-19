94fe60d6dd02683b1a5492f620d5daeea388716e
Gerson Sunyé <gerson.sunye@gmail.com>
2019-09-19 11:03:38 +0200

    Improvements on Xcore syntax.
    - Added a new module "class" to Xcore.

--- src/languages/xcore.js
+++ src/languages/xcore.js
@@ -8,27 +8,40 @@ Category: mde
 function(hljs) {
   return {
     keywords: {
-      keyword:
-      'package import abstract class extends enum interface type wraps op '+
-      'annotation as '+
-      'contains container opposite refers val '+
-      'derived get set unsettable '+
-      'for try catch if else throw return '+
-      'create convert new ',
-      literal:
-        'true false null ',
-      built_in:
-        'BigDecimal BigInteger Boolean Byte Character Class Date Double Float Integer Long Object Short ' +
-        'String boolean int ' 
+      keyword: 'package import abstract class extends enum interface type wraps op ' +
+        'annotation as ' +
+        'contains container opposite refers val ' +
+        'derived get set unsettable ' +
+        'for try catch if else throw return ' +
+        'create convert new ' +
+        '@Ecore @Import @GenModel ',
+      literal: 'true false null ',
+      built_in: 'BigDecimal BigInteger Boolean Byte Character Class Date Double Float Integer Long Object Short ' +
+        'String boolean int '
     },
-    contains: [ 
-      {
+    contains: [{
         className: 'string',
-        begin: '\'', end: '\'',
-        contains: [hljs.BACKSLASH_ESCAPE, {begin: '""'}]
+        begin: '\'',
+        end: '\'',
+        contains: [hljs.BACKSLASH_ESCAPE, {
+          begin: '""'
+        }]
       },
-      hljs.NUMBER_MODE, 
-      hljs.COMMENT('--', '$') ]
+      hljs.NUMBER_MODE,
+      hljs.COMMENT('--', '$'),
+      {
+        className: 'class',
+        beginKeywords: 'class interface',
+        end: /[{;=]/,
+        excludeEnd: true,
+        keywords: 'class extends interface',
+        illegal: /[:"\[\]]/,
+        contains: [{
+            beginKeywords: 'extends'
+          },
+          hljs.UNDERSCORE_TITLE_MODE
+        ]
+      }
+    ]
   }
 }
-
