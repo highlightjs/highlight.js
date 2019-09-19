a097965b04f02e67d54687c09c4fa85904ecfd0c
Gerson Sunyé <gerson.sunye@gmail.com>
2019-09-19 10:46:48 +0200

    ATL syntax improvement
    - Module defintion for module and rule
    - Changed ATL and Xcore examples for better automatic detection

--- src/languages/atl.js
+++ src/languages/atl.js
@@ -7,37 +7,61 @@ Category: mde
 
 function(hljs) {
   return {
+    case_insensitive: false,
+    beginKeywords: 'module rule ',
     keywords: {
-      keyword:
-          'module rule to from thisModule abstract query IN OUT extends helper '+
-          'create refining ' +
-          'select collect reject forAll exists collectNested closure iterate flatten one '+
-          'includes excludes excludesAll includesAll including excluding union intersection ' +
-          'size avg isEmpty notEmpty count sum average '+
-          'abs div mod max min floor round '+
-          'substring concat toInteger toReal toUpper toLower '+
-          'result    let in '+
-          'self each'+
-          'or xor and not implies '+
-          'oclIsUndefined oclIsInvalid hasReturned '+
-          'if then else endif '  +
-          'package, endpackage ' +
-          'asBag  asSequence asOrderedSet asSet ' +
-          'oclIsTypeOf oclIsKindOf oclInState oclIsNew oclIsUndefined oclIsInvalid oclAsType allInstances ' +
-          '@pre ',
-          literal:
-            'true false null unknown ',
-          built_in:
-            'Boolean Integer UnlimitedNatural Real String OrderedSet Tuple OrderedSet Bag Set Sequence OclInvalid OclVoid TupleType OclState Collection OclMessage ' 
+      keyword: 'module rule to from thisModule abstract query IN OUT extends helper def uses ' +
+        'create refining ' +
+        'select collect reject forAll exists collectNested closure iterate flatten one first ' +
+        'includes excludes excludesAll includesAll including excluding union intersection ' +
+        'size avg isEmpty notEmpty count sum average ' +
+        'abs div mod max min floor round ' +
+        'substring concat toInteger toReal toUpper toLower ' +
+        'result    let in ' +
+        'self each' +
+        'or xor and not implies ' +
+        'oclIsUndefined oclIsInvalid hasReturned ' +
+        'if then else endif ' +
+        'package, endpackage ' +
+        'asBag  asSequence asOrderedSet asSet ' +
+        'oclIsTypeOf oclIsKindOf oclInState oclIsNew oclIsUndefined oclIsInvalid oclAsType allInstances ' +
+        '@pre ',
+      literal: 'true false null unknown ',
+      built_in: 'Boolean Integer UnlimitedNatural Real String OrderedSet Tuple OrderedSet Bag Set Sequence OclInvalid OclVoid TupleType OclState Collection OclMessage '
     },
-    contains: [ 
-      {
+    contains: [{
         className: 'string',
-        begin: '\'', end: '\'',
-        contains: [hljs.BACKSLASH_ESCAPE, {begin: '\'\''}]
+        begin: '\'',
+        end: '\'',
+        contains: [hljs.BACKSLASH_ESCAPE]
       },
-      hljs.NUMBER_MODE, 
-      hljs.COMMENT('--', '$') ]
+      hljs.NUMBER_MODE,
+      hljs.COMMENT('--', '$'),
+      {
+        className: 'rule',
+        beginKeywords: 'rule abstract',
+        end: /[{;=]/,
+        excludeEnd: true,
+        keywords: 'rule abstract',
+        illegal: /[:"\[\]]/,
+        contains: [{
+            beginKeywords: 'extends'
+          },
+          hljs.UNDERSCORE_TITLE_MODE
+        ]
+      },
+      {
+        className: 'class',
+        beginKeywords: 'module',
+        end: '$',
+        keywords: 'module',
+        illegal: /[{}$]/,
+        contains: [{
+          beginKeywords: 'module'
+        },
+        hljs.UNDERSCORE_TITLE_MODE
+        ]
+      }
+    ]
   }
 }
-
