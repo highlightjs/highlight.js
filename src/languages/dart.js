c4b48f8fc127d046baf2368dd6b3f2b9bda436af
Sam Rawlins <srawlins@google.com>
2019-09-23 09:30:13 -0700

    Fix last line

--- src/languages/dart.js
+++ src/languages/dart.js
@@ -67,7 +67,7 @@ function (hljs) {
     keyword: 'abstract as assert async await break case catch class const continue covariant default deferred do ' +
       'dynamic else enum export extends extension external factory false final finally for Function get hide if ' +
       'implements import in inferface is library mixin new null on operator part rethrow return set show static ' +
-    'super switch sync this throw true try typedef var void while with yield'
+      'super switch sync this throw true try typedef var void while with yield',
     built_in:
       // dart:core
       'Comparable DateTime Duration Function Iterable Iterator List Map Match Null Object Pattern RegExp Set ' +
