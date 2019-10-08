4751f414ab40d51cf49b124f5c6421419e81e7b7
Nils Knappmeier <github@knappi.org>
2019-10-08 22:44:49 +0200

    fix(Handlebars): add support for raw-blocks and triple-mustaches
    - {{{{raw}}}} {{verbatim parts}} {{{{/raw}}}} {{handlebars again}}
    - {{{epxression that is not HTML escaped}}}
    
    are now supported

--- src/languages/handlebars.js
+++ src/languages/handlebars.js
@@ -5,35 +5,57 @@ Author: Robin Ward <robin.ward@gmail.com>
 Description: Matcher for Handlebars as well as EmberJS additions.
 Category: template
 */
-
 function(hljs) {
   var BUILT_INS = {'builtin-name': 'each in with if else unless bindattr action collection debugger log outlet template unbound view yield'};
+  function MUSTACHE_CONTENTS() {
+    return  {
+      className: 'name',
+      begin: /[a-zA-Z\.-]+/,
+      keywords: BUILT_INS,
+      starts: {
+        endsWithParent: true, relevance: 0,
+        contains: [
+          hljs.QUOTE_STRING_MODE
+        ]
+      }
+    };
+  }
+
   return {
     aliases: ['hbs', 'html.hbs', 'html.handlebars'],
     case_insensitive: true,
     subLanguage: 'xml',
     contains: [
-    hljs.COMMENT('{{!(--)?', '(--)?}}'),
+      hljs.COMMENT('{{!(--)?', '(--)?}}'),
+      // raw block {{{{raw}}}} verbatim xml {{{{/raw}} {{handlebars}}
       {
         className: 'template-tag',
-        begin: /\{\{[#\/]/, end: /\}\}/,
-        contains: [
-          {
-            className: 'name',
-            begin: /[a-zA-Z\.-]+/,
-            keywords: BUILT_INS,
-            starts: {
-              endsWithParent: true, relevance: 0,
-              contains: [
-                hljs.QUOTE_STRING_MODE
-              ]
-            }
-          }
-        ]
+        begin: /\{\{\{\{\//, end: /\}\}\}\}/,
+        contains: [MUSTACHE_CONTENTS()]
+      },
+      {
+        className: 'template-tag',
+        begin: /\{\{\{\{/, end: /\}\}\}\}/,
+        contains: [MUSTACHE_CONTENTS()],
+        starts: {
+          end: '{{{{/',
+          returnEnd: true,
+          subLanguage: 'xml'
+        }
+     },
+      {
+        className: 'template-tag',
+        begin: /{{[#\/]/, end: /}}/,
+        contains: [MUSTACHE_CONTENTS()],
       },
       {
         className: 'template-variable',
-        begin: /\{\{/, end: /\}\}/,
+        begin: '{{{', end: '}}}',
+        keywords: BUILT_INS
+      },
+      {
+        className: 'template-variable',
+        begin: '{{', end: '}}',
         keywords: BUILT_INS
       }
     ]
