873d7c93972955b76985c078a4c04db87a9ebb88
Nils Knappmeier <github@knappi.org>
2019-10-14 09:28:42 -0400

    add tests and ability to deregister language

--- src/highlight.js
+++ src/highlight.js
@@ -779,6 +779,15 @@ https://highlightjs.org/
     }
   }
 
+  function deregisterLanguage(name, language) {
+    var lang = languages[name]
+
+    delete languages[name]
+    if (lang.aliases) {
+      lang.aliases.forEach(function(alias) {delete aliases[alias];});
+    }
+  }
+
   function listLanguages() {
     return objectKeys(languages);
   }
@@ -803,6 +812,7 @@ https://highlightjs.org/
   hljs.initHighlighting = initHighlighting;
   hljs.initHighlightingOnLoad = initHighlightingOnLoad;
   hljs.registerLanguage = registerLanguage;
+  hljs.deregisterLanguage = deregisterLanguage;
   hljs.listLanguages = listLanguages;
   hljs.getLanguage = getLanguage;
   hljs.autoDetection = autoDetection;
