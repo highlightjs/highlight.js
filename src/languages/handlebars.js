bdf87d290e2d37540c1e1910840a3d24cd3069ff
Nils Knappmeier <github@knappi.org>
2019-10-15 16:47:14 +0200

    (handlebars)  support for segment-literals and escaped mustaches (#2184)

--- src/languages/handlebars.js
+++ src/languages/handlebars.js
@@ -6,63 +6,91 @@ Description: Matcher for Handlebars as well as EmberJS additions.
 Website: https://handlebarsjs.com
 Category: template
 */
-function(hljs) {
+function (hljs) {
   var BUILT_INS = {'builtin-name': 'each in with if else unless bindattr action collection debugger log outlet template unbound view yield lookup'};
-  function MUSTACHE_CONTENTS() {
-    return  {
-      className: 'name',
-      begin: /[a-zA-Z\.-]+/,
+
+  var IDENTIFIER_PLAIN_OR_QUOTED = {
+    begin: /".*?"|'.*?'|\[.*?\]|\w+/
+  };
+
+  function BLOCK_MUSTACHE_CONTENTS() {
+    return hljs.inherit(EXPRESSION_OR_HELPER_CALL(), {
+      className: 'name'
+    });
+  }
+
+  function BASIC_MUSTACHE_CONTENTS() {
+    return hljs.inherit(EXPRESSION_OR_HELPER_CALL(), {
+      // relevance 0 for backward compatibility concerning auto-detection
+      relevance: 0
+    });
+  }
+
+  function EXPRESSION_OR_HELPER_CALL() {
+    return hljs.inherit(IDENTIFIER_PLAIN_OR_QUOTED, {
       keywords: BUILT_INS,
-      starts: {
-        endsWithParent: true, relevance: 0,
-        contains: [
-          hljs.QUOTE_STRING_MODE
-        ]
-      }
+      starts: HELPER_PARAMETERS()
+    });
+  }
+
+  function HELPER_PARAMETERS() {
+    return {
+      endsWithParent: true,
+      relevance: 0,
+      contains: [
+        hljs.inherit(IDENTIFIER_PLAIN_OR_QUOTED, {
+          relevance: 0
+        })
+      ]
     };
   }
+
+  var ESCAPE_MUSTACHE_WITH_PRECEEDING_BACKSLASH = {begin: /\\\{\{/, skip: true};
+  var PREVENT_ESCAPE_WITH_ANOTHER_PRECEEDING_BACKSLASH = {begin: /\\\\(?=\{\{)/, skip: true};
 
   return {
     aliases: ['hbs', 'html.hbs', 'html.handlebars'],
     case_insensitive: true,
     subLanguage: 'xml',
     contains: [
+      ESCAPE_MUSTACHE_WITH_PRECEEDING_BACKSLASH,
+      PREVENT_ESCAPE_WITH_ANOTHER_PRECEEDING_BACKSLASH,
       hljs.COMMENT(/\{\{!--/, /--\}\}/),
       hljs.COMMENT(/\{\{!/, /\}\}/),
-      // raw block (open) {{{{raw}}}} verbatim xml {{{{/raw}} {{handlebars}}
       {
+        // open raw block "{{{{raw}}}} content not evaluated {{{{/raw}}}}"
         className: 'template-tag',
         begin: /\{\{\{\{(?!\/)/, end: /\}\}\}\}/,
-        contains: [MUSTACHE_CONTENTS()],
-        starts: {
-          end: /\{\{\{\{\//,
-          returnEnd: true,
-          subLanguage: 'xml'
-        }
+        contains: [BLOCK_MUSTACHE_CONTENTS()],
+        starts: {end: /\{\{\{\{\//, returnEnd: true, subLanguage: 'xml'}
       },
-      // raw block (close)
       {
+        // close raw block
         className: 'template-tag',
         begin: /\{\{\{\{\//, end: /\}\}\}\}/,
-        contains: [MUSTACHE_CONTENTS()]
+        contains: [BLOCK_MUSTACHE_CONTENTS()]
       },
-      // standard blocks {{#block}} ... {{/block}}
       {
+        // open block statement
         className: 'template-tag',
         begin: /\{\{[#\/]/, end: /\}\}/,
-        contains: [MUSTACHE_CONTENTS()],
+        contains: [BLOCK_MUSTACHE_CONTENTS()],
       },
-      // triple mustaches {{{unescapedOutput}}}
       {
+        // template variable or helper-call that is NOT html-escaped
         className: 'template-variable',
         begin: /\{\{\{/, end: /\}\}\}/,
-        keywords: BUILT_INS
+        keywords: BUILT_INS,
+        contains: [BASIC_MUSTACHE_CONTENTS()]
       },
-      // standard mustaches {{{htmlEscapedOutput}}}
       {
+        // template variable or helper-call that is html-escaped
         className: 'template-variable',
         begin: /\{\{/, end: /\}\}/,
-        keywords: BUILT_INS
+        keywords: BUILT_INS,
+        contains: [
+          BASIC_MUSTACHE_CONTENTS()
+        ]
       }
     ]
   };
