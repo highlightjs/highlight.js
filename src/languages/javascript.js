9f2318548e873dbbf86c017ba7e0e6c3987f286b
Milutin Kristofic <milutin.kristofic@gmail.com>
2019-10-31 08:19:26 +0100

    Adding support for type

--- src/languages/javascript.js
+++ src/languages/javascript.js
@@ -113,7 +113,7 @@ function(hljs) {
           contains : [
             {
               className : 'doctag',
-              begin : '@[A-Za-z]+'
+              begin : '@[A-Za-z]+(\\s+\\{[A-Za-z0-9><\\._:-]+\\})?'
             }
           ]
         }
