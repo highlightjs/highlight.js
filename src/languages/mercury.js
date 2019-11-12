542daada864f4e91295cea33ec6130bd6a843cbd
Josh Goebel <me@joshgoebel.com>
2019-11-11 22:47:27 -0500

    fix(mercury): don't change global STRING modes

--- src/languages/mercury.js
+++ src/languages/mercury.js
@@ -49,6 +49,7 @@ function(hljs) {
     begin: '\\\\[abfnrtv]\\|\\\\x[0-9a-fA-F]*\\\\\\|%[-+# *.0-9]*[dioxXucsfeEgGp]',
     relevance: 0
   };
+  STRING.contains = STRING.contains.slice() // we need our own copy of contains
   STRING.contains.push(STRING_FMT);
 
   var IMPLICATION = {
