e2dc6676d5bc063835456b2d0420f728fb52a62d
Josh Goebel <me@joshgoebel.com>
2019-11-13 21:17:50 -0500

    fix Expected an assignment or function call and instead saw an expression

--- src/highlight.js
+++ src/highlight.js
@@ -264,8 +264,11 @@ https://highlightjs.org/
   function restoreLanguageApi(obj) {
     if(API_REPLACES && !obj.langApiRestored) {
       obj.langApiRestored = true;
-      for(var key in API_REPLACES)
-        obj[key] && (obj[API_REPLACES[key]] = obj[key]);
+      for(var key in API_REPLACES) {
+        if (obj[key]) {
+          obj[API_REPLACES[key]] = obj[key];
+        }
+      }
       (obj.contains || []).concat(obj.variants || []).forEach(restoreLanguageApi);
     }
   }
