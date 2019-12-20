6d9a9ccd628de57750955fbbca2dcdd187015b04
Josh Goebel <me@joshgoebel.com>
2019-12-20 16:40:30 -0500

    clean up node build

--- tools/lib/bundling.js
+++ tools/lib/bundling.js
@@ -1,65 +1,20 @@
 const rollup = require('rollup')
 
-async function buildOutput(inputOptions, outputOptions) {
-  const bundle = await rollup.rollup(inputOptions).catch(err => console.log(err));;
-  const { output } = await bundle.generate(outputOptions);
+async function rollupCode(inputOptions, outputOptions) {
+  const output = await generate(inputOptions, outputOptions)
   return output[0].code;
 }
 
-async function buildRaw(inputOptions, outputOptions) {
-  const bundle = await rollup.rollup(inputOptions).catch(err => console.log(err));;
+async function generate(inputOptions, outputOptions) {
+  const bundle = await rollup.rollup(inputOptions).catch(err => console.log(err));
   const { output } = await bundle.generate(outputOptions);
   return output;
 }
 
-async function build(inputOptions, outputOptions) {
-  // create a bundle
-  const bundle = await rollup.rollup(inputOptions).catch(err => console.log(err));;
-
-  // console.log(bundle.watchFiles); // an array of file names this bundle depends on
-
-  // generate code
-  const { output } = await bundle.generate(outputOptions);
-
-  for (const chunkOrAsset of output) {
-    if (chunkOrAsset.type === 'asset') {
-      // For assets, this contains
-      // {
-      //   fileName: string,              // the asset file name
-      //   source: string | Buffer        // the asset source
-      //   type: 'asset'                  // signifies that this is an asset
-      // }
-      // console.log('Asset', chunkOrAsset);
-    } else {
-      // For chunks, this contains
-      // {
-      //   code: string,                  // the generated JS code
-      //   dynamicImports: string[],      // external modules imported dynamically by the chunk
-      //   exports: string[],             // exported variable names
-      //   facadeModuleId: string | null, // the id of a module that this chunk corresponds to
-      //   fileName: string,              // the chunk file name
-      //   imports: string[],             // external modules imported statically by the chunk
-      //   isDynamicEntry: boolean,       // is this chunk a dynamic entry point
-      //   isEntry: boolean,              // is this chunk a static entry point
-      //   map: string | null,            // sourcemaps if present
-      //   modules: {                     // information about the modules in this chunk
-      //     [id: string]: {
-      //       renderedExports: string[]; // exported variable names that were included
-      //       removedExports: string[];  // exported variable names that were removed
-      //       renderedLength: number;    // the length of the remaining code in this module
-      //       originalLength: number;    // the original length of the code in this module
-      //     };
-      //   },
-      //   name: string                   // the name of this chunk as used in naming patterns
-      //   type: 'chunk',                 // signifies that this is a chunk
-      // }
-      // console.log('Chunk', chunkOrAsset.modules);
-      // console.log('Chunk', chunkOrAsset.code);
-    }
-  }
-
-  // or write the bundle to disk
-  await bundle.write(outputOptions)
+async function rollupWrite(inputOptions, outputOptions) {
+  const bundle = await rollup.rollup(inputOptions).catch(err => console.log(err));
+  await bundle.generate(outputOptions);
+  await bundle.write(outputOptions);
 }
 
-module.exports = { build, buildOutput, buildRaw }
+module.exports = { rollupWrite, rollupCode };
