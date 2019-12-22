c7b4f9dc4b8f188967a1c0831de0fa08a48a7cf3
Josh Goebel <me@joshgoebel.com>
2019-12-22 09:04:37 -0500

    clean up build config

--- tools/build_config.js
+++ tools/build_config.js
@@ -2,7 +2,7 @@ const cjsPlugin = require('rollup-plugin-commonjs');
 
 module.exports = {
     build_dir: "build",
-    copyrightYears: "2006-2019",
+    copyrightYears: "2006-2020",
     clean_css: {},
     rollup: {
       node: {
@@ -37,65 +37,13 @@ module.exports = {
         }
       }
     },
-
-    "CJS": {
-      format: "cjs",
-      strict: false,
-    },
-    "terser": {
+    terser: {
         "compress": {
           passes: 2,
           unsafe: true,
           warnings: true,
           dead_code: true,
           toplevel: "funcs"
-        },
-        "mangle": {
-//           "properties": {
-//             "only_cache": true,
-//             "reserved": [],
-//             // "keep_quoted": true,
-//             "builtins": []
-// ,              "cache": {
-//               "propsx" : new Map([
-//                 ["relevance","r"],
-//                 ["contains", "c"],
-//                 ["keywords", "k"],
-//                 ['begin', 'b'],
-//                 ['beginKeywords', 'bK'],
-//                 ['end', 'e'],
-//                 ['endsWithParent', 'eW'],
-//                 ['excludeBegin', 'eB'],
-//                 ['excludeEnd', 'eE'],
-//                 ['returnBegin', 'rB'],
-//                 ['returnEnd', 'rE'],
-//                 ['variants', 'v'],
-//                 // ['className', 'cN'],
-
-//                 ['IDENT_RE', 'IR'],
-//                 ['UNDERSCORE_IDENT_RE', 'UIR'],
-//                 ['NUMBER_RE', 'NR'],
-//                 ['C_NUMBER_RE', 'CNR'],
-//                 ['BINARY_NUMBER_RE', 'BNR'],
-//                 ['RE_STARTERS_RE', 'RSR'],
-//                 ['BACKSLASH_ESCAPE', 'BE'],
-//                 ['APOS_STRING_MODE', 'ASM'],
-//                 ['QUOTE_STRING_MODE', 'QSM'],
-//                 ['PHRASAL_WORDS_MODE', 'PWM'],
-//                 ['C_LINE_COMMENT_MODE', 'CLCM'],
-//                 ['C_BLOCK_COMMENT_MODE', 'CBCM'],
-//                 ['HASH_COMMENT_MODE', 'HCM'],
-//                 ['NUMBER_MODE', 'NM'],
-//                 ['C_NUMBER_MODE', 'CNM'],
-//                 ['BINARY_NUMBER_MODE', 'BNM'],
-//                 ['CSS_NUMBER_MODE', 'CSSNM'],
-//                 ['REGEXP_MODE', 'RM'],
-//                 ['TITLE_MODE', 'TM'],
-//                 ['UNDERSCORE_TITLE_MODE', 'UTM'],
-//                 ['COMMENT', 'C'],
-//               ])
-//             }
-//           }
         }
     }
 }
