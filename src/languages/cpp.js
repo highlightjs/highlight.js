0eb875b4b64ff3bb4330b8461085c97b62f21a98
Josh Goebel <me@joshgoebel.com>
2020-01-01 19:47:07 -0500

    fix language aliases and demo building

--- src/languages/cpp.js
+++ src/languages/cpp.js
@@ -10,6 +10,6 @@ function(hljs) {
   var lang = hljs.getLanguage('c-like').rawDefinition();
   // return auto-detection back on
   lang.disableAutodetect = false;
+  lang.aliases = ['cc', 'c++', 'h++', 'hpp', 'hh', 'hxx', 'cxx'];
   return lang;
-
 }
