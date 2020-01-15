4743bcd64ff60099525143369bd21157653832bb
Josh Goebel <me@joshgoebel.com>
2020-01-15 15:23:44 -0500

    clean up tests further

--- test/browser/highlight_block_callbacks.js
+++ test/browser/highlight_block_callbacks.js
@@ -13,7 +13,35 @@ class ContentAdder {
   }
 }
 
+describe('callback system', function() {
+  it("supports class based plugins", async function() {
+    const testCase = newTestCase({
+      code: "var b",
+      language: "javascript",
+      expect: `<span class="hljs-keyword">var</span> b = <span class="hljs-number">5</span>;`
+    });
+    await buildFakeDOM.bind(this)(testCase);
+
+    this.hljs.addPlugin(new ContentAdder({content:" = 5;"}))
+    this.hljs.highlightBlock(this.block);
+    const actual = this.block.innerHTML;
+    actual.should.equal(testCase.expect);
+
+  })
+})
+
 describe('before:highlightBlock', function() {
+  it('is called', async function() {
+    await buildFakeDOM.bind(this)(defaultCase);
+    var called = false;
+    this.hljs.addPlugin({
+      'before:highlightBlock': ({block, result}) => {
+        called = true;
+      }
+    });
+    this.hljs.highlightBlock(this.block);
+    called.should.equal(true);
+  })
   it('can modify block content before highlight', async function() {
     const testCase = newTestCase({
       code: "This is the original content.",
@@ -34,38 +62,30 @@ describe('before:highlightBlock', function() {
       `<span class="hljs-keyword">var</span> a;`);
   });
 
-  it("supports class based plugins", async function() {
-    const testCase = newTestCase({
-      code: "var b",
-      language: "javascript",
-      expect: `<span class="hljs-keyword">var</span> b = <span class="hljs-number">5</span>;`
-    });
-    await buildFakeDOM.bind(this)(testCase);
-
-    this.hljs.addPlugin(new ContentAdder({content:" = 5;"}))
-    this.hljs.highlightBlock(this.block);
-    const actual = this.block.innerHTML;
-    actual.should.equal(testCase.expect);
-
-  })
 })
 
 describe('after:highlightBlock', function() {
+  it('is called', async function() {
+    await buildFakeDOM.bind(this)(defaultCase);
+    var called = false;
+    this.hljs.addPlugin({
+      'after:highlightBlock': ({block, result}) => {
+        called = true;
+      }
+    });
+    this.hljs.highlightBlock(this.block);
+    called.should.equal(true);
+  })
   it('receives result data', async function() {
     await buildFakeDOM.bind(this)(defaultCase);
-
-    var pluginCalled = 0;
 
     this.hljs.addPlugin({
       'after:highlightBlock': ({block, result}) => {
         result.language.should.equal("javascript")
         result.relevance.should.above(0)
-        pluginCalled += 1;
       }
     });
 
     this.hljs.highlightBlock(this.block);
-    this.hljs.highlightBlock(this.block);
-    pluginCalled.should.equal(2);
   });
 })
