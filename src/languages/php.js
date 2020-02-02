512b547b8e63c77455c9cd4a6d3923bf4f08ab3c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-02-02 10:02:24 +0700

    Added Built-In Keywords
     - This moves `exception`, `parent`, `self` and `static` keyword to built-in.
     - Added `class`, `extends`, `implements`, `interface` and `use` back to the keyword list.
     - Added type aliases such as `boolean` and `integer` (alias of `bool` and `int`)  to the keyword list.
     - Finally found a way to highlight keywords within function arguments based on `csharp.js` language highlighter.

--- src/languages/php.js
+++ src/languages/php.js
@@ -28,24 +28,36 @@ function(hljs) {
     ]
   };
   var NUMBER = {variants: [hljs.BINARY_NUMBER_MODE, hljs.C_NUMBER_MODE]};
+  var KEYWORDS = {
+    keyword:
+    // Magic constants:
+    // <https://www.php.net/manual/en/language.constants.predefined.php>
+    '__CLASS__ __DIR__ __FILE__ __FUNCTION__ __LINE__ __METHOD__ __NAMESPACE__ __TRAIT__ ' +
+    // Function that look like language construct or language construct that look like function:
+    // List of keywords that may not require parenthesis
+    'die echo exit include include_once print require require_once ' +
+    // These are not language construct (function) but operate on the currently-executing function and can access the current symbol table
+    // 'compact extract func_get_arg func_get_args func_num_args get_called_class get_parent_class ' +
+    // Other keywords:
+    // <https://www.php.net/manual/en/reserved.php>
+    // <https://www.php.net/manual/en/language.types.type-juggling.php>
+    'array abstract and as binary bool boolean break callable case catch class clone const continue declare default do double else elseif empty enddeclare endfor endforeach endif endswitch endwhile eval extends final finally float for foreach from global goto if implements instanceof insteadof int integer interface isset iterable list new object or private protected public real return string switch throw trait try unset use var void while xor yield',
+    literal: 'false null true',
+    built_in:
+    // Standard PHP library:
+    // <https://www.php.net/manual/en/book.spl.php>
+    'AppendIterator ArgumentCountError ArithmeticError ArrayIterator ArrayObject AssertionError BadFunctionCallException BadMethodCallException CachingIterator CallbackFilterIterator CompileError Countable DirectoryIterator DivisionByZeroError DomainException EmptyIterator Error ErrorException Exception FilesystemIterator FilterIterator GlobIterator InfiniteIterator InvalidArgumentException IteratorIterator LengthException LimitIterator LogicException MultipleIterator NoRewindIterator OutOfBoundsException OutOfRangeException OuterIterator OverflowException ParentIterator ParseError RangeException RecursiveArrayIterator RecursiveCachingIterator RecursiveCallbackFilterIterator RecursiveDirectoryIterator RecursiveFilterIterator RecursiveIterator RecursiveIteratorIterator RecursiveRegexIterator RecursiveTreeIterator RegexIterator RuntimeException SeekableIterator SplDoublyLinkedList SplFileInfo SplFileObject SplFixedArray SplHeap SplMaxHeap SplMinHeap SplObjectStorage SplObserver SplObserver SplPriorityQueue SplQueue SplStack SplSubject SplSubject SplTempFileObject TypeError UnderflowException UnexpectedValueException ' +
+    // Reserved interfaces:
+    // <https://www.php.net/manual/en/reserved.interfaces.php>
+    'ArrayAccess Closure Generator Iterator IteratorAggregate Serializable Throwable Traversable WeakReference ' +
+    // Reserved classes:
+    // <https://www.php.net/manual/en/reserved.classes.php>
+    'Directory __PHP_Incomplete_Class parent php_user_filter self static stdClass'
+  };
   return {
     aliases: ['php', 'php3', 'php4', 'php5', 'php6', 'php7'],
     case_insensitive: true,
-    keywords: {
-      keyword:
-      // Magic constants:
-      // <https://www.php.net/manual/en/language.constants.predefined.php>
-      '__CLASS__ __DIR__ __FILE__ __FUNCTION__ __LINE__ __METHOD__ __NAMESPACE__ __TRAIT__' +
-      // Function that look like language construct or language construct that look like function:
-      // List of keywords that may not require parenthesis
-      ' die echo exit include include_once parent print require require_once self static' +
-      // These are not language construct (function) but operate on the currently-executing function and can access the current symbol table
-      // ' compact extract func_get_arg func_get_args func_num_args get_called_class get_parent_class' +
-      // Other keywords:
-      // <https://www.php.net/manual/en/reserved.php>
-      ' array abstract and as bool break callable case catch clone const continue declare default do else elseif empty enddeclare endfor endforeach endif endswitch endwhile eval final finally float for foreach from global goto if instanceof insteadof int isset iterable list new object or private protected public return string switch throw trait try unset var void while xor yield',
-      literal: 'false null true'
-    },
+    keywords: KEYWORDS,
     contains: [
       hljs.HASH_COMMENT_MODE,
       hljs.COMMENT('//', '$', {contains: [PREPROCESSOR]}),
@@ -96,12 +108,15 @@ function(hljs) {
       {
         className: 'function',
         beginKeywords: 'fn function', end: /[;{]/, excludeEnd: true,
-        illegal: '\\$|\\[|%',
+        illegal: '[$%\\[]',
         contains: [
           hljs.UNDERSCORE_TITLE_MODE,
           {
             className: 'params',
             begin: '\\(', end: '\\)',
+            excludeBegin: true,
+            excludeEnd: true,
+            keywords: KEYWORDS,
             contains: [
               'self',
               VARIABLE,
