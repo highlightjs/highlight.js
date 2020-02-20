42f3c301c276beaf03cce5e1688db47e2de59548
Josh Goebel <me@joshgoebel.com>
2020-02-20 08:28:07 -0500

    prevent detecting incorret php end tag

--- src/languages/php-template.js
+++ src/languages/php-template.js
@@ -15,6 +15,15 @@ export default function(hljs) {
         begin: /<\?(php|=)?/,
         end: /\?>/,
         subLanguage: 'php',
+        contains: [
+          // We don't want the php closing tag ?> to close the PHP block when
+          // inside any of the following blocks:
+          {begin: '/\\*', end: '\\*/', skip: true},
+          {begin: 'b"', end: '"', skip: true},
+          {begin: 'b\'', end: '\'', skip: true},
+          hljs.inherit(hljs.APOS_STRING_MODE, {illegal: null, className: null, contains: null, skip: true}),
+          hljs.inherit(hljs.QUOTE_STRING_MODE, {illegal: null, className: null, contains: null, skip: true})
+        ]
       }
     ]
   };
