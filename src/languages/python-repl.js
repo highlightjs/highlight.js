891f429adcd0b828458f6e156be41d9344fa8997
Josh Goebel <me@joshgoebel.com>
2020-02-23 00:29:47 -0500

    Update src/languages/python-repl.js
    Co-Authored-By: Vladimir Jimenez <allejo@me.com>

--- src/languages/python-repl.js
+++ src/languages/python-repl.js
@@ -21,7 +21,7 @@ export default function(hljs) {
         },
         variants: [
           { begin: /^>>>(?=[ ]|$)/ },
-          { begin: /^\.\.\.(?=[ ])/ }
+          { begin: /^\.\.\.(?=[ ]|$)/ }
         ]
       },
     ]
