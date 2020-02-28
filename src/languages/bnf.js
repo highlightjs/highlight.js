ec0ec9b6dcad60c964575667345c20f27027262c
Josh Goebel <me@joshgoebel.com>
2020-02-28 14:44:54 -0500

    (chore) simplify BNF grammar

--- src/languages/bnf.js
+++ src/languages/bnf.js
@@ -16,19 +16,17 @@ export default function(hljs){
       // Specific
       {
         begin: /::=/,
-        starts: {
-          end: /$/,
-          contains: [
-            {
-              begin: /</, end: />/
-            },
-            // Common
-            hljs.C_LINE_COMMENT_MODE,
-            hljs.C_BLOCK_COMMENT_MODE,
-            hljs.APOS_STRING_MODE,
-            hljs.QUOTE_STRING_MODE
-          ]
-        }
+        end: /$/,
+        contains: [
+          {
+            begin: /</, end: />/
+          },
+          // Common
+          hljs.C_LINE_COMMENT_MODE,
+          hljs.C_BLOCK_COMMENT_MODE,
+          hljs.APOS_STRING_MODE,
+          hljs.QUOTE_STRING_MODE
+        ]
       }
     ]
   };
