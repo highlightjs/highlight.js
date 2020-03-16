a70fd3e2da1055b46d4bd95fe586fd0032647f79
kopytjuk <kopytjuk@gmail.com>
2020-03-16 22:00:06 +0100

    Add tabular ops.

--- src/languages/kusto.js
+++ src/languages/kusto.js
@@ -7,14 +7,22 @@ Category: common
 */
 
 export default function (hljs) {
+
     var AGGFUNCS = 'any anyif arg_max arg_min avg avgif binary_all_and ' +
         'binary_all_or binary_all_xor buildschema count countif dcount dcountif ' +
         'hll hll_merge make_bag make_bag_if make_list make_list_if make_list_with_nulls ' +
         'make_set make_set_if max maxif min minif percentiles stdev stdevif stdevp ' +
         'sum sumif tdigest tdigest tdigest_merge variance varianceif variancep ';
+    var MISC_OPS = 'datatable range '
+    var TABULAR_OPS = 'as consume count distinct evaluate extend externaldata ' +
+        'facet find fork getschema invoke join limit lookup make-series mv-apply ' +
+        'mv-expand order project project-away project-rename project-reorder parse ' +
+        'parse-where partition print reduce render sample sample-distinct search ' +
+        'serialize sort summarize take top top-nested top-hitters union where '
+
     var KEYWORDS = {
-        keyword:
-            'where and or count project desc by limit sort consume ' +
+        keyword: TABULAR_OPS + MISC_OPS + 
+            'as consume count where and or count project desc by limit sort  ' +
             'summarize render top join on kind extend sort let union' +
             'range from to step reduce with evaluate invoke range',
         built_in:
@@ -23,6 +31,15 @@ export default function (hljs) {
             'bin ago datatable totimespan string int toscalar ',
         literal: 'false true null',
     };
+
+    var NUM_OR_TIMEPERIOD = {
+        className: 'number',
+        begin: hljs.NUMBER_RE + '(' +
+            'tick|microsecond|ms|s|h|d' +
+            ')?',
+        relevance: 10
+    };
+
     return {
         name: "Kusto",
         case_insensitive: true,
@@ -33,13 +50,7 @@ export default function (hljs) {
             hljs.APOS_STRING_MODE,
             hljs.C_LINE_COMMENT_MODE,
             // contains numbers and timeperiods
-            {
-                className: 'number',
-                begin: hljs.NUMBER_RE + '(' +
-                    'tick|microsecond|ms|s|h|d' +
-                    ')?',
-                relevance: 10
-            }
+            NUM_OR_TIMEPERIOD,
         ],
         aliases: ['kql', 'kusto']
     }
