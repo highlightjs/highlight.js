5605afda8d422fa94f6361cb3259df87bf8a1b30
Josh Goebel <me@joshgoebel.com>
2020-04-21 21:50:22 -0400

    eslint comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,17 +19,22 @@ module.exports = {
     "rules": {
       "array-callback-return": "error",
       "block-scoped-var": "error",
+      // we like our semi-colons
       "semi": ["error","always"],
+      // our codebase doesn't do this at all, so disabled for now
       "space-before-function-paren": ["error","never"],
-      // "padded-blocks": "off",
+      // for now ignore diff between types of quoting
       "quotes": "off",
       // "no-multi-spaces": "off",
       "one-var": "off",
+      // this is the style we are already using
       "operator-linebreak": ["error","after", { "overrides": { "?": "after", ":": "after" } }],
+      // sometimes we declare variables with extra spacing
       "indent": ["error", 2, {"VariableDeclarator":2}],
+      // seems like a good idea not to use explicit undefined
       "no-undefined": "error",
 
-      // TODO
+      // TODO maybe
       "camelcase": "off", // TODO: turn on later
       // "init-declarations": ["error","always"]
     }
