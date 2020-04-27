c045374f7090cc476aeb257652c2ed6467224c52
Peter Plantinga <plantinga.peter@gmail.com>
2020-04-27 13:18:33 -0400

    YAML remove inline sequence and mapping chars from strings

--- src/languages/yaml.js
+++ src/languages/yaml.js
@@ -39,7 +39,7 @@ export default function(hljs) {
     variants: [
       {begin: /'/, end: /'/},
       {begin: /"/, end: /"/},
-      {begin: /\S+/}
+      {begin: /[^\s,{}[\]]+/}
     ],
     contains: [
       hljs.BACKSLASH_ESCAPE,
