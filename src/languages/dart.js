145bb82321e6251ac2ae400c393b33d756d5ee7e
Josh Goebel <me@joshgoebel.com>
2020-06-11 18:40:16 -0400

    Update src/languages/dart.js

--- src/languages/dart.js
+++ src/languages/dart.js
@@ -108,7 +108,7 @@ export default function(hljs) {
     keyword: 'abstract as assert async await break case catch class const continue covariant default deferred do ' +
       'dynamic else enum export extends extension external factory false final finally for Function get hide if ' +
       'implements import in inferface is late library mixin new null on operator part required rethrow return set ' +
-      'show static super switch sync this throw true try typedef const void while with yield',
+      'show static super switch sync this throw true try typedef var void while with yield',
     built_in:
       BUILT_IN_TYPES
         .concat(NULLABLE_BUILT_IN_TYPES)
