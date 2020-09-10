90101c256e48db81d629cfb289804516ea32d631
Konrad Rudolph <konrad.rudolph@gmail.com>
2020-09-10 10:43:03 +0100

    Fix R identifier & keyword definitions
    * Add missing identifier cases: R identifiers can start with `._`, and a
    single `.` is a valid identifier.
    * Fix the R keyword list and add a category for built-in non-keyword
    identifiers.

--- src/languages/r.js
+++ src/languages/r.js
@@ -2,12 +2,14 @@
 Language: R
 Description: R is a free software environment for statistical computing and graphics.
 Author: Joe Cheng <joe@rstudio.org>
+Contributors: Konrad Rudolph <konrad.rudolph@gmail.com>
 Website: https://www.r-project.org
 Category: scientific
 */
 
 export default function(hljs) {
-  var IDENT_RE = '([a-zA-Z]|\\.[a-zA-Z.])[a-zA-Z0-9._]*';
+  // FIXME: Support Unicode identifiers.
+  var IDENT_RE = '(([a-zA-Z]|\\.[._a-zA-Z])[._a-zA-Z0-9]*)|\\.(!=\\d)'
 
   return {
     name: 'R',
@@ -18,12 +20,33 @@ export default function(hljs) {
         keywords: {
           $pattern: IDENT_RE,
           keyword:
-            'function if in break next repeat else for return switch while try tryCatch ' +
-            'stop warning require library attach detach source setMethod setGeneric ' +
-            'setGroupGeneric setClass ...',
+            'function if in break next repeat else for while',
           literal:
-            'NULL NA TRUE FALSE T F Inf NaN NA_integer_|10 NA_real_|10 NA_character_|10 ' +
-            'NA_complex_|10'
+            'NULL NA TRUE FALSE Inf NaN NA_integer_|10 NA_real_|10 ' +
+            'NA_character_|10 NA_complex_|10',
+          built_in:
+            // Builtin constants
+            'LETTERS letters month.abb month.name pi T F ... ..1 ..2 ' +
+            // Primitive functions
+            // These are all the functions in `base` that are implemented as a
+            // `.Primitive`, minus those functions that are also keywords.
+            'abs acos acosh all any anyNA Arg as.call as.character' +
+            'as.complex as.double as.environment as.integer as.logical' +
+            'as.null.default as.numeric as.raw asin asinh atan atanh attr' +
+            'attributes baseenv browser c call ceiling class Conj cos cosh' +
+            'cospi cummax cummin cumprod cumsum digamma dim dimnames' +
+            'emptyenv exp expression floor forceAndCall gamma gc.time' +
+            'globalenv Im interactive invisible is.array is.atomic is.call' +
+            'is.character is.complex is.double is.environment is.expression' +
+            'is.finite is.function is.infinite is.integer is.language' +
+            'is.list is.logical is.matrix is.na is.name is.nan is.null' +
+            'is.numeric is.object is.pairlist is.raw is.recursive is.single' +
+            'is.symbol lazyLoadDBfetch length lgamma list log max min' +
+            'missing Mod names nargs nzchar oldClass on.exit pos.to.env' +
+            'proc.time prod quote range Re rep retracemem return round' +
+            'seq_along seq_len seq.int sign signif sin sinh sinpi sqrt' +
+            'standardGeneric substitute sum switch tan tanh tanpi tracemem' +
+            'trigamma trunc unclass untracemem UseMethod xtfrm',
         },
         relevance: 0
       },
