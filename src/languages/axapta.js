330ddc9f54ab124c071468373d2d7f8477420f6d
Ryan Jonasson <ryanjo@microsoft.com>
2020-09-11 09:28:48 -0500

    Upate Axapta keyword list

--- src/languages/axapta.js
+++ src/languages/axapta.js
@@ -13,8 +13,16 @@ export default function(hljs) {
       'while long throw finally protected final return void enum else ' +
       'break new catch byte super case short default double public try this switch ' +
       'continue reverse firstfast firstonly forupdate nofetch sum avg minof maxof count ' +
-      'order group by asc desc index hint like dispaly edit client server ttsbegin ' +
-      'ttscommit str real date container anytype common div mod',
+      'order group by asc desc index hint like display edit client server ttsbegin ' +
+      'ttscommit str real date container anytype common div mod ' +
+      'next guid int64 utcdatetime class interface implements extends in do print ' +
+      'breakpoint retry select delete_from from firstonly1 firstonly10 ' +
+      'firstonly100 firstonly1000 forceliterals forcenestedloop forceselectorder ' +
+      'forceplaceholders repeatableread optimisticlock pessimisticlock ' +
+      'generateonly crosscompany where outer exists notexists join ' +
+      'insert_recordset update_recordset setting ttsabort ' +
+      'flush changecompany client eventhandler delegate as is byref ' +
+      'unchecked validtimestate namespace using const readonly internal var ',
     contains: [
       hljs.C_LINE_COMMENT_MODE,
       hljs.C_BLOCK_COMMENT_MODE,
