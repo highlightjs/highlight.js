1dae975cc651b3a8ecc6621a1f682a232c72706e
Florian Förster <foerster.florianjr@gmail.com>
2020-09-13 10:32:55 +0200

    enh(twig): keywords, filters, functions
    - Update Twig keyword, filter, functions list and fixed highlighting
      inside tag
    - Update AUTHORS.TXT
    - Update CHANGES.md

--- src/languages/twig.js
+++ src/languages/twig.js
@@ -2,74 +2,82 @@
 Language: Twig
 Requires: xml.js
 Author: Luke Holder <lukemh@gmail.com>
+Contributors: Florian Förster <foerster.florianjr@gmail.com>
 Description: Twig is a templating language for PHP
 Website: https://twig.symfony.com
 Category: template
 */
 
-export default function(hljs) {
+export default function (hljs) {
   var PARAMS = {
-    className: 'params',
-    begin: '\\(', end: '\\)'
+    className: "params",
+    begin: "\\(",
+    end: "\\)",
   };
 
-  var FUNCTION_NAMES = 'attribute block constant cycle date dump include ' +
-                  'max min parent random range source template_from_string';
+  var FUNCTION_NAMES =
+    "attribute block constant country_timezones cycle date dump html_classes include " +
+    "max min parent random range source template_from_string";
 
   var FUNCTIONS = {
     beginKeywords: FUNCTION_NAMES,
-    keywords: {name: FUNCTION_NAMES},
+    keywords: { name: FUNCTION_NAMES },
     relevance: 0,
-    contains: [
-      PARAMS
-    ]
+    contains: [PARAMS],
   };
 
   var FILTER = {
     begin: /\|[A-Za-z_]+:?/,
     keywords:
-      'abs batch capitalize column convert_encoding date date_modify default ' +
-      'escape filter first format inky_to_html inline_css join json_encode keys last ' +
-      'length lower map markdown merge nl2br number_format raw reduce replace ' +
-      'reverse round slice sort spaceless split striptags title trim upper url_encode',
-    contains: [
-      FUNCTIONS
-    ]
+      "abs batch capitalize column convert_encoding country_name currency_name currency_symbol data_uri date date_modify default " +
+      "escape filter first format format_currency format_date format_datetime format_number format_time html_to_markdown inky_to_html inline_css join json_encode keys last " +
+      "length lower map markdown_to_html merge nl2br number_format raw reduce replace " +
+      "reverse round slice sort spaceless split striptags timezone_name title trim u upper url_encode",
+    contains: [FUNCTIONS],
   };
 
-  var TAGS = 'apply autoescape block deprecated do embed extends filter flush for from ' +
-    'if import include macro sandbox set use verbatim with';
+  var TAGS =
+    "apply autoescape block deprecated do embed else extends filter flush for from " +
+    "if import in include is macro sandbox set use verbatim with";
 
-  TAGS = TAGS + ' ' + TAGS.split(' ').map(function(t){return 'end' + t}).join(' ');
+  TAGS =
+    TAGS +
+    " " +
+    TAGS.split(" ")
+      .map(function (t) {
+        return "end" + t;
+      })
+      .join(" ");
 
   return {
-    name: 'Twig',
-    aliases: ['craftcms'],
+    name: "Twig",
+    aliases: ["craftcms"],
     case_insensitive: true,
-    subLanguage: 'xml',
+    subLanguage: "xml",
     contains: [
       hljs.COMMENT(/\{#/, /#}/),
       {
-        className: 'template-tag',
-        begin: /\{%/, end: /%}/,
+        className: "template-tag",
+        begin: /\{%/,
+        end: /%}/,
         contains: [
           {
-            className: 'name',
             begin: /\w+/,
+            end: "\\W",
             keywords: TAGS,
             starts: {
-              endsWithParent: true,
               contains: [FILTER, FUNCTIONS],
-              relevance: 0
-            }
-          }
-        ]
+              relevance: 0,
+            },
+          },
+        ],
       },
       {
-        className: 'template-variable',
-        begin: /\{\{/, end: /}}/,
-        contains: ['self', FILTER, FUNCTIONS]
-      }
-    ]
+        className: "template-variable",
+        begin: /\{\{/,
+        end: /}}/,
+        contains: ["self", FILTER, FUNCTIONS],
+      },
+    ],
   };
 }
