a288a8c150837654998dfb8fb34cab4bf3794160
Konrad Rudolph <konrad.rudolph@gmail.com>
2020-09-29 15:27:32 +0100

    Sort builtin names alphabetically

--- src/languages/python.js
+++ src/languages/python.js
@@ -12,12 +12,11 @@ export default function(hljs) {
       'and as assert async await break class continue def del elif else except finally for ' +
       'from global if import in is lambda nonlocal|10 not or pass raise return try while with yield',
     built_in:
-      'abs delattr hash memoryview set all dict help min setattr any dir hex next slice ascii ' +
-      'divmod id object sorted bin enumerate input oct staticmethod bool eval int open str ' +
-      'breakpoint exec isinstance ord sum bytearray filter issubclass pow super bytes float ' +
-      'iter print tuple callable format len property type chr frozenset list range vars ' +
-      'classmethod getattr locals repr zip compile globals map reversed __import__ complex ' +
-      'hasattr max round',
+      '__import__ abs all any ascii bin bool breakpoint bytearray bytes callable chr classmethod ' +
+      'compile complex delattr dict dir divmod enumerate eval exec filter float format frozenset ' +
+      'getattr globals hasattr hash help hex id input int isinstance issubclass iter len list ' +
+      'locals map max memoryview min next object oct open ord pow print property range repr ' +
+      'reversed round set setattr slice sorted staticmethod str sum super tuple type vars zip',
     literal:
       '__debug__ Ellipsis False None NotImplemented True'
   };
