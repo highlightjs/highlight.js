4e8e06aee102c03877aee49fea6623a84631dc60
Ayesh Karunaratne <ayesh@ayesh.me>
2020-10-04 13:35:09 +0700

    [PHP] Add 'php8' as an alias for PHP

--- src/languages/php.js
+++ src/languages/php.js
@@ -85,7 +85,7 @@ export default function(hljs) {
     'Directory __PHP_Incomplete_Class parent php_user_filter self static stdClass'
   };
   return {
-    aliases: ['php', 'php3', 'php4', 'php5', 'php6', 'php7'],
+    aliases: ['php', 'php3', 'php4', 'php5', 'php6', 'php7', 'php8'],
     case_insensitive: true,
     keywords: KEYWORDS,
     contains: [
