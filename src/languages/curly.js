ca3d5e05ee842502dea90932974583318b940ade
jenra <jenra.uwu@gmail.com>
2020-10-06 12:47:26 -0400

    Added stuff to curly.js

--- src/languages/curly.js
+++ src/languages/curly.js
@@ -4,7 +4,32 @@ Author: jenra <jenra.uwu@gmail.com>
 Github: https://github.com/jenra-uwu/curly-lang
 */
 
+// @ts-nocheck
 export default function (hljs)
 {
-
+	return {
+		name: 'curly',
+		keywords: {
+			keyword: 'for some all in pass stop if then else where with match to and or xor type enum class',
+			literal: 'true false'
+		},
+		contains: [
+			hljs.QUOTE_STRING_MODE,
+			hljs.COMMENT(
+				'#', '\n'
+			),
+			{
+				className: 'number',
+				begin: /\b[0-9]+(\.[0-9]+)?/
+			},
+			{
+				className: 'symbol',
+				begin: /\b[A-Za-z_@\$][a-zA-Z0-9'_]*(?=\s*\:)|\.[A-Za-z_@\$][a-zA-Z0-9'_]*/
+			},
+			{
+				className: 'type',
+				begin: /\b[A-Z][a-z0-9_A-Z']*/
+			}
+		]
+	}
 }
