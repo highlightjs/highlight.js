aa45193178bf2b47167e4fc191764bb811fbaa4c
Josh Goebel <me@joshgoebel.com>
2020-10-08 16:38:11 -0400

    enh(autodetect) matlab: remove relevancy from `i` and `j`

--- src/languages/matlab.js
+++ src/languages/matlab.js
@@ -38,8 +38,8 @@ export default function(hljs) {
         'eye repmat rand randn linspace logspace freqspace meshgrid accumarray size length ' +
         'ndims numel disp isempty isequal isequalwithequalnans cat reshape diag blkdiag tril ' +
         'triu fliplr flipud flipdim rot90 find sub2ind ind2sub bsxfun ndgrid permute ipermute ' +
-        'shiftdim circshift squeeze isscalar isvector ans eps realmax realmin pi i inf nan ' +
-        'isnan isinf isfinite j why compan gallery hadamard hankel hilb invhilb magic pascal ' +
+        'shiftdim circshift squeeze isscalar isvector ans eps realmax realmin pi i|0 inf nan ' +
+        'isnan isinf isfinite j|0 why compan gallery hadamard hankel hilb invhilb magic pascal ' +
         'rosser toeplitz vander wilkinson max min nanmax nanmin mean nanmean type table ' +
         'readtable writetable sortrows sort figure plot plot3 scatter scatter3 cellfun ' +
         'legend intersect ismember procrustes hold num2cell '
