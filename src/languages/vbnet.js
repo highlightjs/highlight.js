c6b5be48c55095fc0f972e20f7457cc751a62ace
Jan Pilzer <japilzer@microsoft.com>
2020-10-30 21:51:56 -0700

    Improve vbnet language

--- src/languages/vbnet.js
+++ src/languages/vbnet.js
@@ -6,6 +6,26 @@ Website: https://docs.microsoft.com/en-us/dotnet/visual-basic/getting-started/
 */
 
 export default function(hljs) {
+  const commentMode = hljs.COMMENT(
+    '\'',
+    '$',
+    {
+      returnBegin: true,
+      contains: [
+        {
+          className: 'doctag',
+          begin: '\'\'\'|<!--|-->',
+          contains: [hljs.PHRASAL_WORDS_MODE]
+        },
+        {
+          className: 'doctag',
+          begin: '</?', end: '>',
+          contains: [hljs.PHRASAL_WORDS_MODE]
+        }
+      ]
+    }
+  );
+
   return {
     name: 'Visual Basic .NET',
     aliases: ['vb'],
@@ -24,7 +44,7 @@ export default function(hljs) {
         'select set shadows shared skip static step stop structure strict sub synclock ' + /* s */
         'take text then throw to try unicode until using when where while widening with withevents writeonly xor yield', /* t-y */
       built_in:
-        'boolean byte cbool cbyte cchar cdate cdec cdbl char cint clng cobj csbyte cshort csng cstr ctype ' +  /* b-c */
+        'boolean byte cbool cbyte cchar cdate cdec cdbl char cint clng cobj csbyte cshort csng cstr ctype ' + /* b-c */
         'date decimal directcast double gettype getxmlnamespace iif integer long object ' + /* d-o */
         'sbyte short single string trycast typeof uinteger ulong ushort', /* s-u */
       literal:
@@ -33,30 +53,18 @@ export default function(hljs) {
     illegal: '//|\\{|\\}|endif|gosub|variant|wend|^\\$ ', /* reserved deprecated keywords */
     contains: [
       hljs.inherit(hljs.QUOTE_STRING_MODE, {contains: [{begin: '""'}]}),
-      hljs.COMMENT(
-        '\'',
-        '$',
-        {
-          returnBegin: true,
-          contains: [
-            {
-              className: 'doctag',
-              begin: '\'\'\'|<!--|-->',
-              contains: [hljs.PHRASAL_WORDS_MODE]
-            },
-            {
-              className: 'doctag',
-              begin: '</?', end: '>',
-              contains: [hljs.PHRASAL_WORDS_MODE]
-            }
-          ]
-        }
-      ),
+      commentMode,
       hljs.C_NUMBER_MODE,
       {
         className: 'meta',
-        begin: '#', end: '$',
-        keywords: {'meta-keyword': 'if else elseif end region externalsource'}
+        begin: '#(?:const|disable|else|elseif|enable|end|externalsource|if|region)\\b', end: '$',
+        keywords: {'meta-keyword': 'const disable else elseif enable end externalsource if region'},
+        contains: [commentMode]
+      },
+      {
+        className: 'literal',
+        begin: '#', end: '#',
+        illegal: '\\n'
       }
     ]
   };
