229fc1c5ab618d7fb30530eddfadcce8d6a4a773
Josh Goebel <me@joshgoebel.com>
2020-11-06 01:40:14 -0500

    explain what we are doing

--- .eslintrc.lang.js
+++ .eslintrc.lang.js
@@ -14,5 +14,7 @@ module.exports = {
     ecmaVersion: 2015,
     sourceType: "module"
   },
+  // no rules, this file exists only to lint the grammars and check
+  // that no ES2018 or newer syntax has crept in by accident
   rules: {}
 };
