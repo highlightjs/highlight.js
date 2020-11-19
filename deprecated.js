687ec56842ed748e0aa019faba0b05a01bf583d6
Josh Goebel <me@joshgoebel.com>
2020-11-19 11:58:16 -0500

    remove second reset

--- deprecated.js
+++ deprecated.js
@@ -8,7 +8,7 @@ BgRed = "\x1b[41m"
 
 DEPRECATION = `
 Version 9 of Highlight.js is ${FgRed}no longer supported.${Reset} Please upgrade
-or ask whatever dependency you are using to upgrade.${Reset} More info:
+or ask whatever dependency you are using to upgrade. More info:
 - https://github.com/highlightjs/highlight.js/issues/2877
 - https://github.com/highlightjs/highlight.js/blob/master/VERSION_10_UPGRADE.md`.trim()
 
