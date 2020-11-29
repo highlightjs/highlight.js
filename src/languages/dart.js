f1cdaf87fe1ba6680e86042e398e09f32da607d0
Jan Pilzer <japilzer@microsoft.com>
2020-11-29 14:49:48 -0800

    Fixes dart empty comments

--- src/languages/dart.js
+++ src/languages/dart.js
@@ -152,15 +152,16 @@ export default function(hljs) {
     contains: [
       STRING,
       hljs.COMMENT(
-        '/\\*\\*',
-        '\\*/', {
+        /\/\*\*(?!\/)/,
+        /\*\//,
+        {
           subLanguage: 'markdown',
           relevance: 0
         }
       ),
       hljs.COMMENT(
-        '///+\\s*',
-        '$', {
+        /\/{3,} ?/,
+        /$/, {
           contains: [{
             subLanguage: 'markdown',
             begin: '.',
