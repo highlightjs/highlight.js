56af93ae440ef3a03fcf148c48f43d0d48b33cb1
Josh Goebel <me@joshgoebel.com>
2021-03-17 14:18:34 -0400

    fixup: build monolith again

--- src/stub.js
+++ src/stub.js
@@ -1,11 +1,12 @@
 import HighlightJS from "./highlight.js";
 // @ts-ignore
-import * as builtIns from "builtInLanguages"
+import * as builtIns from "builtInLanguages";
 
 const hljs = HighlightJS;
 
-for (const languageName of Object.keys(builtIns)) {
-  hljs.registerLanguage(languageName, builtIns[languageName]);
+for (const key of Object.keys(builtIns)) {
+  const languageName = key.replace("grmr_", "");
+  hljs.registerLanguage(languageName, builtIns[key]);
 }
 // console.log(hljs.listLanguages());
 
