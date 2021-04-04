e3ec72ac4e36ba1d99a45a20316f4594e4e9b3da
Josh Goebel <me@joshgoebel.com>
2021-04-03 20:24:14 -0400

    chore(lint) properties

--- src/languages/properties.js
+++ src/languages/properties.js
@@ -6,31 +6,34 @@ Category: common, config
 */
 
 export default function(hljs) {
-
   // whitespaces: space, tab, formfeed
-  var WS0 = '[ \\t\\f]*';
-  var WS1 = '[ \\t\\f]+';
+  const WS0 = '[ \\t\\f]*';
+  const WS1 = '[ \\t\\f]+';
   // delimiter
-  var EQUAL_DELIM = WS0+'[:=]'+WS0;
-  var WS_DELIM = WS1;
-  var DELIM = '(' + EQUAL_DELIM + '|' + WS_DELIM + ')';
-  var KEY = '([^\\\\:= \\t\\f\\n]|\\\\.)+';
+  const EQUAL_DELIM = WS0 + '[:=]' + WS0;
+  const WS_DELIM = WS1;
+  const DELIM = '(' + EQUAL_DELIM + '|' + WS_DELIM + ')';
+  const KEY = '([^\\\\:= \\t\\f\\n]|\\\\.)+';
 
-  var DELIM_AND_VALUE = {
-          // skip DELIM
-          end: DELIM,
-          relevance: 0,
-          starts: {
-            // value: everything until end of line (again, taking into account backslashes)
-            className: 'string',
-            end: /$/,
-            relevance: 0,
-            contains: [
-              { begin: '\\\\\\\\'},
-              { begin: '\\\\\\n' }
-            ]
-          }
-        };
+  const DELIM_AND_VALUE = {
+    // skip DELIM
+    end: DELIM,
+    relevance: 0,
+    starts: {
+      // value: everything until end of line (again, taking into account backslashes)
+      className: 'string',
+      end: /$/,
+      relevance: 0,
+      contains: [
+        {
+          begin: '\\\\\\\\'
+        },
+        {
+          begin: '\\\\\\n'
+        }
+      ]
+    }
+  };
 
   return {
     name: '.properties',
@@ -43,8 +46,14 @@ export default function(hljs) {
       {
         returnBegin: true,
         variants: [
-          { begin: KEY + EQUAL_DELIM, relevance: 1 },
-          { begin: KEY + WS_DELIM, relevance: 0 }
+          {
+            begin: KEY + EQUAL_DELIM,
+            relevance: 1
+          },
+          {
+            begin: KEY + WS_DELIM,
+            relevance: 0
+          }
         ],
         contains: [
           {
