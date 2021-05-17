e6c332d135f5e775793854ba79c886b0bc3b4e76
Leopard20 <16115820+Leopard20@users.noreply.github.com>
2021-05-17 23:46:48 +0430

    Using regex for illegal chars

--- src/languages/sqf.js
+++ src/languages/sqf.js
@@ -2504,9 +2504,9 @@ export default function(hljs) {
       PREPROCESSOR
     ],
     illegal: [
-      '\w\$',  //$ is only valid when used with Hex numbers (e.g. $FF) or at the beginning of localized strings (e.g. "$STR_HELLO")
-      '\?',    //There's no ? in SQF
-      '\@'    //There's no @ in SQF
+      /\w\$/,   //$ is only valid when used with Hex numbers (e.g. $FF) or at the beginning of localized strings (e.g. "$STR_HELLO")
+      /\?/  ,   //There's no ? in SQF
+      /@/       //There's no @ in SQF
     ]
   };
 }
