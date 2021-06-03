highlightjs/highlight.js

commit 952fa0a15262518047a40dea60642497612a2695

(enh) simplify Python grammar

src/languages/python.js
@@ -5,6 +5,7 @@ Website: https://www.python.org
 Category: common
 */
 
+import { IDENT_RE } from '../lib/modes.js';
 import * as regex from '../lib/regex.js';
 
 export default function(hljs) {
@@ -376,27 +377,37 @@ export default function(hljs) {
       COMMENT_TYPE,
       hljs.HASH_COMMENT_MODE,
       {
+        match: [
+          /def/, /\s+/,
+          IDENT_RE
+        ],
+        scope: {
+          1: "keyword",
+          3: "title.function"
+        },
+        contains: [ PARAMS ]
+      },
+      {
         variants: [
           {
-            className: 'function',
-            beginKeywords: 'def'
+            match: [
+              /class/, /\s+/,
+              IDENT_RE, /\s*/,
+              /\(\s*/, IDENT_RE,/\s*\)/
+            ],
           },
           {
-            className: 'class',
-            beginKeywords: 'class'
+            match: [
+              /class/, /\s+/,
+              IDENT_RE
+            ],
           }
         ],
-        end: /:/,
-        illegal: /[${=;\n,]/,
-        contains: [
-          hljs.UNDERSCORE_TITLE_MODE,
-          PARAMS,
-          {
-            begin: /->/,
-            endsWithParent: true,
-            keywords: KEYWORDS
-          }
-        ]
+        scope: {
+          1: "keyword",
+          3: "title.class",
+          6: "title.class.inherited",
+        }
       },
       {
         className: 'meta',

commit 318eda317628e9d2187090809a50195aec6dfb69

fix(python) identifiers starting with underscore not highlighted
Since hljs version 11, python identifiers starting with an underscore
character were no longer highlighted (regression introduced in 952fa0a).

Add new python markup test related to identifiers highlighting.

src/languages/python.js
@@ -5,7 +5,7 @@ Website: https://www.python.org
 Category: common
 */
 
-import { IDENT_RE } from '../lib/modes.js';
+import { UNDERSCORE_IDENT_RE } from '../lib/modes.js';
 import * as regex from '../lib/regex.js';
 
 export default function(hljs) {
@@ -379,7 +379,7 @@ export default function(hljs) {
       {
         match: [
           /def/, /\s+/,
-          IDENT_RE
+          UNDERSCORE_IDENT_RE
         ],
         scope: {
           1: "keyword",
@@ -392,14 +392,14 @@ export default function(hljs) {
           {
             match: [
               /class/, /\s+/,
-              IDENT_RE, /\s*/,
-              /\(\s*/, IDENT_RE,/\s*\)/
+              UNDERSCORE_IDENT_RE, /\s*/,
+              /\(\s*/, UNDERSCORE_IDENT_RE,/\s*\)/
             ],
           },
           {
             match: [
               /class/, /\s+/,
-              IDENT_RE
+              UNDERSCORE_IDENT_RE
             ],
           }
         ],