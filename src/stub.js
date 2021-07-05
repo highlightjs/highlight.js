984fc043c466371965abfdccf6ebe8e244919b03
Josh Goebel <me@joshgoebel.com>
2021-07-05 16:24:46 -0400

    Update src/stub.js

--- src/stub.js
+++ src/stub.js
@@ -5,6 +5,10 @@ import * as builtIns from "builtInLanguages";
 const hljs = HighlightJS;
 
 for (const key of Object.keys(builtIns)) {
+  // our builtInLanguages Rollup plugin has to use `_` to allow identifiers to be 
+  // compatible with `export` naming conventions, so we need to convert the 
+  // identifiers back into the more typical dash style that we use for language 
+  // naming via the API
   const languageName = key.replace("grmr_", "").replace("_", "-");
   hljs.registerLanguage(languageName, builtIns[key]);
 }
