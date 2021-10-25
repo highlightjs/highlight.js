98cebbc832d26c92a9c1a0f166c40e96b6ccb27b
Jan Pilzer <japilzer@microsoft.com>
2021-10-24 17:01:13 -0700

    Make demo page keyboard accessible

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,17 @@
 hljs.debugMode();
 hljs.highlightAll();
 
+const addEventListeners = (element, listener) => {
+  element.addEventListener("click", listener);
+  element.addEventListener("keydown", (event) => {
+    if (event.key === " " || event.key === "Enter") {
+      listener(event);
+    }
+  });
+};
+
 document.querySelectorAll(".categories > li").forEach((category) => {
-  category.addEventListener("click", (event) => {
+  addEventListeners(category, (event) => {
     const current = document.querySelector(".categories .current");
     const currentCategory = current.dataset.category;
     const nextCategory = event.target.dataset.category;
@@ -24,14 +33,18 @@ document.querySelectorAll(".categories > li").forEach((category) => {
 });
 
 document.querySelectorAll(".styles > li").forEach((style) => {
-  style.addEventListener("click", (event) => {
+  addEventListeners(style, (event) => {
     const current = document.querySelector(".styles .current");
     const currentStyle = current.textContent;
     const nextStyle = event.target.textContent;
 
     if (currentStyle !== nextStyle) {
-      document.querySelector(`link[title="${nextStyle}"]`).removeAttribute("disabled");
-      document.querySelector(`link[title="${currentStyle}"]`).setAttribute("disabled", "disabled");
+      document
+        .querySelector(`link[title="${nextStyle}"]`)
+        .removeAttribute("disabled");
+      document
+        .querySelector(`link[title="${currentStyle}"]`)
+        .setAttribute("disabled", "disabled");
 
       current.classList.remove("current");
       event.target.classList.add("current");
