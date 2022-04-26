cf923c737c3bc323ee556d845147f3984a7fb6cd
Josh Goebel <me@joshgoebel.com>
2022-04-26 12:07:58 -0400

    enh(profile) small improvements

--- src/languages/profile.js
+++ src/languages/profile.js
@@ -10,30 +10,33 @@ export default function(hljs) {
     contains: [
       hljs.C_NUMBER_MODE,
       {
-        begin: '[a-zA-Z_][\\da-zA-Z_]+\\.[\\da-zA-Z_]{1,3}',
+        begin: /[a-zA-Z_][\da-zA-Z_]+\.[\da-zA-Z_]{1,3}/,
         end: ':',
         excludeEnd: true
       },
       {
-        begin: '(ncalls|tottime|cumtime)',
+        begin: /^\s*(ncalls|tottime|cumtime)/,
         end: '$',
-        keywords: 'ncalls tottime|10 cumtime|10 filename',
-        relevance: 10
+        keywords: 'ncalls tottime cumtime filename percall lineno function',
+        contains: [
+          { match: /[:()]/, scope: "punctuation" }
+        ]
       },
       {
-        begin: 'function calls',
-        end: '$',
-        contains: [ hljs.C_NUMBER_MODE ],
-        relevance: 10
+        match: /function calls(?=\s)/,
+        relevance: 2
       },
       hljs.APOS_STRING_MODE,
       hljs.QUOTE_STRING_MODE,
       {
-        className: 'string',
-        begin: '\\(',
-        end: '\\)$',
-        excludeBegin: true,
-        excludeEnd: true,
+        match: [
+          /\(/,
+          /.*?/,
+          /\)$/
+        ],
+        scope: {
+          2: "string"
+        },
         relevance: 0
       }
     ]
