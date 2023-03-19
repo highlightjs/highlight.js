77a8e5c1654af53e302408c369623f3b4597ea53
Josh Goebel <me@joshgoebel.com>
2023-03-19 16:51:53 -0400

    (release prep) bump for 10.8.0 release

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ copyright = u'2012–2022, Ivan Sagalaev'
 # built documents.
 
 # The full version, including alpha/beta/rc tags.
-release = '11.7.0'
+release = '11.8.0'
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
