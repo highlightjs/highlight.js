5811b723751c7ec030b09b24dd746f757f6af97e
Michael Voříšek <mvorisek@mvorisek.cz>
2023-06-20 01:27:57 +0200

    revert build_browser - minify as before

--- tools/build_browser.js
+++ tools/build_browser.js
@@ -172,7 +172,7 @@ function installDemoStyles() {
     if (stat.isDirectory()) return;
 
     if (file.endsWith(".css")) {
-      installCleanCSS(`./src/styles/${file}`, `demo/styles/${file}`, { minify: false });
+      installCleanCSS(`./src/styles/${file}`, `demo/styles/${file}`);
     } else {
       // images, backgrounds, etc
       install(`./src/styles/${file}`, `demo/styles/${file}`);
