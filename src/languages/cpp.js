80f0ed1fe9ce74b7ebbc0a7a8e4ef93b1247cf1e
tomwt-awa <156514381+tomwt-awa@users.noreply.github.com>
2024-01-21 15:04:58 +0800

    Update cpp.js

--- src/languages/cpp.js
+++ src/languages/cpp.js
@@ -501,7 +501,8 @@ export default function(hljs) {
     // `return some()`, `else if()`, `(x*sum(1, 2))`
     variants: [
       {
-        begin: regex.lookahead('='),
+        begin: /=/,
+        beginScope: "operator",
         end: /;/
       },
       {
