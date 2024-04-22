0db0128d31fa2ce502390e9cab55e689c3f7544b
BhJaipa <105307427+BhJaipal@users.noreply.github.com>
2024-04-22 23:12:06 +0530

    formatting and color change, type import

--- src/languages/c.js
+++ src/languages/c.js
@@ -4,13 +4,13 @@ Category: common, system
 Website: https://en.wikipedia.org/wiki/C_(programming_language)
 */
 
-/** @type LanguageFn */
-export default function(hljs) {
+/** @type {import("highlight.js").LanguageFn} */
+export default function (hljs) {
   const regex = hljs.regex;
   // added for historic reasons because `hljs.C_LINE_COMMENT_MODE` does
   // not include such support nor can we be sure all the grammars depending
   // on it would desire this behavior
-  const C_LINE_COMMENT_MODE = hljs.COMMENT('//', '$', { contains: [ { begin: /\\\n/ } ] });
+  const C_LINE_COMMENT_MODE = hljs.COMMENT('//', '$', { contains: [{ begin: /\\\n/ }] });
   const DECLTYPE_AUTO_RE = 'decltype\\(auto\\)';
   const NAMESPACE_RE = '[a-zA-Z_]\\w*::';
   const TEMPLATE_ARGUMENT_RE = '<[^<>]+>';
@@ -18,7 +18,7 @@ export default function(hljs) {
     + DECLTYPE_AUTO_RE + '|'
     + regex.optional(NAMESPACE_RE)
     + '[a-zA-Z_]\\w*' + regex.optional(TEMPLATE_ARGUMENT_RE)
-  + ')';
+    + ')';
 
 
   const TYPES = {
@@ -40,7 +40,7 @@ export default function(hljs) {
         begin: '(u8?|U|L)?"',
         end: '"',
         illegal: '\\n',
-        contains: [ hljs.BACKSLASH_ESCAPE ]
+        contains: [hljs.BACKSLASH_ESCAPE]
       },
       {
         begin: '(u8?|U|L)?\'(' + CHARACTER_ESCAPES + "|.)",
@@ -68,9 +68,11 @@ export default function(hljs) {
     className: 'meta',
     begin: /#\s*[a-z]+\b/,
     end: /$/,
-    keywords: { keyword:
+    keywords: {
+      keyword:
         'if else elif endif define undef warning error line '
-        + 'pragma _Pragma ifdef ifndef elifdef elifndef include' },
+        + 'pragma _Pragma ifdef ifndef elifdef elifndef include'
+    },
     contains: [
       {
         begin: /\\\n/,
@@ -223,7 +225,7 @@ export default function(hljs) {
         begin: /\(/,
         end: /\)/,
         keywords: KEYWORDS,
-        contains: EXPRESSION_CONTAINS.concat([ 'self' ]),
+        contains: EXPRESSION_CONTAINS.concat(['self']),
         relevance: 0
       }
     ]),
@@ -246,7 +248,7 @@ export default function(hljs) {
       {
         begin: FUNCTION_TITLE,
         returnBegin: true,
-        contains: [ hljs.inherit(TITLE_MODE, { className: "title.function" }) ],
+        contains: [hljs.inherit(TITLE_MODE, { className: "title.function" })],
         relevance: 0
       },
       // allow for multiple declarations, e.g.:
@@ -293,7 +295,7 @@ export default function(hljs) {
 
   return {
     name: "C",
-    aliases: [ 'h' ],
+    aliases: ['h'],
     keywords: KEYWORDS,
     // Until differentiations are added between `c` and `cpp`, `c` will
     // not be auto-detected to avoid auto-detect conflicts between C and C++
