a7b28b82928687e92a920c3e21762d6d3aca1320
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-04-24 12:09:01 +0530

    Update llvm.js

--- src/languages/llvm.js
+++ src/languages/llvm.js
@@ -7,7 +7,7 @@ Category: assembler
 Audit: 2020
 */
 
-/** @type {import("highlight.js").LanguageFn} */
+/** @type {LanguageFn} */
 export default function (hljs) {
   const regex = hljs.regex;
   const IDENT_RE = /([-a-zA-Z$._][\w$.-]*)/;
