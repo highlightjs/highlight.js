2aac412acf2fb12da17ef39cf0acab07bb2db4f8
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-04-24 09:35:50 +0530

    Update bash.js

--- src/languages/bash.js
+++ src/languages/bash.js
@@ -6,7 +6,7 @@ Website: https://www.gnu.org/software/bash/
 Category: common, scripting
 */
 
-/** @type {import("highlight.js").LanguageFn} */
+/** @type {LanguageFn} */
 export default function (hljs) {
   const regex = hljs.regex;
   const VAR = {};
