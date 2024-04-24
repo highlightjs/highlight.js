b25eae14918d8e0add844a4256816fd9ff94f340
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2024-04-24 09:32:53 +0530

    Update armasm.js

--- src/languages/armasm.js
+++ src/languages/armasm.js
@@ -5,7 +5,7 @@ Description: ARM Assembly including Thumb and Thumb2 instructions
 Category: assembler
 */
 
-/** @type {import("highlight.js").LanguageFn} */
+/** @type {LanguageFn} */
 export default function (hljs) {
   // local labels: %?[FB]?[AT]?\d{1,2}\w+
 
