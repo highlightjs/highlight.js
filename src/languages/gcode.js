9d288158c11ecfd26e2a9f3be6fceac4148b0f20
barthy <post@barthy.koeln>
2024-04-24 09:15:24 +0200

    enh(gcode): review fixes and on:begin filter

--- src/languages/gcode.js
+++ src/languages/gcode.js
@@ -10,7 +10,7 @@ export default function(hljs) {
   const regex = hljs.regex;
   const GCODE_KEYWORDS = {
     $pattern: /[A-Z]+|%/,
-    keywords: [
+    keyword: [
       // conditions
       'THEN',
       'ELSE',
@@ -62,8 +62,31 @@ export default function(hljs) {
   };
 
 
-  const LETTER_BOUNDARY_RE = /(?<![A-Z])/;
+  // TODO: post v12 lets use look-behind, until then \b and a callback filter will be used
+  // const LETTER_BOUNDARY_RE = /(?<![A-Z])/;
+  const LETTER_BOUNDARY_RE = /\b/;
+
+  function LETTER_BOUNDARY_CALLBACK(matchdata, response) {
+    if (matchdata.index === 0) {
+      return;
+    }
+
+    const charBeforeMatch = matchdata.input[matchdata.index - 1];
+    if (charBeforeMatch >= '0' && charBeforeMatch <= '9') {
+      return;
+    }
+
+    response.ignoreMatch();
+  }
+
   const NUMBER_RE = /[+-]?((\.\d+)|(\d+)(\.\d*)?)/;
+
+  const GENERAL_MISC_FUNCTION_RE = /[GM]\s*\d+(\.\d+)?/;
+  const TOOLS_RE = /T\s*\d+/;
+  const SUBROUTINE_RE = /O\s*\d+/;
+  const SUBROUTINE_NAMED_RE = /O<.+>/;
+  const AXES_RE = /[ABCUVWXYZ]\s*/;
+  const PARAMETERS_RE = /[FHIJKPQRS]\s*/;
 
   const GCODE_CODE = [
     // comments
@@ -79,9 +102,17 @@ export default function(hljs) {
       variants: [
         // G General functions: G0, G5.1, G5.2, …
         // M Misc functions: M0, M55.6, M199, …
-        { match: regex.concat(LETTER_BOUNDARY_RE, /[GM]\s*\d+(\.\d+)?/) },
+        { match: regex.concat(LETTER_BOUNDARY_RE, GENERAL_MISC_FUNCTION_RE) },
+        {
+          begin: GENERAL_MISC_FUNCTION_RE,
+          'on:begin': LETTER_BOUNDARY_CALLBACK
+        },
         // T Tools
-        { match: regex.concat(LETTER_BOUNDARY_RE, /T\s*\d+/) }
+        { match: regex.concat(LETTER_BOUNDARY_RE, TOOLS_RE), },
+        {
+          begin: TOOLS_RE,
+          'on:begin': LETTER_BOUNDARY_CALLBACK
+        }
       ]
     },
 
@@ -89,9 +120,17 @@ export default function(hljs) {
       scope: 'symbol',
       variants: [
         // O Subroutine ID: O100, O110, …
-        { match: regex.concat(LETTER_BOUNDARY_RE, /O\s*\d+/) },
+        { match: regex.concat(LETTER_BOUNDARY_RE, SUBROUTINE_RE) },
+        {
+          begin: SUBROUTINE_RE,
+          'on:begin': LETTER_BOUNDARY_CALLBACK
+        },
         // O Subroutine name: O<some>, …
-        { match: regex.concat(LETTER_BOUNDARY_RE, /O<.+>/) },
+        { match: regex.concat(LETTER_BOUNDARY_RE, SUBROUTINE_NAMED_RE) },
+        {
+          begin: SUBROUTINE_NAMED_RE,
+          'on:begin': LETTER_BOUNDARY_CALLBACK
+        },
         // Checksum at end of line: *71, *199, …
         { match: /\*\s*\d+\s*$/ }
       ]
@@ -108,13 +147,25 @@ export default function(hljs) {
     },
 
     {
-      scope: 'property', // Physical axes
-      match: regex.concat(LETTER_BOUNDARY_RE, /[ABCUVWXYZ]\s*/, NUMBER_RE)
+      scope: 'property', // Physical axes,
+      variants: [
+        { match: regex.concat(LETTER_BOUNDARY_RE, AXES_RE, NUMBER_RE) },
+        {
+          begin: regex.concat(AXES_RE, NUMBER_RE),
+          'on:begin': LETTER_BOUNDARY_CALLBACK
+        },
+      ]
     },
 
     {
       scope: 'params', // Different types of parameters
-      match: regex.concat(LETTER_BOUNDARY_RE, /[FHIJKPQRS]\s*/, NUMBER_RE)
+      variants: [
+        { match: regex.concat(LETTER_BOUNDARY_RE, PARAMETERS_RE, NUMBER_RE) },
+        {
+          begin: regex.concat(PARAMETERS_RE, NUMBER_RE),
+          'on:begin': LETTER_BOUNDARY_CALLBACK
+        },
+      ]
     },
   ];
 
@@ -124,6 +175,8 @@ export default function(hljs) {
     // Some implementations (CNC controls) of G-code are interoperable with uppercase and lowercase letters seamlessly.
     // However, most prefer all uppercase and uppercase is customary.
     case_insensitive: true,
+    // TODO: post v12 with the use of look-behind this can be enabled
+    disableAutodetect: true,
     keywords: GCODE_KEYWORDS,
     contains: GCODE_CODE
   };
