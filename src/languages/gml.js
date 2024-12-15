5221c23109e7f5eb786632d20dd550bcef09ec6e
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-12-16 10:42:55 +1100

    fix(gml) template strings cannot contain newlines

--- src/languages/gml.js
+++ src/languages/gml.js
@@ -1173,6 +1173,7 @@ export default function(hljs) {
         end: /\"/,
         beginScope: "string",
         endScope: "string",
+        illegal: /\n/,
         contains: [
           STRING_ESCAPE,
           STRING_SUBSTITUTION,
