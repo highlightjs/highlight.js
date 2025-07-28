67eb46a4aa9e74d0d894b498342be21a16666dfa
Lee Yunjin <gzblues61@daum.net>
2025-07-28 18:45:42 +0900

    add XDG Desktop shortcut file highlighter

---
+++ src/languages/desktop.js
@@ -0,0 +1,87 @@
+/*
+Language: XDGDesktop
+Description: XDG Desktop Entry Specification file format.
+Contributors: Lee Yunjin <gzblues61@daum.net>
+Category: common, config
+Website: https://www.freedesktop.org/wiki/Specifications/desktop-entry-spec/
+*/
+
+export default function(hljs) {
+  const LITERALS = {
+    className: 'literal',
+    begin: /\b(true|false|Application|Link|Directory)\b/,
+    relevance: 0
+  };
+
+  const FIELD_CODES = {
+    className: 'variable',
+    match: /%[fFuUcCiIkKvV]/
+  };
+
+  const QUOTED_STRING = {
+    className: 'string',
+    begin: /"/,
+    end: /"/,
+    contains: [ hljs.BACKSLASH_ESCAPE ],
+    relevance: 0
+  };
+
+  const COMMENT_MODE = {
+    className: 'comment',
+    begin: /#/,
+    end: /$/,
+    relevance: 0
+  };
+
+  const SECTION_HEADER = {
+    className: 'section',
+    begin: /^\[(Desktop Entry|Desktop Action\s+[A-Za-z0-9_-]+)\]/,
+    relevance: 10
+  };
+
+  const MIMETYPE_FORMAT = {
+    className: 'type',
+    match: /\b[a-zA-Z0-9_.+-]+\/[a-zA-Z0-9_.+-]+/,
+    relevance: 0
+  };
+
+  const KEY_VALUE_PAIR = {
+    begin: /^[A-Za-z0-9_-]+(\[[A-Za-z0-9_@.]+\])?\s*=/,
+    end: /$/,
+    returnBegin: true,
+    contains: [
+      {
+        className: 'attribute',
+        begin: /^[A-Za-z0-9_-]+(\[[A-Za-z0-9_@.]+\])?/,
+        relevance: 10
+      },
+      {
+        className: 'operator',
+        match: /=/,
+        relevance: 0
+      },
+      COMMENT_MODE,
+      LITERALS,
+      QUOTED_STRING,
+      FIELD_CODES,
+      MIMETYPE_FORMAT,
+      {
+        className: 'punctuation',
+        match: /;/,
+        relevance: 0
+      }
+    ]
+  };
+
+  return {
+    name: 'Desktop',
+    aliases: ['desktop'],
+    case_insensitive: false,
+    contains: [
+      COMMENT_MODE,
+      SECTION_HEADER,
+      KEY_VALUE_PAIR
+    ]
+  };
+}
+
