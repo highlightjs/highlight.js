545722887adc9742060717450b93c88937397fbb
Lee Yunjin <gzblues61@daum.net>
2025-08-02 13:32:13 +0900

    make field code to case insensitive one

--- src/languages/freedesktop.js
+++ src/languages/freedesktop.js
@@ -8,7 +8,7 @@ Website: https://www.freedesktop.org/
 export default function(hljs) {
   const FIELD_CODES = {
     className: 'variable',
-    begin: /%[a-zA-Z]/,
+    begin: /%[a-z]/i,
     relevance: 0
   };
 
