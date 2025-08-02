4f76d4075f855580cfa983ed145f2a7fa813192d
Lee Yunjin <gzblues61@daum.net>
2025-08-02 13:12:35 +0900

    add wider field codes

--- src/languages/freedesktop.js
+++ src/languages/freedesktop.js
@@ -8,7 +8,7 @@ Website: https://www.freedesktop.org/
 export default function(hljs) {
   const FIELD_CODES = {
     className: 'variable',
-    match: /%[fFuUcCiIkKvV]/,
+    match: /%[a-zA-Z]/,
     relevance: 0
   };
 
