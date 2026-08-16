e1a76caedfd179cdd2742d83afb8045703ca45a8
pikammmmm <124257101+pikammmmm@users.noreply.github.com>
2026-08-16 20:29:30 +0200

    fix(haml) recognize implicit div tag lines starting with .class or #id
    The tag mode only opened on `^\s*%`, so HAML's implicit-div syntax
    (`.class` / `#id` at the start of a line) got no highlighting at all.
    Add a second tag-mode variant that begins on those lines and reuses
    the same nested selector/attribute rules, now extracted into shared
    constants.
    
    The implicit-div variant carries relevance 0 throughout: lines
    starting with `.foo` / `#foo` are common in CSS, C preprocessor and
    assembler sources, and must not pull highlightAuto towards HAML.
    
    Fixes #3783

--- src/languages/haml.js
+++ src/languages/haml.js
@@ -8,6 +8,65 @@ Category: template
 
 // TODO support filter tags like :javascript, support inline HTML
 export default function(hljs) {
+  const SELECTOR_ID = {
+    className: 'selector-id',
+    begin: '#[\\w-]+'
+  };
+  const SELECTOR_CLASS = {
+    className: 'selector-class',
+    begin: '\\.[\\w-]+'
+  };
+  const RUBY_HASH_ATTRS = {
+    begin: /\{\s*/,
+    end: /\s*\}/,
+    contains: [
+      {
+        begin: ':\\w+\\s*=>',
+        end: ',\\s+',
+        returnBegin: true,
+        endsWithParent: true,
+        contains: [
+          {
+            className: 'attr',
+            begin: ':\\w+'
+          },
+          hljs.APOS_STRING_MODE,
+          hljs.QUOTE_STRING_MODE,
+          {
+            begin: '\\w+',
+            relevance: 0
+          }
+        ]
+      }
+    ]
+  };
+  const HTML_ATTRS = {
+    begin: '\\(\\s*',
+    end: '\\s*\\)',
+    excludeEnd: true,
+    contains: [
+      {
+        begin: '\\w+\\s*=',
+        end: '\\s+',
+        returnBegin: true,
+        endsWithParent: true,
+        contains: [
+          {
+            className: 'attr',
+            begin: '\\w+',
+            relevance: 0
+          },
+          hljs.APOS_STRING_MODE,
+          hljs.QUOTE_STRING_MODE,
+          {
+            begin: '\\w+',
+            relevance: 0
+          }
+        ]
+      }
+    ]
+  };
+
   return {
     name: 'HAML',
     case_insensitive: true,
@@ -38,64 +97,26 @@ export default function(hljs) {
             className: 'selector-tag',
             begin: '\\w+'
           },
-          {
-            className: 'selector-id',
-            begin: '#[\\w-]+'
-          },
-          {
-            className: 'selector-class',
-            begin: '\\.[\\w-]+'
-          },
-          {
-            begin: /\{\s*/,
-            end: /\s*\}/,
-            contains: [
-              {
-                begin: ':\\w+\\s*=>',
-                end: ',\\s+',
-                returnBegin: true,
-                endsWithParent: true,
-                contains: [
-                  {
-                    className: 'attr',
-                    begin: ':\\w+'
-                  },
-                  hljs.APOS_STRING_MODE,
-                  hljs.QUOTE_STRING_MODE,
-                  {
-                    begin: '\\w+',
-                    relevance: 0
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            begin: '\\(\\s*',
-            end: '\\s*\\)',
-            excludeEnd: true,
-            contains: [
-              {
-                begin: '\\w+\\s*=',
-                end: '\\s+',
-                returnBegin: true,
-                endsWithParent: true,
-                contains: [
-                  {
-                    className: 'attr',
-                    begin: '\\w+',
-                    relevance: 0
-                  },
-                  hljs.APOS_STRING_MODE,
-                  hljs.QUOTE_STRING_MODE,
-                  {
-                    begin: '\\w+',
-                    relevance: 0
-                  }
-                ]
-              }
-            ]
-          }
+          SELECTOR_ID,
+          SELECTOR_CLASS,
+          RUBY_HASH_ATTRS,
+          HTML_ATTRS
+        ]
+      },
+      {
+        className: 'tag',
+        // implicit div: the line starts directly with `.class` or `#id`;
+        // `[\w-]` keeps `#{interpolation}` out of this case
+        begin: '^\\s*(?=[.#][\\w-])',
+        // relevance 0 everywhere: lines starting with `.foo` / `#foo` are
+        // common in other languages (CSS selectors, C preprocessor,
+        // assembler directives), so they must not count towards HAML
+        relevance: 0,
+        contains: [
+          hljs.inherit(SELECTOR_ID, { relevance: 0 }),
+          hljs.inherit(SELECTOR_CLASS, { relevance: 0 }),
+          hljs.inherit(RUBY_HASH_ATTRS, { relevance: 0 }),
+          hljs.inherit(HTML_ATTRS, { relevance: 0 })
         ]
       },
       { begin: '^\\s*[=~]\\s*' },
